5617970525d0b493c10c1316a50071da991c810a
Baltan <617640006@qq.com>
2019-03-08 16:47:19 +0800

    178. Rank Scores

---
+++ src/leetcode/database/RankScores.sql
@@ -0,0 +1,1 @@
+select s2.Score,c.rank from Scores s2,(select @rank:=@rank+1 rank,a.score score from (select distinct s1.Score score from Scores s1 order by s1.Score desc) a,(select @rank:=0) b) c where s2.Score=c.score order by c.rank
