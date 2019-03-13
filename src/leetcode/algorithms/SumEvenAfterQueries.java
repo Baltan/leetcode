f88b992c775418d3b1d489c0523c41461ce600ba
Baltan <617640006@qq.com>
2019-03-13 13:55:07 +0800

    985. Sum of Even Numbers After Queries

---
+++ src/leetcode/algorithms/SumEvenAfterQueries.java
@@ -0,0 +1,45 @@
+package leetcode.algorithms;
+
+import leetcode.util.OutputUtils;
+
+import java.util.Arrays;
+
+/**
+ * Description: Sum of Even Numbers After Queries
+ *
+ * @author Baltan
+ * @date 2019-03-13 11:25
+ */
+public class SumEvenAfterQueries {
+    public static void main(String[] args) {
+        int[] A1 = {1, 2, 3, 4};
+        int[][] queries1 = {{1, 0}, {-3, 1}, {-4, 0}, {2, 3}};
+        OutputUtils.print1DIntegerArray(sumEvenAfterQueries(A1, queries1));
+    }
+
+    public static int[] sumEvenAfterQueries(int[] A, int[][] queries) {
+        int queriesLength = queries.length;
+        int[] result = new int[queriesLength];
+        int sum = Arrays.stream(A).filter(v -> (v & 1) == 0).sum();
+
+        for (int i = 0; i < queriesLength; i++) {
+            int oldVal = A[queries[i][1]];
+            int val = queries[i][0];
+
+            if ((oldVal & 1) == 0) {
+                if ((val & 1) == 0) {
+                    sum += val;
+                } else {
+                    sum -= oldVal;
+                }
+            } else {
+                if ((val & 1) == 1) {
+                    sum += (oldVal + val);
+                }
+            }
+            result[i] = sum;
+            A[queries[i][1]] = oldVal + val;
+        }
+        return result;
+    }
+}
