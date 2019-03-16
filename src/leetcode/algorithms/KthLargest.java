fefc35fa49eeee032efb930a3a1751a8e68c243c
Baltan <617640006@qq.com>
2019-03-16 13:13:10 +0800

    703. Kth Largest Element in a Stream

---
+++ src/leetcode/algorithms/KthLargest.java
@@ -0,0 +1,44 @@
+package leetcode.algorithms;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * Description: Kth Largest Element in a Stream
+ *
+ * @author Baltan
+ * @date 2019-03-16 12:40
+ */
+public class KthLargest {
+    private Queue<Integer> queue;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        queue = new PriorityQueue<>(k);
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        if (queue.size() < k) {
+            queue.offer(val);
+        } else {
+            if (queue.peek() < val) {
+                queue.poll();
+                queue.offer(val);
+            }
+        }
+        return queue.peek();
+    }
+
+    public static void main(String[] args) {
+        KthLargest kthLargest = new KthLargest(3, new int[]{4, 5, 8, 2});
+        System.out.println(kthLargest.add(3));
+        System.out.println(kthLargest.add(5));
+        System.out.println(kthLargest.add(10));
+        System.out.println(kthLargest.add(9));
+        System.out.println(kthLargest.add(4));
+    }
+}
