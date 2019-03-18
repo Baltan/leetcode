4a51ffcdf5f27a0b381168f3fe98c6984d941c08
Baltan <617640006@qq.com>
2019-03-18 14:17:34 +0800

    938. Range Sum of BST

---
+++ src/leetcode/algorithms/RangeSumBST.java
@@ -0,0 +1,41 @@
+package leetcode.algorithms;
+
+import leetcode.entity.TreeNode;
+import leetcode.util.BinaryTreeUtils;
+
+/**
+ * Description: Range Sum of BST
+ *
+ * @author Baltan
+ * @date 2019-03-18 13:56
+ */
+public class RangeSumBST {
+    public static void main(String[] args) {
+        TreeNode root1 = BinaryTreeUtils.arrayToBinaryTree(new Integer[]{10, 5, 15, 3, 7, null, 18}, 0);
+        System.out.println(rangeSumBST(root1, 7, 15));
+
+        TreeNode root2 =
+                BinaryTreeUtils.arrayToBinaryTree(new Integer[]{10, 5, 15, 3, 7, 13, 18, 1, null, 6}, 0);
+        System.out.println(rangeSumBST(root2, 6, 10));
+    }
+
+    public static int rangeSumBST(TreeNode root, int L, int R) {
+        if (root == null) {
+            return 0;
+        }
+
+        int sum = 0;
+
+        if (root.val >= L && root.val <= R) {
+            sum += root.val;
+        }
+
+        if (root.val <= L) {
+            return sum + rangeSumBST(root.right, L, R);
+        } else if (root.val >= R) {
+            return sum + rangeSumBST(root.left, L, R);
+        } else {
+            return sum + rangeSumBST(root.right, L, R) + rangeSumBST(root.left, L, R);
+        }
+    }
+}
