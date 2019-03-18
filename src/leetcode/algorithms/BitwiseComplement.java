228ce08ba42ac144c31c704d8fe7dbff3e5b88c5
Baltan <617640006@qq.com>
2019-03-18 10:24:40 +0800

    1012. Complement of Base 10 Integer

---
+++ src/leetcode/algorithms/BitwiseComplement.java
@@ -0,0 +1,29 @@
+package leetcode.algorithms;
+
+/**
+ * Description: Complement of Base 10 Integer
+ *
+ * @author Baltan
+ * @date 2019-03-18 10:17
+ */
+public class BitwiseComplement {
+    public static void main(String[] args) {
+        System.out.println(bitwiseComplement(5));
+        System.out.println(bitwiseComplement(7));
+        System.out.println(bitwiseComplement(10));
+        System.out.println(bitwiseComplement(0));
+        System.out.println(bitwiseComplement(4));
+        System.out.println(bitwiseComplement(1));
+        System.out.println(bitwiseComplement(16));
+    }
+
+    public static int bitwiseComplement(int N) {
+        int binaryBitNum = 1;
+
+        while (Math.pow(2, binaryBitNum) <= N) {
+            binaryBitNum++;
+        }
+
+        return (int) (Math.pow(2, binaryBitNum) - 1 - N);
+    }
+}
