be2b24c1a3af3783dea4546766643150a92f4aa1
Baltan <617640006@qq.com>
2019-03-19 14:19:53 +0800

    921. Minimum Add to Make Parentheses Valid

---
+++ src/leetcode/algorithms/MinAddToMakeValid.java
@@ -0,0 +1,36 @@
+package leetcode.algorithms;
+
+import java.util.Stack;
+
+/**
+ * Description: Minimum Add to Make Parentheses Valid
+ *
+ * @author Baltan
+ * @date 2019-03-19 14:10
+ */
+public class MinAddToMakeValid {
+    public static void main(String[] args) {
+        System.out.println(minAddToMakeValid("())"));
+        System.out.println(minAddToMakeValid("((("));
+        System.out.println(minAddToMakeValid("()"));
+        System.out.println(minAddToMakeValid("()))(("));
+        System.out.println(minAddToMakeValid(""));
+        System.out.println(minAddToMakeValid(")))))"));
+        System.out.println(minAddToMakeValid("((((("));
+        System.out.println(minAddToMakeValid(")))((("));
+        System.out.println(minAddToMakeValid("))(()("));
+    }
+
+    public static int minAddToMakeValid(String S) {
+        Stack<Character> stack = new Stack<>();
+        int length = S.length();
+        for (int i = 0; i < length; i++) {
+            if (S.charAt(i) == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else {
+                stack.add(S.charAt(i));
+            }
+        }
+        return stack.size();
+    }
+}
