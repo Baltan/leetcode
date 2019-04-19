4b047dc8bc18bf21e6640939fdb3c0506f150627
Baltan <617640006@qq.com>
2019-04-19 11:16:12 +0800

    1027. Longest Arithmetic Sequence

--- src/leetcode/algorithms/LongestArithSeqLength.java
+++ src/leetcode/algorithms/LongestArithSeqLength.java
@@ -11,8 +11,8 @@ import java.util.Map;
  */
 public class LongestArithSeqLength {
     public static void main(String[] args) {
-//        System.out.println(longestArithSeqLength(new int[]{3, 6, 9, 12}));
-//        System.out.println(longestArithSeqLength(new int[]{9, 4, 7, 2, 10}));
+        System.out.println(longestArithSeqLength(new int[]{3, 6, 9, 12}));
+        System.out.println(longestArithSeqLength(new int[]{9, 4, 7, 2, 10}));
         System.out.println(longestArithSeqLength(new int[]{20, 1, 15, 3, 10, 5, 8}));
         System.out.println(longestArithSeqLength(new int[]{83, 20, 17, 43, 52, 78, 68, 45}));
     }
