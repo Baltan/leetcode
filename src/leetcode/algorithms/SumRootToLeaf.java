5c0ecf99b56e50297b1cc58128a4269eb927f0ea
Baltan <617640006@qq.com>
2019-05-21 22:57:55 +0800

    1022. Sum of Root To Leaf Binary Numbers

--- src/leetcode/algorithms/SumRootToLeaf.java
+++ src/leetcode/algorithms/SumRootToLeaf.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Description: Sum of Root To Leaf Binary Numbers
+ * Description: 1022. Sum of Root To Leaf Binary Numbers
  *
  * @author Baltan
  * @date 2019-04-08 09:12
