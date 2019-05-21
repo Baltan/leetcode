9399fac71c762dd83d58742fe060121dd80d344a
Baltan <617640006@qq.com>
2019-05-21 13:47:16 +0800

    28. Implement strStr()

--- src/leetcode/algorithms/StrStr.java
+++ src/leetcode/algorithms/StrStr.java
@@ -1,16 +1,33 @@
 package leetcode.algorithms;
 
+import java.util.Objects;
+
 /**
- * Description:Implement strStr()
+ * Description: 28. Implement strStr()
  *
  * @author Baltan
  * @date 2017/11/28 08:28
  */
 public class StrStr {
     public static void main(String[] args) {
+        System.out.println(strStr("hello", "ll"));
+        System.out.println(strStr("aaaaa", "bba"));
+        System.out.println(strStr("aaaaa", ""));
     }
 
     public static int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        if (Objects.equals(needle, "")) {
+            return 0;
+        }
+
+        int length = haystack.length();
+        int wordLength = needle.length();
+
+        for (int i = 0; i < length + 1 - wordLength; i++) {
+            if (Objects.equals(haystack.substring(i, i + wordLength), needle)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
