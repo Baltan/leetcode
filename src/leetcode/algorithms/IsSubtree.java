41f67f4567d6b61e5094c14227e59ab814bbd85f
Baltan <617640006@qq.com>
2019-05-21 17:00:20 +0800

    572. Subtree of Another Tree

--- src/leetcode/algorithms/IsSubtree.java
+++ src/leetcode/algorithms/IsSubtree.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Description: Subtree of Another Tree
+ * Description: 572. Subtree of Another Tree
  *
  * @author Baltan
  * @date 2018/8/8 16:53
