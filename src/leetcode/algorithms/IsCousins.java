d75855bdd0383c6f67b1c698cc56f3db0d8afc6b
Baltan <617640006@qq.com>
2019-05-21 22:43:21 +0800

    993. Cousins in Binary Tree

--- src/leetcode/algorithms/IsCousins.java
+++ src/leetcode/algorithms/IsCousins.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Description: Cousins in Binary Tree
+ * Description: 993. Cousins in Binary Tree
  *
  * @author Baltan
  * @date 2019-03-15 12:40
