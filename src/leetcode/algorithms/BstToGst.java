f812faceb0c1dfaba8d5624e342f8176c713f45e
Baltan <617640006@qq.com>
2019-05-31 16:59:06 +0800

    1038. Binary Search Tree to Greater Sum Tree

--- src/leetcode/algorithms/BstToGst.java
+++ src/leetcode/algorithms/BstToGst.java
@@ -5,16 +5,16 @@ import leetcode.util.BinaryTreeUtils;
 
 /**
  * Description: 1038. Binary Search Tree to Greater Sum Tree
- * @see ConvertBST
  *
  * @author Baltan
  * @date 2019-05-06 09:24
+ * @see ConvertBST
  */
 public class BstToGst {
     public static void main(String[] args) {
         TreeNode root1 = BinaryTreeUtils.arrayToBinaryTree(
                 new Integer[]{4, 1, 6, 0, 2, 5, 7, null, null, null, 3, null, null, null, 8}, 0);
-        System.out.println(BinaryTreeUtils.preOrder(bstToGst(root1)));
+        System.out.println(BinaryTreeUtils.recursivelyPreOrder(bstToGst(root1)));
     }
 
     private static int sum = 0;
