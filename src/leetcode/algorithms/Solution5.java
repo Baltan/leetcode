2e41233d4d57a3afe2e49aa039f38547b4b2718d
Baltan <617640006@qq.com>
2019-07-06 10:28:59 +0800

    398. Random Pick Index

--- src/leetcode/algorithms/Solution5.java
+++ src/leetcode/algorithms/Solution5.java
@@ -23,6 +23,16 @@ public class Solution5 {
         int randomNum;
         int length = nums.length;
 
+        /**
+         * 假设target在数组中的索引依次为：m,n,p……
+         * (1)i==m时，count==1,randomNum为0的可能性是1，result为m的可能性为1
+         * (2)i==n时，count==2,randomNum为0的可能性是1/2，不为0的可能性是1-1/2，
+         *    result为n的可能性为1/2，result仍旧为m的可能性为(1-1/2)*1=1/2
+         * (3)i==p时，count==3,randomNum为0的可能性是1/3，不为0的可能性是1-1/3，
+         *    result为p的可能性为1/3，result仍旧为n或m的可能性为(1-1/3)*1/2=1/3
+         * ……
+         * 以此类推保证每个值被取到的可能性相等
+         */
         for (int i = 0; i < length; i++) {
             if (nums[i] == target) {
                 count++;
