e898d37060afb8602fc8be1b49368e600af0e530
Baltan <617640006@qq.com>
2019-07-16 09:28:17 +0800

    174. Dungeon Game

--- src/leetcode/algorithms/CalculateMinimumHP.java
+++ src/leetcode/algorithms/CalculateMinimumHP.java
@@ -31,20 +31,31 @@ public class CalculateMinimumHP {
 
         int rows = dungeon.length;
         int cols = dungeon[0].length;
-
+        /**
+         * 保存进入某个格子前至少需要带有的血量，minHPMatrix[0][0]即为进入左上角的格子前至少要有的血量
+         */
         int[][] minHPMatrix = new int[rows][cols];
-
+        /**
+         * 如果最右下角这个格子血量x不为负，则进入这个格子的时候至少有1滴血即可，
+         * 如果最右下角这个格子血量x为负，因为出格子时至少还要有1滴血，则进入这个格子的时候至少有1-x滴血即可
+         */
         minHPMatrix[rows - 1][cols - 1] =
                 dungeon[rows - 1][cols - 1] >= 0 ? 1 : 1 - dungeon[rows - 1][cols - 1];
-
+        /**
+         * 沿最下面这条边走（当处于最下面一排时，此时只能向右走），出某个格子后的血量即为进入其右边格子的最少要求的血量
+         */
         for (int i = cols - 2; i >= 0; i--) {
             minHPMatrix[rows - 1][i] = Math.max(1, minHPMatrix[rows - 1][i + 1] - dungeon[rows - 1][i]);
         }
-
+        /**
+         * 沿最右边这条边走（当处于最右边一排时，此时只能向下走），出某个格子后的血量即为进入其下边格子的最少要求的血量
+         */
         for (int i = rows - 2; i >= 0; i--) {
             minHPMatrix[i][cols - 1] = Math.max(1, minHPMatrix[i + 1][cols - 1] - dungeon[i][cols - 1]);
         }
-
+        /**
+         * 其余格子，出某个格子后的血量即为进入其右边格子或其下面格子的最少要求的血量的较小值
+         */
         for (int i = rows - 2; i >= 0; i--) {
             for (int j = cols - 2; j >= 0; j--) {
                 int minHP = Math.min(minHPMatrix[i][j + 1], minHPMatrix[i + 1][j]);
