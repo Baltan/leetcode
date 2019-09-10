0f1e096bad48b26d41df401ee2100e9324570736
Baltan <617640006@qq.com>
2019-09-10 20:07:21 +0800

    1186. Maximum Subarray Sum with One Deletion

--- src/leetcode/algorithms/MaximumSum.java
+++ src/leetcode/algorithms/MaximumSum.java
@@ -19,19 +19,44 @@ public class MaximumSum {
     }
 
     public static int maximumSum(int[] arr) {
-        int length = arr.length;
+        if (arr.length == 1) {
+            return arr[0];
+        }
+        /**
+         * 保存子数组最大和
+         */
         int result = Integer.MIN_VALUE;
-
-        for (int i = 0; i < length; i++) {
-            result = Math.max(result, arr[i]);
-            int min = arr[i];
-            int sum = arr[i];
-
-            for (int j = i + 1; j < length; j++) {
-                sum += arr[j];
-                min = Math.min(min, arr[j]);
-                result = Math.max(result, Math.max(sum, sum - min));
-            }
+        int length = arr.length;
+        /**
+         * 保存不删除元素的子数组的最大和
+         */
+        int[] dp1 = new int[length];
+        /**
+         * 保存删除一个元素的子数组的最大和
+         */
+        int[] dp2 = new int[length];
+        dp1[0] = arr[0];
+        /**
+         * 取当前元素（包含）之前的一串连续元素之和和当前元素的较大值
+         */
+        dp1[1] = Math.max(dp1[0] + arr[1], arr[1]);
+        /**
+         * 因为必须删除一个元素，dp2[0]不存在，dp2[1]为删除第一个元素或删除第二个元素后的较大值
+         */
+        dp2[1] = Math.max(arr[0], arr[1]);
+        /**
+         * dp2[1]一定比dp1[0]大，不用比较dp1[0]
+         */
+        result = Math.max(result, dp2[1]);
+        result = Math.max(result, dp1[1]);
+        /**
+         * 遍历数组，同时保存每一轮操作后的子数组最大和
+         */
+        for (int i = 2; i < length; i++) {
+            dp1[i] = Math.max(dp1[i - 1] + arr[i], arr[i]);
+            result = Math.max(result, dp1[i]);
+            dp2[i] = Math.max(dp2[i - 1] + arr[i], dp1[i - 1]);
+            result = Math.max(result, dp2[i]);
         }
         return result;
     }
