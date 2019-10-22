06a5e2f39d872f3323891b28f3740cef82b755f8
Baltan <617640006@qq.com>
2019-10-22 10:16:38 +0800

    1163. Last Substring in Lexicographical Order —— Time Limit Exceeded

--- src/leetcode/algorithms/LastSubstring.java
+++ src/leetcode/algorithms/LastSubstring.java
@@ -10,16 +10,42 @@ public class LastSubstring {
     public static void main(String[] args) {
         System.out.println(lastSubstring("abab"));
         System.out.println(lastSubstring("leetcode"));
+        System.out.println(lastSubstring(
+                "xbylisvborylklftlkcioajuxwdhahdgezvyjbgaznzayfwsaumeccpfwamfzmkinezzwobllyxktqeibfoupcpptncggrdqbkji"
+        ));
     }
 
     public static String lastSubstring(String s) {
-        String max = "";
+        int maxHead = s.charAt(0);
+        int maxHeadIndex = 0;
         int length = s.length();
 
-        for (int i = 0; i < length; i++) {
-            String substring = s.substring(i);
-            max = substring.compareTo(max) > 0 ? substring : max;
+        for (int i = 1; i < length; i++) {
+            char c = s.charAt(i);
+
+            if (c > maxHead) {
+                maxHead = c;
+                maxHeadIndex = i;
+            } else if (c == maxHead && isGreater(s, maxHeadIndex, i)) {
+                maxHead = c;
+                maxHeadIndex = i;
+            }
         }
-        return max;
+        return s.substring(maxHeadIndex);
+    }
+
+    public static boolean isGreater(String s, int i, int j) {
+        int length = s.length();
+
+        while (i < length && j < length) {
+            if (s.charAt(j) > s.charAt(i)) {
+                return true;
+            } else if (s.charAt(j) < s.charAt(i)) {
+                return false;
+            }
+            i++;
+            j++;
+        }
+        return false;
     }
 }
