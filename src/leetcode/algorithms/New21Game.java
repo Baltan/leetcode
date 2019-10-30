5ece60d25d417c4efece420df7fcbe6aaaff3a3b
Baltan <617640006@qq.com>
2019-10-30 08:58:49 +0800

    837. New 21 Game —— Time Limit Exceeded

--- src/leetcode/algorithms/New21Game.java
+++ src/leetcode/algorithms/New21Game.java
@@ -1,5 +1,7 @@
 package leetcode.algorithms;
 
+import java.util.Arrays;
+
 /**
  * Description: 837. New 21 Game
  *
@@ -16,23 +18,34 @@ public class New21Game {
     }
 
     public static double new21Game(int N, int K, int W) {
-        double result = 0.0;
-
+        /**
+         * 参考：
+         * <a href="https://leetcode-cn.com/problems/new-21-game/solution/di-tui-gong-shi-yi-ji-xiang-xi-jie-ti-si-lu-by-wan/"></a>
+         */
         if (K == 0) {
             return 1.0;
         }
 
-        for (int i = 1; i <= W; i++) {
-            double p = 1.0 / W;
+        double result = 0;
+        double[] dp = new double[N + 1];
+        dp[1] = 1.0;
+        double p = 1.0 / W;
 
-            if (i >= K) {
-                if (i <= N) {
-                    result += p;
-                }
-            } else {
-                result += new21Game(N - i, K - i, W) * p;
+        for (int i = 1; i <= N; i++) {
+            int prevMin = Math.max(0, i - W);
+            int prevMax = Math.min(i - 1, K - 1);
+            /**
+             * 得到i的概率只与得到[prevMin,prevMax]的概率有关
+             */
+            for (int j = prevMin; j <= prevMax; j++) {
+                dp[i] += dp[j] * p;
             }
         }
+
+        for (int i = K; i <= N; i++) {
+            result += dp[i];
+        }
+        System.out.println(Arrays.toString(dp));
         return result;
     }
 }
