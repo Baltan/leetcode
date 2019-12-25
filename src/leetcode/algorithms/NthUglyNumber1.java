e1af2cacffa00e3bda3c7c8a736dd4544b75ae88
Baltan <617640006@qq.com>
2019-12-25 14:10:05 +0800

    264. Ugly Number II

--- src/leetcode/algorithms/NthUglyNumber1.java
+++ src/leetcode/algorithms/NthUglyNumber1.java
@@ -32,11 +32,26 @@ public class NthUglyNumber1 {
         list.add(1);
 
         while (list.size() < n) {
+            /**
+             * 将丑数序列中index1索引处的丑数乘以2，得到一个更大的丑数
+             */
             int i1 = list.get(index1) * 2;
+            /**
+             * 将丑数序列中index2索引处的丑数乘以3，得到一个更大的丑数
+             */
             int i2 = list.get(index2) * 3;
+            /**
+             * 将丑数序列中index3索引处的丑数乘以5，得到一个更大的丑数
+             */
             int i3 = list.get(index3) * 5;
+            /**
+             * 将以上三个丑数的最小值作为新丑数加入到丑数序列中
+             */
             int min = Math.min(i1, Math.min(i2, i3));
             list.add(min);
+            /**
+             * 将得到新丑数之前的那个丑数的索引后移一个位置
+             */
             index1 = i1 == min ? index1 + 1 : index1;
             index2 = i2 == min ? index2 + 1 : index2;
             index3 = i3 == min ? index3 + 1 : index3;
