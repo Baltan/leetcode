ee06690c81967e564286db7588847ccf1f3bdf80
Baltan <617640006@qq.com>
2020-01-10 11:23:51 +0800

    827. Making A Large Island

--- src/leetcode/algorithms/LargestIsland.java
+++ src/leetcode/algorithms/LargestIsland.java
@@ -18,28 +18,51 @@ public class LargestIsland {
         System.out.println(largestIsland(grid3));
     }
 
+    /**
+     * 当前岛屿的面积
+     */
     public static int area;
 
     public static int largestIsland(int[][] grid) {
         int result = 0;
         int rows = grid.length;
         int cols = grid[0].length;
-
+        /**
+         * 对于每一个格子，如果当前格子是海洋，假设把这个格子变成陆地后，计算包含当前格子的岛屿的面积，
+         * 获得所有新产生的岛屿的最大面积
+         */
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == 0) {
                     grid[i][j] = 1;
+                    /**
+                     * isVisited[i][j]标记格子grid[i][j]是否已经被计算过面积
+                     */
                     boolean[][] isVisited = new boolean[rows][cols];
                     area = 0;
                     dfs(grid, isVisited, i, j);
                     result = Math.max(result, area);
+                    /**
+                     * 计算完成后，需要将当前假设变成陆地的格子还原成海洋
+                     */
                     grid[i][j] = 0;
                 }
             }
         }
+        /**
+         * 如果result仍旧是初始化的值0，说明地图上没有海洋，只有陆地
+         */
         return result == 0 ? rows * cols : result;
     }
 
+    /**
+     * 计算包含grid[i][j]格子的岛屿的面积
+     *
+     * @param grid
+     * @param isVisited
+     * @param i
+     * @param j
+     */
     public static void dfs(int[][] grid, boolean[][] isVisited, int i, int j) {
         int rows = grid.length;
         int cols = grid[0].length;
