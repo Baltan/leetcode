048637bd6960df520f3c4b7c8cbe859fe9b63c60
Baltan <617640006@qq.com>
2020-01-20 14:09:20 +0800

    1234. Replace the Substring for Balanced String

--- src/leetcode/algorithms/BalancedString.java
+++ src/leetcode/algorithms/BalancedString.java
@@ -29,8 +29,14 @@ public class BalancedString {
         int eCount = 0;
         int rCount = 0;
         int length = s.length();
+        /**
+         * 最后得到的平衡字符串中每个字母应该出现的次数
+         */
         int average = length / 4;
-
+        int leftIndex = 0;
+        /**
+         * 统计字符串s中每种字母出现的次数
+         */
         for (int i = 0; i < length; i++) {
             char c = s.charAt(i);
 
@@ -44,37 +50,54 @@ public class BalancedString {
                 rCount++;
             }
         }
-
-        for (int i = 0; i < length; i++) {
-            int substringQCount = 0;
-            int substringWCount = 0;
-            int substringECount = 0;
-            int substringRCount = 0;
-            int substringLength = 0;
-            int index = i;
-
-            while (index < length && (qCount - substringQCount > average ||
-                    wCount - substringWCount > average || eCount - substringECount > average ||
-                    rCount - substringRCount > average)) {
-                char c = s.charAt(index);
+        /**
+         * 滑动窗口计算子串的最小长度
+         */
+        for (int rightIndex = 0; rightIndex < length; rightIndex++) {
+            char c = s.charAt(rightIndex);
+            /**
+             * 将窗口外的字母减掉右端点的字母
+             */
+            if (c == 'Q') {
+                qCount--;
+            } else if (c == 'W') {
+                wCount--;
+            } else if (c == 'E') {
+                eCount--;
+            } else {
+                rCount--;
+            }
+            /**
+             * 如果除去索引为[leftIndex,rightIndex]的字母，窗口外的每种字母数量都不大于average个，
+             * 则替换子串s.substring(leftIndex,rightIndex+1)即可
+             */
+            while (qCount <= average && wCount <= average && eCount <= average && rCount <= average) {
+                result = Math.min(result, rightIndex - leftIndex + 1);
+                /**
+                 * 如果当前窗口的长度已经为0了，无法再向右移动左端点了，退出当前循环后，将右端点右移
+                 * 后继续计算
+                 */
+                if (rightIndex - leftIndex + 1 == 0) {
+                    break;
+                }
+                /**
+                 * 因为接下去要左移左端点缩小窗口，所以窗口外的字母要加上当前左端点的字母
+                 */
+                c = s.charAt(leftIndex);
 
                 if (c == 'Q') {
-                    substringQCount++;
+                    qCount++;
                 } else if (c == 'W') {
-                    substringWCount++;
+                    wCount++;
                 } else if (c == 'E') {
-                    substringECount++;
+                    eCount++;
                 } else {
-                    substringRCount++;
+                    rCount++;
                 }
-
-                index++;
-                substringLength++;
-            }
-
-            if (qCount - substringQCount <= average && wCount - substringWCount <= average &&
-                    eCount - substringECount <= average && rCount - substringRCount <= average) {
-                result = Math.min(result, substringLength);
+                /**
+                 * 将左端点右移
+                 */
+                leftIndex++;
             }
         }
         return result;
