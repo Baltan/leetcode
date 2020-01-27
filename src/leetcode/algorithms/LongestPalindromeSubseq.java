b1892d62c18f731c479136f34609626259aed447
Baltan <617640006@qq.com>
2020-01-27 14:38:09 +0800

    516. Longest Palindromic Subsequence

--- src/leetcode/algorithms/LongestPalindromeSubseq.java
+++ src/leetcode/algorithms/LongestPalindromeSubseq.java
@@ -5,6 +5,7 @@ package leetcode.algorithms;
  *
  * @author Baltan
  * @date 2019-10-04 11:54
+ * @see MinInsertions
  */
 public class LongestPalindromeSubseq {
     public static void main(String[] args) {
