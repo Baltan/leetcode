aa025b1a6d614bb6f104477fd3e8d7131faa52e3
Baltan <617640006@qq.com>
2020-02-15 13:24:58 +0800

    122. Best Time to Buy and Sell Stock II

--- src/leetcode/algorithms/MaxProfit1.java
+++ src/leetcode/algorithms/MaxProfit1.java
@@ -5,15 +5,27 @@ package leetcode.algorithms;
  *
  * @author Baltan
  * @date 2017/11/17 14:04
+ * @see MaxProfit
+ * @see MaxProfit2
+ * @see MaxProfit3
+ * @see MaxProfit4
+ * @see MaxProfit5
  */
 public class MaxProfit1 {
     public static void main(String[] args) {
-
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(maxProfit(new int[]{1, 2, 3, 4, 5}));
+        System.out.println(maxProfit(new int[]{7, 6, 4, 3, 1}));
     }
 
     public static int maxProfit(int[] prices) {
         int profit = 0;
+
         for (int i = 0; i < prices.length - 1; i++) {
+            /**
+             * 相邻的两天中，如果后一天的股票价格高于前一天，就应该在这两天完成一次交易获得更多的
+             * 收益
+             */
             if (prices[i] < prices[i + 1]) {
                 profit += (prices[i + 1] - prices[i]);
             }
