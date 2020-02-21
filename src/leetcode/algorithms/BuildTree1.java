48c0a0912b1f26b64d9dbdaffaeb524ee6db6f08
Baltan <617640006@qq.com>
2020-02-21 14:44:41 +0800

    106. Construct Binary Tree from Inorder and Postorder Traversal

--- src/leetcode/algorithms/BuildTree1.java
+++ src/leetcode/algorithms/BuildTree1.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
  * @author Baltan
  * @date 2020-01-31 11:41
  * @see BuildTree
+ * @see ConstructFromPrePost
  */
 public class BuildTree1 {
     public static void main(String[] args) {
