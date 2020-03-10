52d1b302ad059f1af7e4beb3d9ecac8b1ee05ae2
Baltan <617640006@qq.com>
2020-03-11 00:49:33 +0800

    1013. Partition Array Into Three Parts With Equal Sum

--- src/leetcode/algorithms/CanThreePartsEqualSum.java
+++ src/leetcode/algorithms/CanThreePartsEqualSum.java
@@ -1,7 +1,5 @@
 package leetcode.algorithms;
 
-import java.util.Arrays;
-
 /**
  * Description: 1013. Partition Array Into Three Parts With Equal Sum
  *
@@ -17,50 +15,49 @@ public class CanThreePartsEqualSum {
     }
 
     public static boolean canThreePartsEqualSum(int[] A) {
-        if (A == null || A.length < 3) {
-            return false;
-        }
+        /**
+         * 数组A中所有数字的和
+         */
+        int sum = 0;
 
-        int sum = Arrays.stream(A).sum();
+        for (int value : A) {
+            sum += value;
+        }
 
         if (sum % 3 != 0) {
             return false;
         }
-
+        /**
+         * 分成3组后，每组中元素的和
+         */
+        int average = sum / 3;
+        /**
+         * 当前分割的组数
+         */
+        int groupCount = 0;
+        /**
+         * 当前一组中所有元素的和
+         */
+        int currentSum = 0;
         int length = A.length;
-        int partitionSum = sum / 3;
-        int index = 0;
-        int sum1 = A[index++];
 
-        while (sum1 != partitionSum) {
-            if (index == length) {
-                return false;
+        for (int i = 0; i < length; i++) {
+            currentSum += A[i];
+            /**
+             * 如果当前这组数字的和为average，则说明得到了一个目标分组，继续尝试下一个分组
+             */
+            if (currentSum == average) {
+                groupCount++;
+                currentSum = 0;
+                /**
+                 * 如果已经得到了2个分组并且还有剩余的数字，则剩余的数字的和必然是average，此时说明可以将数
+                 * 组分成和相等的三个部分，返回true
+                 */
+                if (groupCount == 2 && i < length - 1) {
+                    return true;
+                }
             }
-            sum1 += A[index++];
         }
-
-        if (index == length) {
-            return false;
-        }
-
-        int sum2 = A[index++];
-
-        while (sum2 != partitionSum) {
-            if (index == length) {
-                return false;
-            }
-            sum2 += A[index++];
-        }
-
-        if (index == length) {
-            return false;
-        }
-
-        int sum3 = 0;
-
-        for (int i = index; i < length; i++) {
-            sum3 += A[i];
-        }
-        return sum3 == partitionSum;
+        return false;
     }
 }
