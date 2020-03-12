a8d02523ed631bf2af827dd96543daefbcb3b97f
Baltan <617640006@qq.com>
2020-03-12 15:19:35 +0800

    2. Add Two Numbers

--- src/leetcode/algorithms/AddTwoNumbers.java
+++ src/leetcode/algorithms/AddTwoNumbers.java
@@ -1,6 +1,7 @@
 package leetcode.algorithms;
 
 import leetcode.entity.ListNode;
+import leetcode.util.ListNodeUtils;
 import leetcode.util.OutputUtils;
 
 /**
@@ -11,57 +12,58 @@ import leetcode.util.OutputUtils;
  */
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        ListNode listNode11 = new ListNode(2);
-        ListNode listNode12 = new ListNode(4);
-        ListNode listNode13 = new ListNode(3);
-        listNode11.next = listNode12;
-        listNode12.next = listNode13;
-        ListNode listNode21 = new ListNode(5);
-        ListNode listNode22 = new ListNode(6);
-        ListNode listNode23 = new ListNode(4);
-        listNode21.next = listNode22;
-        listNode22.next = listNode23;
-        OutputUtils.printListNode(addTwoNumbers(listNode11, listNode21));
+        ListNode listNode11 = ListNodeUtils.arrayToListNode(new int[]{2, 4, 3});
+        ListNode listNode12 = ListNodeUtils.arrayToListNode(new int[]{5, 6, 4});
+        OutputUtils.printListNode(addTwoNumbers(listNode11, listNode12));
 
-        ListNode listNode31 = new ListNode(1);
-        ListNode listNode32 = new ListNode(2);
-        ListNode listNode33 = new ListNode(1);
-        listNode31.next = listNode32;
-        listNode32.next = listNode33;
-        ListNode listNode41 = new ListNode(9);
-        ListNode listNode42 = new ListNode(9);
-        ListNode listNode43 = new ListNode(9);
-        listNode41.next = listNode42;
-        listNode42.next = listNode43;
-        OutputUtils.printListNode(addTwoNumbers(listNode31, listNode41));
+        ListNode listNode21 = ListNodeUtils.arrayToListNode(new int[]{1, 2, 1});
+        ListNode listNode22 = ListNodeUtils.arrayToListNode(new int[]{9, 9, 9});
+        OutputUtils.printListNode(addTwoNumbers(listNode21, listNode22));
+
+        ListNode listNode31 = ListNodeUtils.arrayToListNode(new int[]{7, 2, 4, 3});
+        ListNode listNode32 = ListNodeUtils.arrayToListNode(new int[]{5, 6, 4});
+        OutputUtils.printListNode(addTwoNumbers(listNode31, listNode32));
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int val = l1.val + l2.val;
-        ListNode l3 = new ListNode(val >= 10 ? val - 10 : val);
-        ListNode temp = l3;
-        int carryBit = val >= 10 ? 1 : 0;
-        while (l1.next != null || l2.next != null) {
-            if (l1.next != null && l2.next != null) {
-                val = l1.next.val + l2.next.val + carryBit;
+        /**
+         * 进位
+         */
+        int carryBit = 0;
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+
+        while (l1 != null || l2 != null) {
+            int sum = carryBit;
+
+            if (l1 != null) {
+                sum += l1.val;
                 l1 = l1.next;
-                l2 = l2.next;
-            } else if (l1.next != null && l2.next == null) {
-                val = l1.next.val + carryBit;
-                l1 = l1.next;
-            } else {
-                val = l2.next.val + carryBit;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
                 l2 = l2.next;
             }
-            ListNode currentListNode = new ListNode(val >= 10 ? val - 10 : val);
-            carryBit = val >= 10 ? 1 : 0;
-            temp.next = currentListNode;
-            temp = currentListNode;
+            /**
+             * 判断是否有进位
+             */
+            if (sum >= 10) {
+                sum -= 10;
+                carryBit = 1;
+            } else {
+                carryBit = 0;
+            }
+
+            curr.next = new ListNode(sum);
+            curr = curr.next;
         }
+        /**
+         * 如果链表最后的数字加完后还有进位，要将进位追加到链表最后
+         */
         if (carryBit == 1) {
-            ListNode currentListNode = new ListNode(1);
-            temp.next = currentListNode;
+            curr.next = new ListNode(carryBit);
         }
-        return l3;
+        return dummy.next;
     }
 }
