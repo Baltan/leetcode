3ec07607151aca305d52813b10e1ffea18749d33
Baltan <617640006@qq.com>
2020-03-12 16:38:29 +0800

    面试题 01.07. 旋转矩阵

---
+++ src/leetcode/interview/Rotate.java
@@ -0,0 +1,41 @@
+package leetcode.interview;
+
+import leetcode.util.OutputUtils;
+
+/**
+ * Description: 面试题 01.07. 旋转矩阵
+ *
+ * @author Baltan
+ * @date 2018/9/14 10:15
+ * @see leetcode.algorithms.Rotate1
+ */
+public class Rotate {
+    public static void main(String[] args) {
+        int[][] matrix1 = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        rotate(matrix1);
+
+        int[][] matrix2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotate(matrix2);
+
+        int[][] matrix3 = {{1, 2}, {3, 4}};
+        rotate(matrix3);
+
+        int[][] matrix4 = {{1}};
+        rotate(matrix4);
+    }
+
+    public static void rotate(int[][] matrix) {
+        int length = matrix.length;
+
+        for (int i = 0; i < length / 2; i++) {
+            for (int j = i; j < length - i - 1; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[length - 1 - j][i];
+                matrix[length - 1 - j][i] = matrix[length - 1 - i][length - 1 - j];
+                matrix[length - 1 - i][length - 1 - j] = matrix[j][length - 1 - i];
+                matrix[j][length - 1 - i] = temp;
+            }
+        }
+        OutputUtils.print2DIntegerArray(matrix);
+    }
+}
