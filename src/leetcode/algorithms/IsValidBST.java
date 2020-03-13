4196e9b84fc7cbbe767c535b60f646e3563aaa7b
Baltan <617640006@qq.com>
2020-03-13 18:00:16 +0800

    98. Validate Binary Search Tree

--- src/leetcode/algorithms/IsValidBST.java
+++ src/leetcode/algorithms/IsValidBST.java
@@ -1,6 +1,7 @@
 package leetcode.algorithms;
 
 import leetcode.entity.TreeNode;
+import leetcode.util.BinaryTreeUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,40 +11,27 @@ import java.util.List;
  *
  * @author Baltan
  * @date 2019-05-23 09:40
+ * @see leetcode.interview.IsValidBST
  */
 public class IsValidBST {
     public static void main(String[] args) {
-        TreeNode treeNode11 = new TreeNode(2);
-        TreeNode treeNode12 = new TreeNode(1);
-        TreeNode treeNode13 = new TreeNode(3);
-        treeNode11.left = treeNode12;
-        treeNode11.right = treeNode13;
-        System.out.println(isValidBST(treeNode11));
+        TreeNode root1 = BinaryTreeUtils.arrayToBinaryTree(new Integer[]{2, 1, 3}, 0);
+        System.out.println(isValidBST(root1));
 
-        TreeNode treeNode21 = new TreeNode(5);
-        TreeNode treeNode22 = new TreeNode(1);
-        TreeNode treeNode23 = new TreeNode(4);
-        TreeNode treeNode24 = new TreeNode(3);
-        TreeNode treeNode25 = new TreeNode(6);
-        treeNode21.left = treeNode22;
-        treeNode21.right = treeNode23;
-        treeNode23.left = treeNode24;
-        treeNode23.right = treeNode25;
-        System.out.println(isValidBST(treeNode21));
+        TreeNode root2 = BinaryTreeUtils.arrayToBinaryTree(new Integer[]{5, 1, 4, null, null, 3, 6}, 0);
+        System.out.println(isValidBST(root2));
 
-        TreeNode treeNode31 = new TreeNode(1);
-        System.out.println(isValidBST(treeNode31));
+        TreeNode root3 = BinaryTreeUtils.arrayToBinaryTree(new Integer[]{1}, 0);
+        System.out.println(isValidBST(root3));
 
-        TreeNode treeNode41 = new TreeNode(5);
-        TreeNode treeNode42 = new TreeNode(2);
-        TreeNode treeNode43 = new TreeNode(8);
-        TreeNode treeNode44 = new TreeNode(3);
-        TreeNode treeNode45 = new TreeNode(9);
-        treeNode41.left = treeNode42;
-        treeNode41.right = treeNode43;
-        treeNode43.left = treeNode44;
-        treeNode43.right = treeNode45;
-        System.out.println(isValidBST(treeNode41));
+        TreeNode root4 = BinaryTreeUtils.arrayToBinaryTree(new Integer[]{5, 2, 8, 3, null, null, 9}, 0);
+        System.out.println(isValidBST(root4));
+
+        TreeNode root5 = BinaryTreeUtils.arrayToBinaryTree(new Integer[]{1, null, 1}, 0);
+        System.out.println(isValidBST(root5));
+
+        TreeNode root6 = BinaryTreeUtils.arrayToBinaryTree(new Integer[]{1, null, 1}, 0);
+        System.out.println(isValidBST(root6));
     }
 
     public static boolean isValidBST(TreeNode root) {
@@ -53,7 +41,9 @@ public class IsValidBST {
 
         List<Integer> result = inOrder(root);
         int size = result.size();
-
+        /**
+         * 二叉搜索树的中序遍历结果是严格递增的
+         */
         for (int i = 1; i < size; i++) {
             if (result.get(i) <= result.get(i - 1)) {
                 return false;
@@ -62,6 +52,12 @@ public class IsValidBST {
         return true;
     }
 
+    /**
+     * 中序遍历二叉树
+     *
+     * @param root
+     * @return
+     */
     public static List<Integer> inOrder(TreeNode root) {
         List<Integer> result = new ArrayList<>();
 
