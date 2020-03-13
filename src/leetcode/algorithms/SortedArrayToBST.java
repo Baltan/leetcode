cbc39de4f7523defb52861e263f0981292c34012
Baltan <617640006@qq.com>
2020-03-13 17:18:37 +0800

    108. Convert Sorted Array to Binary Search Tree

--- src/leetcode/algorithms/SortedArrayToBST.java
+++ src/leetcode/algorithms/SortedArrayToBST.java
@@ -1,36 +1,41 @@
 package leetcode.algorithms;
 
 import leetcode.entity.TreeNode;
-import leetcode.util.OutputUtils;
-
-import java.util.Arrays;
 
 /**
  * Description: 108. Convert Sorted Array to Binary Search Tree
  *
  * @author Baltan
  * @date 2019-02-26 09:57
+ * @see leetcode.interview.SortedArrayToBST
  */
 public class SortedArrayToBST {
     public static void main(String[] args) {
-        int[] arr1 = {-10, -3, 0, 5, 9};
-        TreeNode node1 = sortedArrayToBST(arr1);
-        OutputUtils.printPreOrderBinaryTree(node1);
+        int[] nums1 = {-10, -3, 0, 5, 9};
+        System.out.println(sortedArrayToBST(nums1));
     }
 
     public static TreeNode sortedArrayToBST(int[] nums) {
-        if (nums == null || nums.length == 0) {
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    public static TreeNode sortedArrayToBST(int[] nums, int start, int end) {
+        if (nums == null || nums.length == 0 || start > end || start < 0 || end >= nums.length) {
             return null;
         }
-
-        int length = nums.length;
-        int midIndex = length / 2;
-
-        TreeNode root = new TreeNode(nums[midIndex]);
-
-        root.left = sortedArrayToBST(Arrays.copyOfRange(nums, 0, midIndex));
-        root.right = sortedArrayToBST(Arrays.copyOfRange(nums, midIndex + 1, length));
-
+        /**
+         * 取nums中间的数字作为根节点root的值
+         */
+        int mid = (start + end) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        /**
+         * 将nums左半边的数字递归构造二叉搜索树作为root的左子树
+         */
+        root.left = sortedArrayToBST(nums, start, mid - 1);
+        /**
+         * 将nums右半边的数字递归构造二叉搜索树作为root的右子树
+         */
+        root.right = sortedArrayToBST(nums, mid + 1, end);
         return root;
     }
 }
