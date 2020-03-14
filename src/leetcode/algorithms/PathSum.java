d9bb9c22e13bd2a2429ee88272d5afc84ae62e6f
Baltan <617640006@qq.com>
2020-03-14 14:17:41 +0800

    437. Path Sum III

--- src/leetcode/algorithms/PathSum.java
+++ src/leetcode/algorithms/PathSum.java
@@ -1,75 +1,74 @@
 package leetcode.algorithms;
 
 import leetcode.entity.TreeNode;
+import leetcode.util.BinaryTreeUtils;
 
 /**
  * Description: 437. Path Sum III
  *
  * @author Baltan
  * @date 2018/8/9 09:26
+ * @see leetcode.interview.PathSum
+ * @see PathSum1
  */
 public class PathSum {
     public static void main(String[] args) {
-        TreeNode root1 = new TreeNode(10);
-        TreeNode node1 = new TreeNode(5);
-        TreeNode node2 = new TreeNode(-3);
-        TreeNode node3 = new TreeNode(3);
-        TreeNode node4 = new TreeNode(2);
-        TreeNode node5 = new TreeNode(11);
-        TreeNode node6 = new TreeNode(3);
-        TreeNode node7 = new TreeNode(-2);
-        TreeNode node8 = new TreeNode(1);
-        root1.left = node1;
-        root1.right = node2;
-        node1.left = node3;
-        node1.right = node4;
-        node2.right = node5;
-        node3.left = node6;
-        node3.right = node7;
-        node4.right = node8;
+        TreeNode root1 = BinaryTreeUtils
+                .arrayToBinaryTree(new Integer[]{10, 5, -3, 3, 2, null, 11, 3, -2, null, 1}, 0);
         System.out.println(pathSum(root1, 8));
 
-        TreeNode root2 = new TreeNode(1);
-        TreeNode node9 = new TreeNode(2);
-        TreeNode node10 = new TreeNode(3);
-        TreeNode node11 = new TreeNode(4);
-        TreeNode node12 = new TreeNode(5);
-        root2.right = node9;
-        node9.right = node10;
-        node10.right = node11;
-        node11.right = node12;
+        TreeNode root2 =
+                BinaryTreeUtils.arrayToBinaryTree(new Integer[]{1, 2, null, 3, null, null, null, 4, null,
+                        null, null, null, null, null, null, 5}, 0);
         System.out.println(pathSum(root2, 3));
     }
 
     public static int pathSum(TreeNode root, int sum) {
-        int num = 0;
+        int result = 0;
+
         if (root == null) {
-            return num;
+            return result;
         }
-        num += dfs(root, sum);
-        if (root.left != null) {
-            num += pathSum(root.left, sum);
-        }
-        if (root.right != null) {
-            num += pathSum(root.right, sum);
-        }
-        return num;
+        /**
+         * 从根节点开始，方向向下的路径总和为sum的路径条数
+         */
+        result += dfs(root, sum);
+        /**
+         * 在根节点的左子树中，从左子树根节点开始，方向向下的路径总和为sum的路径条数
+         */
+        result += pathSum(root.left, sum);
+        /**
+         * 在根节点的右子树中，从右子树根节点开始，方向向下的路径总和为sum的路径条数
+         */
+        result += pathSum(root.right, sum);
+        return result;
     }
 
+    /**
+     * 从根节点开始，方向向下的路径总和为sum的路径条数
+     *
+     * @param root
+     * @param sum
+     * @return
+     */
     public static int dfs(TreeNode root, int sum) {
-        int num = 0;
+        int result = 0;
+
         if (root == null) {
-            return num;
+            return result;
         }
+
         if (root.val == sum) {
-            num++;
+            result++;
         }
-        if (root.left != null) {
-            num += dfs(root.left, sum - root.val);
-        }
-        if (root.right != null) {
-            num += dfs(root.right, sum - root.val);
-        }
-        return num;
+        /**
+         * 在root的左子树中查找路径总和为sum-root.val的路径条数
+         */
+        result += dfs(root.left, sum - root.val);
+        /**
+         * 在root的右子树中查找路径总和为sum-root.val的路径条数
+         */
+        result += dfs(root.right, sum - root.val);
+        return result;
     }
 }
