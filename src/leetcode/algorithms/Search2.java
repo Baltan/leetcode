2cbcd3f17e424e8399a9080cd841189c557e753a
Baltan <617640006@qq.com>
2020-03-18 14:17:43 +0800

    81. Search in Rotated Sorted Array II

--- src/leetcode/algorithms/Search2.java
+++ src/leetcode/algorithms/Search2.java
@@ -5,6 +5,9 @@ package leetcode.algorithms;
  *
  * @author Baltan
  * @date 2019-05-18 17:30
+ * @see Search
+ * @see Search1
+ * @see leetcode.interview.Search
  */
 public class Search2 {
     public static void main(String[] args) {
@@ -19,6 +22,7 @@ public class Search2 {
     public static boolean search(int[] nums, int target) {
         int lo = 0;
         int hi = nums.length - 1;
+
         while (lo <= hi) {
             int mid = (lo + hi) / 2;
 
@@ -27,12 +31,20 @@ public class Search2 {
             } else if (nums[lo] == nums[mid]) {
                 lo++;
             } else if (nums[mid] >= nums[lo]) {
+                /**
+                 * 如果nums[mid]>=nums[lo]，则前半段是单调递增的。如果nums[lo]<=target<nums[mid]，则
+                 * 前半段中可能会存在target，在前半段中继续二分搜索，否则在后半段中继续二分搜索
+                 */
                 if (nums[mid] > target && nums[lo] <= target) {
                     hi = mid - 1;
                 } else {
                     lo = mid + 1;
                 }
             } else {
+                /**
+                 * 如果nums[mid]<nums[lo]，则后半段是单调递增的。如果nums[mid]<target<=nums[hi]，则后
+                 * 半段中可能会存在target，在后半段中继续二分搜索，否则在前半段中继续二分搜索
+                 */
                 if (nums[mid] < target && nums[hi] >= target) {
                     lo = mid + 1;
                 } else {
