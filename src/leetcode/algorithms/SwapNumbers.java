3aee9bc8d65dd3b23711355142d370a0babc79aa
Baltan <617640006@qq.com>
2020-03-19 16:05:56 +0800

    面试题 16.01. 交换数字

---
+++ src/leetcode/algorithms/SwapNumbers.java
@@ -0,0 +1,22 @@
+package leetcode.algorithms;
+
+import leetcode.util.OutputUtils;
+
+/**
+ * Description: 面试题 16.01. 交换数字
+ *
+ * @author Baltan
+ * @date 2020-03-19 16:03
+ */
+public class SwapNumbers {
+    public static void main(String[] args) {
+        OutputUtils.print1DIntegerArray(swapNumbers(new int[]{1, 2}));
+    }
+
+    public static int[] swapNumbers(int[] numbers) {
+        numbers[0] = numbers[0] ^ numbers[1];
+        numbers[1] = numbers[0] ^ numbers[1];
+        numbers[0] = numbers[0] ^ numbers[1];
+        return numbers;
+    }
+}
