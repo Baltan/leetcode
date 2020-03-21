05ea57d3047fff2ec4638acc6a9a651af58f3604
Baltan <617640006@qq.com>
2020-03-21 12:43:54 +0800

    260. Single Number III

--- src/leetcode/algorithms/SingleNumber3.java
+++ src/leetcode/algorithms/SingleNumber3.java
@@ -20,6 +20,13 @@ public class SingleNumber3 {
         OutputUtils.print1DIntegerArray(singleNumber(new int[]{2, 1, 2, 3, 6, 3, 7, 6}));
     }
 
+    /**
+     * 参考：
+     * <a href="https://leetcode-cn.com/problems/single-number-iii/solution/lowbitpan-duan-dang-qian-shu-zi-shu-yu-na-yi-ge-zu/"></a>
+     *
+     * @param nums
+     * @return
+     */
     public static int[] singleNumber(int[] nums) {
         if (nums == null) {
             return null;
@@ -32,10 +39,13 @@ public class SingleNumber3 {
         }
 
         /**
-         * 保留value二进制表示的最右边的1，其他变为0
+         * 保留value二进制表示的最右边的1，其他变为0，只出现1次的两个数在这位上肯定不同，否则在这位上会
+         * 有偶数个1，偶数个1相互异或后的值应该为0
          */
         value &= -value;
-
+        /**
+         * 将这位为0的所有数相互异或，将这位为1的所有数相互异或，可以得到两个只出现1次的数
+         */
         for (int num : nums) {
             if ((value & num) == 0) {
                 result[0] ^= num;
