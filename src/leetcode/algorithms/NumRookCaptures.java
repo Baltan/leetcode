eae25a14fa2308d4f9a5ee281b6fb35ab036846f
Baltan <617640006@qq.com>
2020-03-26 00:11:15 +0800

    999. Available Captures for Rook

--- src/leetcode/algorithms/NumRookCaptures.java
+++ src/leetcode/algorithms/NumRookCaptures.java
@@ -29,44 +29,58 @@ public class NumRookCaptures {
 
     public static int numRookCaptures(char[][] board) {
         int result = 0;
-        boolean[][] books = new boolean[8][8];
 
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if (board[i][j] == 'R') {
                     int k1 = j - 1;
+                    int k2 = j + 1;
+                    int k3 = i - 1;
+                    int k4 = i + 1;
+                    /**
+                     * 车一直向左走
+                     */
                     while (k1 >= 0 && board[i][k1] == '.') {
                         k1--;
                     }
-                    if (k1 >= 0 && board[i][k1] == 'p' && !books[i][k1]) {
+
+                    if (k1 >= 0 && board[i][k1] == 'p') {
                         result++;
-                        books[i][k1] = true;
                     }
-                    int k2 = j + 1;
+                    /**
+                     * 车一直向右走
+                     */
                     while (k2 < 8 && board[i][k2] == '.') {
                         k2++;
                     }
-                    if (k2 < 8 && board[i][k2] == 'p' && !books[i][k2]) {
+
+                    if (k2 < 8 && board[i][k2] == 'p') {
                         result++;
-                        books[i][k2] = true;
                     }
-                    int k3 = i - 1;
+                    /**
+                     * 车一直向上走
+                     */
                     while (k3 >= 0 && board[k3][j] == '.') {
                         k3--;
                     }
-                    if (k3 >= 0 && board[k3][j] == 'p' && !books[k3][j]) {
-                        result++;
-                        books[k3][j] = true;
-                    }
 
-                    int k4 = i + 1;
+                    if (k3 >= 0 && board[k3][j] == 'p') {
+                        result++;
+                    }
+                    /**
+                     * 车一直向下走
+                     */
                     while (k4 < 8 && board[k4][j] == '.') {
                         k4++;
                     }
-                    if (k4 < 8 && board[k4][j] == 'p' && !books[k4][j]) {
+
+                    if (k4 < 8 && board[k4][j] == 'p') {
                         result++;
-                        books[k4][j] = true;
                     }
+                    /**
+                     * 因为棋盘上只有一个"车"，找到就可以直接返回结果了
+                     */
+                    return result;
                 }
             }
         }
