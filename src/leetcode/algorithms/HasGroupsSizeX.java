88c5c57d1c9a84a7716e26c1e0689926a81a4e12
Baltan <617640006@qq.com>
2020-03-27 00:27:49 +0800

    914. X of a Kind in a Deck of Cards

--- src/leetcode/algorithms/HasGroupsSizeX.java
+++ src/leetcode/algorithms/HasGroupsSizeX.java
@@ -8,6 +8,7 @@ import java.util.Map;
  *
  * @author Baltan
  * @date 2019-03-17 08:58
+ * @see HasGroupsSizeX1
  */
 public class HasGroupsSizeX {
     public static void main(String[] args) {
@@ -20,12 +21,17 @@ public class HasGroupsSizeX {
     }
 
     public static boolean hasGroupsSizeX(int[] deck) {
+        /**
+         * i -> deck中i出现的次数
+         */
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int num : deck) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-
+        /**
+         * deck中出现次数最少的那个元素的出现次数
+         */
         int minNum = Integer.MAX_VALUE;
 
         for (int value : map.values()) {
@@ -35,15 +41,22 @@ public class HasGroupsSizeX {
         if (minNum == 1) {
             return false;
         }
-
+        /**
+         * 从最小可能值2开始逐一判断是否是deck中所有出现过的元素的出现次数的最大公约数
+         */
         for (int i = 2; i <= minNum; i++) {
+            /**
+             * 标记i是否是deck中所有出现过的元素的出现次数的最大公约数
+             */
             boolean flag = true;
+
             for (int value : map.values()) {
                 if (value % i != 0) {
                     flag = false;
                     break;
                 }
             }
+
             if (flag) {
                 return true;
             }
