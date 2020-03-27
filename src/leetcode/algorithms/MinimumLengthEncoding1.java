31a3c8b33e5b7e3ff2c6e75e99f9e14b794fde71
Baltan <617640006@qq.com>
2020-03-28 00:21:11 +0800

    820. Short Encoding of Words

--- src/leetcode/algorithms/MinimumLengthEncoding1.java
+++ src/leetcode/algorithms/MinimumLengthEncoding1.java
@@ -1,15 +1,17 @@
 package leetcode.algorithms;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Description: 820. Short Encoding of Words
  *
  * @author Baltan
  * @date 2020-02-19 13:14
- * @see MinimumLengthEncoding1
+ * @see MinimumLengthEncoding
  */
-public class MinimumLengthEncoding {
+public class MinimumLengthEncoding1 {
     public static void main(String[] args) {
         System.out.println(minimumLengthEncoding(new String[]{"time", "me", "bell"}));
     }
@@ -21,17 +23,20 @@ public class MinimumLengthEncoding {
          */
         Arrays.sort(words, (x, y) -> y.length() - x.length());
         StringBuilder builder = new StringBuilder();
+        /**
+         * 已经加入编码的单词
+         */
+        Set<String> usedStrings = new HashSet<>();
 
+        outer:
         for (String word : words) {
-            /**
-             * 如果当前索引字符串中已经包含了word+"#"，说明这个单词可以在当前索引字符串中直接定
-             * 位索引，否则就要在索引字符串中追加这个单词
-             */
-            if (builder.indexOf(word + "#") != -1) {
-                continue;
-            } else {
-                builder.append(word + "#");
+            for (String usedString : usedStrings) {
+                if (usedString.endsWith(word)) {
+                    continue outer;
+                }
             }
+            usedStrings.add(word);
+            builder.append(word).append("#");
         }
         return builder.length();
     }
