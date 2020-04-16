81ef9ef951217e9f581c3a4072a4c47c958d6718
Baltan <617640006@qq.com>
2020-04-16 20:57:10 +0800

    56. Merge Intervals

--- src/leetcode/algorithms/Merge1.java
+++ src/leetcode/algorithms/Merge1.java
@@ -30,19 +30,30 @@ public class Merge1 {
 
     public static List<Interval> merge(List<Interval> intervals) {
         List<Interval> res = new ArrayList<>();
+
         if (intervals == null || intervals.size() == 0) {
             return res;
         }
+        /**
+         * 如果只有一个区间，则不需要合并，直接返回该区间即可
+         */
         if (intervals.size() == 1) {
             return intervals;
         }
-
+        /**
+         * 将所有区间按照区间起点升序排雷
+         */
         Collections
                 .sort(intervals, Comparator.comparingInt((Interval interval) -> interval.start));
         res.add(intervals.get(0));
+
         for (int i = 1; i < intervals.size(); i++) {
             Interval interval1 = res.get(res.size() - 1);
             Interval interval2 = intervals.get(i);
+            /**
+             * 如果当前区间的起点和result中的最后一个区间重合，说明这两个区间可以继续合并，合并后区
+             * 间的终点即为两个区间终点的较大值；否则将当前区间加入到result中
+             */
             if (interval2.start <= interval1.end) {
                 interval1.end = Math.max(interval1.end, interval2.end);
             } else {
--- src/leetcode/algorithms/Merge2.java
+++ src/leetcode/algorithms/Merge2.java
@@ -24,31 +24,43 @@ public class Merge2 {
     }
 
     public static int[][] merge(int[][] intervals) {
-        List<int[]> list = new ArrayList<>();
+        List<int[]> intervalList = new ArrayList<>();
 
         if (intervals == null || intervals.length == 0) {
             return new int[][]{};
         }
+        /**
+         * 如果只有一个区间，则不需要合并，直接返回该区间即可
+         */
         if (intervals.length == 1) {
             return intervals;
         }
-
+        /**
+         * 将所有区间按照区间起点升序排雷
+         */
         Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
-        list.add(intervals[0]);
+        intervalList.add(intervals[0]);
+
         for (int i = 1; i < intervals.length; i++) {
-            int[] interval1 = list.get(list.size() - 1);
+            int[] interval1 = intervalList.get(intervalList.size() - 1);
             int[] interval2 = intervals[i];
+            /**
+             * 如果当前区间的起点和intervalList中的最后一个区间重合，说明这两个区间可以继续合并，
+             * 合并后区间的终点即为两个区间终点的较大值；否则将当前区间加入到intervalList中
+             */
             if (interval2[0] <= interval1[1]) {
                 interval1[1] = Math.max(interval1[1], interval2[1]);
             } else {
-                list.add(intervals[i]);
+                intervalList.add(intervals[i]);
             }
         }
+        /**
+         * 将intervalList转化为数组形式返回
+         */
+        int[][] result = new int[intervalList.size()][2];
 
-        int[][] result = new int[list.size()][2];
-
-        for (int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i);
+        for (int i = 0; i < intervalList.size(); i++) {
+            result[i] = intervalList.get(i);
         }
         return result;
     }
