cd10ed005a3c33da1deb015392ffe11ae35f7661
Baltan <617640006@qq.com>
2020-04-18 11:01:00 +0800

    11. Container With Most Water

--- src/leetcode/algorithms/MaxArea.java
+++ src/leetcode/algorithms/MaxArea.java
@@ -15,10 +15,19 @@ public class MaxArea {
         int area = 0;
         int firstIndex = 0;
         int lastIndex = height.length - 1;
+        /**
+         * 双指针
+         */
         while (firstIndex < lastIndex) {
             int width = Math.min(height[firstIndex], height[lastIndex]);
             int length = lastIndex - firstIndex;
+            /**
+             * 左右指针所指向的两条垂线和x轴围成的面积
+             */
             area = Math.max(area, width * length);
+            /**
+             * 如果左指针指向的垂线较短，就右移左指针，否则就左移右指针
+             */
             if (height[firstIndex] < height[lastIndex]) {
                 firstIndex++;
             } else {
