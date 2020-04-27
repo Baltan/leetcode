da78909924e161e775b56e7315c89653e1b8cb52
Baltan <617640006@qq.com>
2020-04-27 17:00:12 +0800

    23. Merge k Sorted Lists

--- src/leetcode/algorithms/MergeKLists.java
+++ src/leetcode/algorithms/MergeKLists.java
@@ -1,6 +1,7 @@
 package leetcode.algorithms;
 
 import leetcode.entity.ListNode;
+import leetcode.util.ListNodeUtils;
 import leetcode.util.OutputUtils;
 
 /**
@@ -8,22 +9,13 @@ import leetcode.util.OutputUtils;
  *
  * @author Baltan
  * @date 2018/8/31 14:17
+ * @see MergeKLists1
  */
 public class MergeKLists {
     public static void main(String[] args) {
-        ListNode head1 = new ListNode(1);
-        ListNode node1 = new ListNode(4);
-        ListNode node2 = new ListNode(5);
-        head1.next = node1;
-        node1.next = node2;
-        ListNode head2 = new ListNode(1);
-        ListNode node3 = new ListNode(3);
-        ListNode node4 = new ListNode(4);
-        head2.next = node3;
-        node3.next = node4;
-        ListNode head3 = new ListNode(2);
-        ListNode node5 = new ListNode(6);
-        head3.next = node5;
+        ListNode head1 = ListNodeUtils.arrayToListNode(new int[]{1, 4, 5});
+        ListNode head2 = ListNodeUtils.arrayToListNode(new int[]{1, 3, 4});
+        ListNode head3 = ListNodeUtils.arrayToListNode(new int[]{1, 2, 6});
         OutputUtils.printListNode(mergeKLists(new ListNode[]{head1, head2, head3}));
     }
 
@@ -35,12 +27,22 @@ public class MergeKLists {
         }
         int length = lists.length;
         ListNode res = merge2Lists(lists[0], lists[1]);
+        /**
+         * 逐一按顺序合并lists中的各个链表
+         */
         for (int i = 2; i < length; i++) {
             res = merge2Lists(res, lists[i]);
         }
         return res;
     }
 
+    /**
+     * 合并两个链表
+     *
+     * @param listNode1
+     * @param listNode2
+     * @return
+     */
     public static ListNode merge2Lists(ListNode listNode1, ListNode listNode2) {
         if (listNode1 == null) {
             return listNode2;
@@ -49,11 +51,17 @@ public class MergeKLists {
         } else {
             if (listNode1.val < listNode2.val) {
                 ListNode head = new ListNode(listNode1.val);
+                /**
+                 * 递归合并listNode1剩余部分链表和listNode2
+                 */
                 ListNode tail = merge2Lists(listNode1.next, listNode2);
                 head.next = tail;
                 return head;
             } else {
                 ListNode head = new ListNode(listNode2.val);
+                /**
+                 * 递归合并listNode2剩余部分链表和listNode1
+                 */
                 ListNode tail = merge2Lists(listNode1, listNode2.next);
                 head.next = tail;
                 return head;
