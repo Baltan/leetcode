8fed9bf3f82d997c9c3a4329fa1390f39fa9327a
Baltan <617640006@qq.com>
2020-05-01 22:42:32 +0800

    21. Merge Two Sorted Lists

--- src/leetcode/algorithms/MergeTwoLists.java
+++ src/leetcode/algorithms/MergeTwoLists.java
@@ -1,6 +1,7 @@
 package leetcode.algorithms;
 
 import leetcode.entity.ListNode;
+import leetcode.util.ListNodeUtils;
 import leetcode.util.OutputUtils;
 
 /**
@@ -11,59 +12,45 @@ import leetcode.util.OutputUtils;
  */
 public class MergeTwoLists {
     public static void main(String[] args) {
-        ListNode listNode11 = new ListNode(1);
-        ListNode listNode12 = new ListNode(2);
-        ListNode listNode13 = new ListNode(4);
-        listNode11.next = listNode12;
-        listNode12.next = listNode13;
-
-        ListNode listNode21 = new ListNode(1);
-        ListNode listNode22 = new ListNode(3);
-        ListNode listNode23 = new ListNode(4);
-        listNode21.next = listNode22;
-        listNode22.next = listNode23;
-
-        OutputUtils.printListNode(mergeTwoLists(listNode11, listNode21));
+        ListNode l11 = ListNodeUtils.arrayToListNode(new int[]{1, 2, 4});
+        ListNode l12 = ListNodeUtils.arrayToListNode(new int[]{1, 3, 4});
+        OutputUtils.printListNode(mergeTwoLists(l11, l12));
     }
 
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null && l2 == null) {
-            return null;
-        } else if (l1 == null) {
+        if (l1 == null) {
             return l2;
-        } else if (l2 == null) {
+        }
+
+        if (l2 == null) {
             return l1;
         }
-        ListNode listNode;
-        if (l1.val <= l2.val) {
-            listNode = new ListNode(l1.val);
-            l1 = l1.next;
-        } else {
-            listNode = new ListNode((l2.val));
-            l2 = l2.next;
-        }
-        ListNode temp = listNode;
-        while (l1 != null || l2 != null) {
-            if (l1 == null && l2 != null) {
-                temp.next = l2;
-                temp = temp.next;
+
+        ListNode dummy = new ListNode(-1);
+        ListNode currNode = dummy;
+        /**
+         * 只要两个链表都还有节点，就将较小的那个头节点截取下来接到合并后的链表最后，直到某一条链表的所有
+         * 节点都拼接完后，将另一条链表剩余的部分直接拼接到合并后的链表最后即可
+         */
+        while (l1 != null && l2 != null) {
+            if (l1.val >= l2.val) {
+                currNode.next = l2;
+                currNode = currNode.next;
                 l2 = l2.next;
-            } else if (l2 == null && l1 != null) {
-                temp.next = l1;
-                temp = temp.next;
+                currNode.next = null;
+            } else {
+                currNode.next = l1;
+                currNode = currNode.next;
                 l1 = l1.next;
-            } else if (l1 != null && l2 != null) {
-                if (l1.val <= l2.val) {
-                    temp.next = l1;
-                    temp = temp.next;
-                    l1 = l1.next;
-                } else {
-                    temp.next = l2;
-                    temp = temp.next;
-                    l2 = l2.next;
-                }
+                currNode.next = null;
             }
         }
-        return listNode;
+
+        if (l1 != null) {
+            currNode.next = l1;
+        } else {
+            currNode.next = l2;
+        }
+        return dummy.next;
     }
 }
