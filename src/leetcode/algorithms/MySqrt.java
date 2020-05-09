ad3a5e16b919c99721600d763e56ad7d4fd0234e
Baltan <617640006@qq.com>
2020-05-09 23:43:14 +0800

    69. Sqrt(x)

--- src/leetcode/algorithms/MySqrt.java
+++ src/leetcode/algorithms/MySqrt.java
@@ -13,13 +13,40 @@ public class MySqrt {
         System.out.println(mySqrt(9));
         System.out.println(mySqrt(14));
         System.out.println(mySqrt(16));
+        System.out.println(mySqrt(24));
+        System.out.println(mySqrt(36));
+        System.out.println(mySqrt(575));
+        System.out.println(mySqrt(3532));
+        System.out.println(mySqrt(47658));
+        System.out.println(mySqrt(25252262));
+        System.out.println(mySqrt(364747584));
     }
 
     public static int mySqrt(int x) {
-        long res = x;
-        while (res * res > x) {
-            res = (x + res * res) / (2 * res);
+        long lo = 0;
+        /**
+         * 整型x的上限是2^31-1，所以所求答案不可能大于2^16
+         */
+        long hi = (int) Math.pow(2, 16);
+        /**
+         * 二分查找
+         */
+        while (lo < hi) {
+            long mid = (lo + hi) / 2;
+            long pow = mid * mid;
+
+            if (pow > x) {
+                hi = mid - 1;
+            } else if (pow < x) {
+                if ((mid + 1) * (mid + 1) <= x) {
+                    lo = mid + 1;
+                } else {
+                    return (int) mid;
+                }
+            } else {
+                return (int) mid;
+            }
         }
-        return (int) res;
+        return (int) lo;
     }
 }
