4a2560b0ec6fea44ff765a1951d3a54c3f61e62a
Baltan <617640006@qq.com>
2020-05-24 11:48:42 +0800

    4. Median of Two Sorted Arrays

--- src/leetcode/algorithms/FindMedianSortedArrays.java
+++ src/leetcode/algorithms/FindMedianSortedArrays.java
@@ -1,7 +1,5 @@
 package leetcode.algorithms;
 
-import java.util.Arrays;
-
 /**
  * Description: 4. Median of Two Sorted Arrays
  *
@@ -15,28 +13,56 @@ public class FindMedianSortedArrays {
     }
 
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1 == null) {
+        /**
+         * 如果nums1为空数组，返回nums2的中位数
+         */
+        if (nums1.length == 0) {
             return nums2.length % 2 == 0 ? (nums2[nums2.length / 2 - 1] + nums2[nums2.length / 2]) / 2.0 :
                     nums2[nums2.length / 2] / 1.0;
         }
-        if (nums2 == null) {
+        /**
+         * 如果nums2为空数组，返回nums1的中位数
+         */
+        if (nums2.length == 0) {
             return nums1.length % 2 == 0 ? (nums1[nums1.length / 2 - 1] + nums1[nums1.length / 2]) / 2.0 :
                     nums1[nums1.length / 2] / 1.0;
         }
-        int nums1Length = nums1.length;
-        int nums2Length = nums2.length;
-        int numsLength = nums1Length + nums2Length;
-        int[] nums = new int[nums1Length + nums2Length];
-        for (int i = 0; i < numsLength; i++) {
-            if (i < nums1Length) {
-                nums[i] = nums1[i];
+
+        int length1 = nums1.length;
+        int length2 = nums2.length;
+        int index1 = 0;
+        int index2 = 0;
+        int mergeLength = length1 + length2;
+        /**
+         * nums1和nums2合并后的数组，数组中的所有元素按照升序排列
+         */
+        int[] nums = new int[length1 + length2];
+        /**
+         * 合并nums1和nums2
+         */
+        for (int i = 0; i < mergeLength; i++) {
+            if (index1 < length1 && index2 < length2) {
+                if (nums1[index1] <= nums2[index2]) {
+                    nums[i] = nums1[index1];
+                    index1++;
+                } else {
+                    nums[i] = nums2[index2];
+                    index2++;
+                }
+            } else if (index1 >= length1) {
+                nums[i] = nums2[index2];
+                index2++;
             } else {
-                nums[i] = nums2[i - nums1Length];
+                nums[i] = nums1[index1];
+                index1++;
             }
         }
-        Arrays.sort(nums);
+        /**
+         * 合并后数组nums的中位数
+         */
         double median =
-                numsLength % 2 == 0 ? (nums[numsLength / 2 - 1] + nums[numsLength / 2]) / 2.0 : nums[numsLength / 2] / 1.0;
+                mergeLength % 2 == 0 ? (nums[mergeLength / 2 - 1] + nums[mergeLength / 2]) / 2.0 :
+                        nums[mergeLength / 2] / 1.0;
         return median;
     }
 }
