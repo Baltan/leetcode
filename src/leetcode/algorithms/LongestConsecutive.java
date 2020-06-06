5b904f3ed77a24723d7c83aacdaef080db50a34b
Baltan <617640006@qq.com>
2020-06-06 13:25:07 +0800

    128. Longest Consecutive Sequence

--- src/leetcode/algorithms/LongestConsecutive.java
+++ src/leetcode/algorithms/LongestConsecutive.java
@@ -22,6 +22,9 @@ public class LongestConsecutive {
     public static int longestConsecutive(int[] nums) {
         int result = 0;
         Set<Long> set = new HashSet<>();
+        /**
+         * 记录已经在连续序列中计算过的数字
+         */
         Set<Long> isVisited = new HashSet<>();
 
         for (int num : nums) {
@@ -29,18 +32,28 @@ public class LongestConsecutive {
         }
 
         for (long num : set) {
+            /**
+             * 已经计算过的数字不再重复计算
+             */
             if (!isVisited.contains(num)) {
+                /**
+                 * 当前包含num的连续序列的长度
+                 */
                 int count = 1;
                 isVisited.add(num);
                 long i = num + 1;
                 long j = num - 1;
-
+                /**
+                 * 从num开始向后计算连续序列的长度
+                 */
                 while (set.contains(i) && !isVisited.contains(i)) {
                     count++;
                     isVisited.add(i);
                     i++;
                 }
-
+                /**
+                 * 从num开始向前计算连续序列的长度
+                 */
                 while (set.contains(j) && !isVisited.contains(j)) {
                     count++;
                     isVisited.add(j);
