a5a000c120b29139d510e2cd49f7a5a78676dd72
Baltan <617640006@qq.com>
2020-06-17 07:34:51 +0800

    1014. Best Sightseeing Pair

--- src/leetcode/algorithms/MaxScoreSightseeingPair.java
+++ src/leetcode/algorithms/MaxScoreSightseeingPair.java
@@ -11,13 +11,26 @@ public class MaxScoreSightseeingPair {
         System.out.println(maxScoreSightseeingPair(new int[]{8, 1, 5, 2, 6}));
     }
 
+    /**
+     * 参考：
+     * <a href="https://leetcode-cn.com/problems/best-sightseeing-pair/solution/zui-jia-guan-guang-zu-he-by-leetcode-solution/"></a>
+     *
+     * @param A
+     * @return
+     */
     public static int maxScoreSightseeingPair(int[] A) {
         int length = A.length;
         int maxI = 0 + A[0];
         int result = Integer.MIN_VALUE;
 
         for (int i = 1; i < length; i++) {
+            /**
+             * 到第i个景点为止一对观光景点可以取得的最高分
+             */
             result = Math.max(A[i] - i + maxI, result);
+            /**
+             * 到第i个景点为止，x+A[x]的最高分
+             */
             maxI = Math.max(maxI, i + A[i]);
         }
         return result;
