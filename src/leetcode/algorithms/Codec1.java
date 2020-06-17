8031e281184bee8e0fdf6854872aaf0b513f0af8
Baltan <617640006@qq.com>
2020-06-17 13:33:07 +0800

    297. Serialize and Deserialize Binary Tree

--- src/leetcode/algorithms/Codec1.java
+++ src/leetcode/algorithms/Codec1.java
@@ -1,6 +1,7 @@
 package leetcode.algorithms;
 
 import leetcode.entity.TreeNode;
+import leetcode.util.BinaryTreeUtils;
 
 import java.util.Objects;
 import java.util.Stack;
@@ -36,13 +37,20 @@ public class Codec1 {
         if (data == null || !data.startsWith("[") || !data.endsWith("]") || Objects.equals(data, "[]")) {
             return null;
         }
+        /**
+         * 查找字符串data中第二个"["的位置
+         */
         int index = data.indexOf("[", 1);
+        /**
+         * 索引位置为0的字符是data中的第一个"["，第一个"["和第二个"["之间的部分是二叉树根节点的值
+         */
         TreeNode root = new TreeNode(Integer.valueOf(data.substring(1, index)));
         int length = data.length();
         Stack<Character> stack = new Stack<>();
 
         for (int i = 1; ; i++) {
             char c = data.charAt(i);
+
             if (c != '[' && c != ']') {
                 continue;
             } else if (c == '[') {
@@ -50,7 +58,9 @@ public class Codec1 {
             } else if (c == ']') {
                 stack.pop();
             }
-
+            /**
+             * 当stack中没有元素时，就找到了一棵子树序列化后的字符串
+             */
             if (stack.isEmpty()) {
                 root.left = deserialize(data.substring(index, i + 1));
                 root.right = deserialize(data.substring(i + 1, length - 1));
@@ -60,32 +70,20 @@ public class Codec1 {
     }
 
     public static void main(String[] args) {
-        TreeNode treeNode11 = new TreeNode(1);
-        TreeNode treeNode12 = new TreeNode(2);
-        TreeNode treeNode13 = new TreeNode(3);
-        TreeNode treeNode14 = new TreeNode(4);
-        TreeNode treeNode15 = new TreeNode(5);
-        treeNode11.left = treeNode12;
-        treeNode11.right = treeNode13;
-        treeNode13.left = treeNode14;
-        treeNode13.right = treeNode15;
-        String str1 = serialize(treeNode11);
+        TreeNode root1 = BinaryTreeUtils.arrayToBinaryTree(new Integer[]{1, 2, 3, 4, 5}, 0);
+        String str1 = serialize(root1);
         System.out.println(str1);
 
-        TreeNode root1 = deserialize(str1);
-        System.out.println(root1);
+        TreeNode root11 = deserialize(str1);
+        System.out.println(root11);
 
         System.out.println("----------------------------------------");
 
-        TreeNode treeNode21 = new TreeNode(-1);
-        TreeNode treeNode22 = new TreeNode(0);
-        TreeNode treeNode23 = new TreeNode(1);
-        treeNode21.left = treeNode22;
-        treeNode21.right = treeNode23;
-        String str2 = serialize(treeNode21);
+        TreeNode root2 = BinaryTreeUtils.arrayToBinaryTree(new Integer[]{-1, 0, 1}, 0);
+        String str2 = serialize(root2);
         System.out.println(str2);
 
-        TreeNode root2 = deserialize(str2);
-        System.out.println(root2);
+        TreeNode root21 = deserialize(str2);
+        System.out.println(root21);
     }
 }
