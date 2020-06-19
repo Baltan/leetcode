92fb8dbcf102f06a57f6005deee8174756051f27
Baltan <617640006@qq.com>
2020-06-19 08:39:31 +0800

    125. Valid Palindrome

--- src/leetcode/algorithms/IsPalindrome.java
+++ src/leetcode/algorithms/IsPalindrome.java
@@ -10,53 +10,59 @@ public class IsPalindrome {
     public static void main(String[] args) {
         System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
         System.out.println(isPalindrome("race a car"));
+        System.out.println(isPalindrome(".,"));
     }
 
     public static boolean isPalindrome(String s) {
-        if (s.isEmpty()) {
-            return true;
-        }
-        int start = 0;
-        int end = s.length() - 1;
-        while (start < end) {
-            int startCharValue = s.charAt(start);
-            int endCharValue = s.charAt(end);
-            boolean startCharIsNotDigitOrLetter =
-                    !(startCharValue >= 48 && startCharValue <= 57) && !(startCharValue >= 65 && startCharValue <= 90) &&
-                            !(startCharValue >= 97 && startCharValue <= 122);
-            boolean endCharIsNotDigitOrLetter =
-                    !(endCharValue >= 48 && endCharValue <= 57) && !(endCharValue >= 65 && endCharValue <= 90) &&
-                            !(endCharValue >= 97 && endCharValue <= 122);
-            boolean startCharIsDigit = startCharValue >= 48 && startCharValue <= 57;
-            boolean endCharIsDigit = endCharValue >= 48 && endCharValue <= 57;
-            boolean startCharIsLetter =
-                    (startCharValue >= 65 && startCharValue <= 90 || startCharValue >= 97 && startCharValue <= 122);
-            boolean endCharIsLetter = (endCharValue >= 65 && endCharValue <= 90 || endCharValue >= 97 && endCharValue <= 122);
-            if (startCharIsNotDigitOrLetter) {
-                start++;
-                continue;
+        char[] charArray = s.toCharArray();
+        int length = charArray.length;
+        int lo = 0;
+        int hi = length - 1;
+
+        while (lo < hi) {
+            /**
+             * 从低位向高位找到第一个字母或数字
+             */
+            while (lo < length && !Character.isAlphabetic(charArray[lo]) &&
+                    !Character.isDigit(charArray[lo])) {
+                lo++;
             }
-            if (endCharIsNotDigitOrLetter) {
-                end--;
-                continue;
+            /**
+             * 从高位向低位找到第一个字母或数字
+             */
+            while (hi >= 0 && !Character.isAlphabetic(charArray[hi]) && !Character.isDigit(charArray[hi])) {
+                hi--;
             }
-            if (startCharIsDigit && endCharIsDigit) {
-                if (s.substring(start, start + 1).equals(s.substring(end, end + 1))) {
-                    start++;
-                    end--;
-                } else {
-                    return false;
-                }
-            } else if (startCharIsLetter && endCharIsLetter) {
-                if (s.substring(start, start + 1).equalsIgnoreCase(s.substring(end, end + 1))) {
-                    start++;
-                    end--;
-                } else {
-                    return false;
-                }
-            } else {
+            /**
+             * 如果低位和高位上都没有数字或字母了，直接返回true
+             */
+            if (lo >= length && hi < 0) {
+                return true;
+            }
+            /**
+             * 如果低位和高位上一边有数字或字母，另一边没有，直接返回false
+             */
+            if (lo >= length || hi < 0) {
                 return false;
             }
+            /**
+             * 如果找到的是字母，转为小写字母
+             */
+            if (Character.isAlphabetic(charArray[lo])) {
+                charArray[lo] = Character.toLowerCase(charArray[lo]);
+            }
+            /**
+             * 如果找到的是字母，转为小写字母
+             */
+            if (Character.isAlphabetic(charArray[hi])) {
+                charArray[hi] = Character.toLowerCase(charArray[hi]);
+            }
+
+            if (charArray[lo] != charArray[hi]) {
+                return false;
+            }
+            lo++;
+            hi--;
         }
         return true;
     }
