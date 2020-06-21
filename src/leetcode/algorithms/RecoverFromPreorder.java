6e7cf8bb7130cae270a5343cdf0da7a13df67d15
Baltan <617640006@qq.com>
2020-06-21 16:03:34 +0800

    1028. Recover a Tree From Preorder Traversal

--- src/leetcode/algorithms/RecoverFromPreorder.java
+++ src/leetcode/algorithms/RecoverFromPreorder.java
@@ -2,8 +2,7 @@ package leetcode.algorithms;
 
 import leetcode.entity.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 /**
  * Description: 1028. Recover a Tree From Preorder Traversal
@@ -16,68 +15,91 @@ public class RecoverFromPreorder {
         System.out.println(recoverFromPreorder("1-2--3--4-5--6--7"));
         System.out.println(recoverFromPreorder("1-2--3---4-5--6---7"));
         System.out.println(recoverFromPreorder("1-401--349---90--88"));
+        System.out.println(recoverFromPreorder("3"));
     }
 
     public static TreeNode recoverFromPreorder(String S) {
-        if (S == null || S.length() == 0 || !Character.isDigit(S.charAt(0))) {
+        if (S == null || Objects.equals(S, "")) {
             return null;
         }
-
-        if (!S.contains("-")) {
-            return new TreeNode(Integer.valueOf(S));
-        }
-
+        /**
+         * levels[i]表示最近遍历到的第i层的节点
+         */
+        TreeNode[] levels = new TreeNode[1000];
+        /**
+         * hasChild[i]表示最近遍历到的第i层的节点是否已经找到子节点
+         */
+        boolean[] hasChild = new boolean[1000];
+        /**
+         * 当前遍历到的节点的值
+         */
+        int value = 0;
+        /**
+         * 当前遍历的到的连续"-"的个数，即当前处理到的节点的层数（0-based）
+         */
+        int dashCount;
         int length = S.length();
-        List<Integer> list = new ArrayList<>(2);
-        TreeNode root = new TreeNode(Integer.valueOf(S.substring(0, S.indexOf("-"))));
-
-        for (int i = 1; i < length - 1; i++) {
-            if (S.charAt(i) == '-' && S.charAt(i - 1) != '-' && S.charAt(i + 1) != '-') {
-                list.add(i);
-            }
+        char[] charArray = S.toCharArray();
+        /**
+         * 当前遍历到的S中的字符的索引位置
+         */
+        int index = 0;
+        /**
+         * 查找根节点的值
+         */
+        while (index < length && charArray[index] != '-') {
+            value = value * 10 + (charArray[index] - '0');
+            index++;
         }
 
-        if (list.size() == 2) {
-            String substr1 = S.substring(list.get(0) + 1, list.get(1));
-            String substr2 = S.substring(list.get(1) + 1, length);
-            int length1 = substr1.length();
-            int length2 = substr2.length();
-            StringBuilder builder1 = new StringBuilder();
-            StringBuilder builder2 = new StringBuilder();
+        levels[0] = new TreeNode(value);
 
-            for (int i = 0; i < length1; i++) {
-                if (i - 1 >= 0 && substr1.charAt(i) == '-' && substr1.charAt(i - 1) != '-') {
-                    continue;
-                } else {
-                    builder1.append(substr1.charAt(i));
-                }
+        while (index < length) {
+            /**
+             * 还原dashCount和value的初始值
+             */
+            dashCount = 0;
+            value = 0;
+            /**
+             * 查找下一个节点前连续的"-"的个数，确定下一个节点的层数
+             */
+            while (index < length && charArray[index] == '-') {
+                dashCount++;
+                index++;
             }
-
-            for (int i = 0; i < length2; i++) {
-                if (i - 1 >= 0 && substr2.charAt(i) == '-' && substr2.charAt(i - 1) != '-') {
-                    continue;
-                } else {
-                    builder2.append(substr2.charAt(i));
-                }
+            /**
+             * 查找下一个节点的值
+             */
+            while (index < length && charArray[index] != '-') {
+                value = value * 10 + (charArray[index] - '0');
+                index++;
             }
-
-            root.left = recoverFromPreorder(builder1.toString());
-            root.right = recoverFromPreorder(builder2.toString());
-        } else if (list.size() == 1) {
-            String substr1 = S.substring(list.get(0) + 1, length);
-            int length1 = substr1.length();
-            StringBuilder builder1 = new StringBuilder();
-
-            for (int i = 0; i < length1; i++) {
-                if (i - 1 >= 0 && substr1.charAt(i) == '-' && substr1.charAt(i - 1) != '-') {
-                    continue;
-                } else {
-                    builder1.append(substr1.charAt(i));
-                }
+            /**
+             * 创建下一个节点对象
+             */
+            TreeNode node = new TreeNode(value);
+            /**
+             * 更新第dashCount层的节点，接下去如果还有第dashCount+1层的节点，只可能为该节点的子节点
+             */
+            levels[dashCount] = node;
+            /**
+             * 标记当前第dashCount层的节点没有子节点
+             */
+            hasChild[dashCount] = false;
+            /**
+             * 如果第dashCount-1层的节点已经有了子节点（即左子节点），则当前第dashCount层的节点为上层节
+             * 点的右子节点，否则为左子节点
+             */
+            if (!hasChild[dashCount - 1]) {
+                levels[dashCount - 1].left = node;
+                /**
+                 * 标记第dashCount-1层的节点已经有了子节点
+                 */
+                hasChild[dashCount - 1] = true;
+            } else {
+                levels[dashCount - 1].right = node;
             }
-
-            root.left = recoverFromPreorder(builder1.toString());
         }
-        return root;
+        return levels[0];
     }
 }
