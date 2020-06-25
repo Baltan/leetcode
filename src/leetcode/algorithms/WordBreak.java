b4559bdd1b42d1862449994d1a8b259286895a3d
Baltan <617640006@qq.com>
2020-06-25 22:03:04 +0800

    139. Word Break

--- src/leetcode/algorithms/WordBreak.java
+++ src/leetcode/algorithms/WordBreak.java
@@ -31,24 +31,39 @@ public class WordBreak {
     }
 
     public static boolean wordBreak(String s, List<String> wordDict) {
+        /**
+         * 保存需要被拆分的子串
+         */
         Queue<String> queue = new LinkedList<>();
         queue.offer(s);
+        /**
+         * 保存已经被拆分的子串，避免重复的子串加入到queue中
+         */
         Set<String> set = new HashSet<>();
         set.add(s);
 
         while (!queue.isEmpty()) {
+            /**
+             * 当前将要被拆分的子串
+             */
             String currentStr = queue.poll();
-            for (int i = 0, size = wordDict.size(); i < size; i++) {
-                String wordStr = wordDict.get(i);
-                if (currentStr.startsWith(wordStr)) {
-                    if (Objects.equals(currentStr, wordStr)) {
-                        return true;
-                    } else if (wordStr.length() < currentStr.length()) {
-                        String newStr = currentStr.substring(wordStr.length());
-                        if (!set.contains(newStr)) {
-                            queue.offer(newStr);
-                            set.add(newStr);
-                        }
+
+            for (String word : wordDict) {
+                /**
+                 * 如果wordDict中存在某个单词和当前将要被拆分的子串currentStr相同，则说明最原始的字符串
+                 * s可以被拆分成wordDict中的若干个单词；否则如果wordDict中某个单词和currentStr的头部相
+                 * 同，则将currentStr除去头部后剩余的子串加入queue中，后面继续尝试拆分剩余的子串
+                 */
+                if (Objects.equals(currentStr, word)) {
+                    return true;
+                } else if (currentStr.startsWith(word)) {
+                    String newStr = currentStr.substring(word.length());
+                    /**
+                     * 如果剩余的子串已经被加入到queue中，就不再重复加入
+                     */
+                    if (!set.contains(newStr)) {
+                        queue.offer(newStr);
+                        set.add(newStr);
                     }
                 }
             }
