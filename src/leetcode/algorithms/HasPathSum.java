4c626da77162d8f8bafe5fc8a11c2d4b11f37082
Baltan <617640006@qq.com>
2020-07-07 08:40:03 +0800

    112. Path Sum

--- src/leetcode/algorithms/HasPathSum.java
+++ src/leetcode/algorithms/HasPathSum.java
@@ -1,6 +1,7 @@
 package leetcode.algorithms;
 
 import leetcode.entity.TreeNode;
+import leetcode.util.BinaryTreeUtils;
 
 /**
  * Description: 112. Path Sum
@@ -10,23 +11,9 @@ import leetcode.entity.TreeNode;
  */
 public class HasPathSum {
     public static void main(String[] args) {
-        TreeNode root1 = new TreeNode(5);
-        TreeNode node1 = new TreeNode(4);
-        TreeNode node2 = new TreeNode(8);
-        TreeNode node3 = new TreeNode(11);
-        TreeNode node4 = new TreeNode(13);
-        TreeNode node5 = new TreeNode(4);
-        TreeNode node6 = new TreeNode(7);
-        TreeNode node7 = new TreeNode(2);
-        TreeNode node8 = new TreeNode(1);
-        root1.left = node1;
-        root1.right = node2;
-        node1.left = node3;
-        node2.left = node4;
-        node2.right = node5;
-        node3.left = node6;
-        node3.right = node7;
-        node5.right = node8;
+        TreeNode root1 =
+                BinaryTreeUtils.arrayToBinaryTree(new Integer[]{5, 4, 8, 11, null, 13, 4, 7, 2, null, null,
+                        null, null, null, 1}, 0);
         System.out.println(hasPathSum(root1, 22));
     }
 
@@ -34,6 +21,7 @@ public class HasPathSum {
         if (root == null) {
             return false;
         }
+
         if (root.left == null && root.right == null) {
             return root.val == sum;
         }
