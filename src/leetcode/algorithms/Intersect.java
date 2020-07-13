af54d415ebded472c9edb13f2ab5879fab203cbe
Baltan <617640006@qq.com>
2020-07-13 09:09:48 +0800

    350. Intersection of Two Arrays II

--- src/leetcode/algorithms/Intersect.java
+++ src/leetcode/algorithms/Intersect.java
@@ -2,7 +2,8 @@ package leetcode.algorithms;
 
 import leetcode.util.OutputUtils;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -14,24 +15,43 @@ import java.util.List;
 public class Intersect {
     public static void main(String[] args) {
         OutputUtils.print1DIntegerArray(intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2}));
+        OutputUtils.print1DIntegerArray(intersect(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4}));
     }
 
     public static int[] intersect(int[] nums1, int[] nums2) {
-        List<Integer> nums1List = new ArrayList<>();
-        List<Integer> intersectList = new ArrayList<>();
-        for (int i = 0; i < nums1.length; i++) {
-            nums1List.add(nums1[i]);
-        }
-        for (int i = 0; i < nums2.length; i++) {
-            if (nums1List.contains(nums2[i])) {
-                intersectList.add(nums2[i]);
-                nums1List.remove(new Integer(nums2[i]));
+        List<Integer> list = new LinkedList<>();
+        int length1 = nums1.length;
+        int length2 = nums2.length;
+        int index1 = 0;
+        int index2 = 0;
+        /**
+         * 将nums1和nums2都进行升序排列
+         */
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        /**
+         * 双指针，如果当前nums1和nums2指向的数字相同，则将该数字加入到交集中，同时向后移动两个指针；否则向后移动指向
+         * 较小数字的指针
+         */
+        while (index1 < length1 && index2 < length2) {
+            if (nums1[index1] == nums2[index2]) {
+                list.add(nums1[index1]);
+                index1++;
+                index2++;
+            } else if (nums1[index1] < nums2[index2]) {
+                index1++;
+            } else {
+                index2++;
             }
         }
-        int[] intersectArray = new int[intersectList.size()];
-        for (int i = 0; i < intersectList.size(); i++) {
-            intersectArray[i] = intersectList.get(i);
+
+        int size = list.size();
+        int index = 0;
+        int[] result = new int[size];
+
+        for (int value : list) {
+            result[index++] = value;
         }
-        return intersectArray;
+        return result;
     }
 }
