70130c3a73bd67cd4ee03fe66b153d55292276d0
Baltan <617640006@qq.com>
2020-07-15 07:56:12 +0800

    96. Unique Binary Search Trees

--- src/leetcode/algorithms/NumTrees.java
+++ src/leetcode/algorithms/NumTrees.java
@@ -17,21 +17,22 @@ public class NumTrees {
     }
 
     public static int numTrees(int n) {
-        if (n < 0) {
-            return 1;
-        }
-
-        if (n == 0 || n == 1) {
+        /**
+         * 如果n小于1，则只能得到一棵空树；如果n等于1，则只能得到一棵只有根节点1的二叉树
+         */
+        if (n <= 1) {
             return 1;
         }
 
         int[] dp = new int[n + 1];
         dp[0] = 1;
-        dp[1] = 1;
         /**
          * 如果一棵二叉搜索树一共有i个节点，即二叉搜索树节点的值为1，2，……，i
          */
-        for (int i = 2; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
+            /**
+             * 当根节点值为j时，二叉搜索树的种数
+             */
             int count = 0;
             /**
              * 假设根节点的值为j，则左子树一共有j-1个节点，右子树一共有i-j个节点，此时二叉搜索树一共有
