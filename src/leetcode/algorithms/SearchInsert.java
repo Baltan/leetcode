3bc7ff710699b8d191ad1e9059a86016fa5ff801
Baltan <617640006@qq.com>
2020-07-17 08:53:48 +0800

    35. Search Insert Position

--- src/leetcode/algorithms/SearchInsert.java
+++ src/leetcode/algorithms/SearchInsert.java
@@ -2,25 +2,45 @@ package leetcode.algorithms;
 
 /**
  * Description: 35. Search Insert Position
- * @author Baltan
  *
+ * @author Baltan
  * @date 2017/11/8 13:55
  */
 public class SearchInsert {
     public static void main(String[] args) {
-
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 5));
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 2));
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 7));
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 0));
     }
 
     public static int searchInsert(int[] nums, int target) {
-        if (nums.length == 0) {
+        int length = nums.length;
+
+        if (length == 0) {
             return 0;
-        } else {
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == target || nums[i] > target) {
-                    return i;
-                }
+        }
+        /**
+         * 如果nums中所有数字都小于target，则将target插入到nums数组最后
+         */
+        if (nums[length - 1] < target) {
+            return length;
+        }
+
+        int lo = 0;
+        int hi = length - 1;
+        /**
+         * 二分查找nums中第一个大于等于target的数字
+         */
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+
+            if (nums[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
             }
         }
-        return nums.length;
+        return lo;
     }
 }
