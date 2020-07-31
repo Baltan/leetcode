82c91b655c0d4069b2e79a3d8f78e098b5a47926
Baltan <617640006@qq.com>
2020-07-31 23:04:27 +0800

    319. Bulb Switcher

--- src/leetcode/algorithms/BulbSwitch.java
+++ src/leetcode/algorithms/BulbSwitch.java
@@ -5,6 +5,10 @@ package leetcode.algorithms;
  *
  * @author Baltan
  * @date 2019-06-21 09:25
+ * @see BulbSwitch1
+ * @see FlipLights
+ * @see NumTimesAllBlue
+ * @see MinFlips1
  */
 public class BulbSwitch {
     public static void main(String[] args) {
@@ -24,8 +28,13 @@ public class BulbSwitch {
     public static int bulbSwitch(int n) {
         int result = 0;
         int i = 1;
-
-        while (i * i - 1 < n) {
+        /**
+         * 根据题意，第1轮所有编号为1的倍数的灯泡会被开关一次，第2轮所有编号为2的倍数的灯泡会被开关一次，第3轮所有
+         * 编号为3的倍数的灯泡会被开关一次，以此类推，第n轮所有编号为n的倍数的灯泡会被开关一次。所以对于某个灯泡的
+         * 编号m，如果它在[1,n]有k个因数，则它会被开关k次。最后开着的灯泡都被开关了奇数次，即灯泡的编号有奇数个因
+         * 数，则这些编号一定是平方数，所以求[1,n]中平方数的个数即可
+         */
+        while (i * i <= n) {
             result++;
             i++;
         }
