83ee0390d7f38806e54dcaeb78778a23d1da49b1
Baltan <617640006@qq.com>
2020-08-05 07:53:52 +0800

    337. House Robber III

--- src/leetcode/algorithms/Rob2.java
+++ src/leetcode/algorithms/Rob2.java
@@ -1,45 +1,26 @@
 package leetcode.algorithms;
 
 import leetcode.entity.TreeNode;
+import leetcode.util.BinaryTreeUtils;
 
 /**
  * Description: 337. House Robber III
  *
  * @author Baltan
  * @date 2019-06-25 09:21
+ * @see Rob
+ * @see Rob1
  */
 public class Rob2 {
     public static void main(String[] args) {
-        TreeNode treeNode11 = new TreeNode(3);
-        TreeNode treeNode12 = new TreeNode(2);
-        TreeNode treeNode13 = new TreeNode(3);
-        TreeNode treeNode14 = new TreeNode(3);
-        TreeNode treeNode15 = new TreeNode(1);
-        treeNode11.left = treeNode12;
-        treeNode11.right = treeNode13;
-        treeNode12.right = treeNode14;
-        treeNode13.right = treeNode15;
-        System.out.println(rob(treeNode11));
+        TreeNode root1 = BinaryTreeUtils.arrayToBinaryTree(new Integer[]{3, 2, 3, null, 3, null, 1}, 0);
+        System.out.println(rob(root1));
 
-        TreeNode treeNode21 = new TreeNode(3);
-        TreeNode treeNode22 = new TreeNode(4);
-        TreeNode treeNode23 = new TreeNode(5);
-        TreeNode treeNode24 = new TreeNode(1);
-        TreeNode treeNode25 = new TreeNode(3);
-        TreeNode treeNode26 = new TreeNode(1);
-        treeNode21.left = treeNode22;
-        treeNode21.right = treeNode23;
-        treeNode22.left = treeNode24;
-        treeNode22.right = treeNode25;
-        treeNode23.right = treeNode26;
-        System.out.println(rob(treeNode21));
+        TreeNode root2 = BinaryTreeUtils.arrayToBinaryTree(new Integer[]{3, 4, 5, 1, 3, null, 1}, 0);
+        System.out.println(rob(root2));
 
-        TreeNode treeNode31 = new TreeNode(2);
-        TreeNode treeNode32 = new TreeNode(1);
-        TreeNode treeNode33 = new TreeNode(3);
-        treeNode31.left = treeNode32;
-        treeNode31.right = treeNode33;
-        System.out.println(rob(treeNode31));
+        TreeNode root3 = BinaryTreeUtils.arrayToBinaryTree(new Integer[]{2, 1, 3}, 0);
+        System.out.println(rob(root3));
     }
 
     public static int rob(TreeNode root) {
@@ -48,16 +29,25 @@ public class Rob2 {
         } else if (root.left == null && root.right == null) {
             return root.val;
         } else {
+            /**
+             * 选择根节点可以得到的最大金额
+             */
             int chooseRoot = root.val;
-
+            /**
+             * 向左子树方向选择
+             */
             if (root.left != null) {
                 chooseRoot = chooseRoot + rob(root.left.left) + rob(root.left.right);
             }
-
+            /**
+             * 向右子树方向选择
+             */
             if (root.right != null) {
                 chooseRoot = chooseRoot + rob(root.right.left) + rob(root.right.right);
             }
-
+            /**
+             * 不选择根节点可以得到的最大金额
+             */
             int notChooseRoot = rob(root.left) + rob(root.right);
             return Math.max(chooseRoot, notChooseRoot);
         }
