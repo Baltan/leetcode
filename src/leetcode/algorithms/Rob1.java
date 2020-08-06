5e1b7a54ac3dfa33c6568184d7e33239cc4bda01
Baltan <617640006@qq.com>
2020-08-06 13:30:17 +0800

    213. House Robber II

--- src/leetcode/algorithms/Rob1.java
+++ src/leetcode/algorithms/Rob1.java
@@ -5,6 +5,8 @@ package leetcode.algorithms;
  *
  * @author Baltan
  * @date 2019-06-08 09:23
+ * @see Rob
+ * @see Rob2
  */
 public class Rob1 {
     public static void main(String[] args) {
@@ -26,19 +28,28 @@ public class Rob1 {
         if (nums.length == 2) {
             return Math.max(nums[0], nums[1]);
         }
-
+        /**
+         * 因为所有房子排成一个环状，所以第一间房屋和最后一件房屋不可能同时选中，考虑不包含第一件房屋和最后一间房
+         * 屋的两种情况
+         */
         int length = nums.length - 1;
-        int[] help1 = new int[length];
-        int[] help2 = new int[length];
-        help1[0] = nums[0];
-        help1[1] = Math.max(nums[0], nums[1]);
-        help2[0] = nums[1];
-        help2[1] = Math.max(nums[1], nums[2]);
+        /**
+         * dp1[i]为到nums[i]为止可以获得的最高金额（不包括最后一间房屋）
+         */
+        int[] dp1 = new int[length];
+        /**
+         * dp2[i]为到nums[i+1]为止可以获得的最高金额（不包括第一间房屋）
+         */
+        int[] dp2 = new int[length];
+        dp1[0] = nums[0];
+        dp1[1] = Math.max(nums[0], nums[1]);
+        dp2[0] = nums[1];
+        dp2[1] = Math.max(nums[1], nums[2]);
 
         for (int i = 2; i < length; i++) {
-            help1[i] = Math.max(help1[i - 2] + nums[i], help1[i - 1]);
-            help2[i] = Math.max(help2[i - 2] + nums[i + 1], help2[i - 1]);
+            dp1[i] = Math.max(dp1[i - 2] + nums[i], dp1[i - 1]);
+            dp2[i] = Math.max(dp2[i - 2] + nums[i + 1], dp2[i - 1]);
         }
-        return Math.max(help1[length - 1], help2[length - 1]);
+        return Math.max(dp1[length - 1], dp2[length - 1]);
     }
 }
