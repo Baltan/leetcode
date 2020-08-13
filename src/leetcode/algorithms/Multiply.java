3e6efb9cfe3d15bdae78bb9ac79a5a71de5ff639
Baltan <617640006@qq.com>
2020-08-13 12:54:06 +0800

    43. Multiply Strings

--- src/leetcode/algorithms/Multiply.java
+++ src/leetcode/algorithms/Multiply.java
@@ -15,37 +15,50 @@ public class Multiply {
     }
 
     public static String multiply(String num1, String num2) {
-        if (num1 == null || num2 == null || num1.length() == 0 || num2.length() == 0) {
-            return "";
-        }
         if ("0".equals(num1) || "0".equals(num2)) {
             return "0";
         }
+
         int length1 = num1.length();
         int length2 = num2.length();
-        char[] num1Array = num1.toCharArray();
-        char[] num2Array = num2.toCharArray();
-        char[] array = new char[length1 + length2];
-        int arrayLength = length1 + length2;
+        int totalLength = length1 + length2;
+        char[] chars1 = num1.toCharArray();
+        char[] chars2 = num2.toCharArray();
+        char[] result = new char[length1 + length2];
+        /**
+         * 进位
+         */
         int carry = 0;
-        for (int i = arrayLength - 1; i >= 0; i--) {
-            int offset = arrayLength - 1 - i;
-            int firstNum;
-            int secondNum;
+        /**
+         * 最最低位开始向最高位计算result的每一位的值
+         */
+        for (int i = totalLength - 1; i >= 0; i--) {
+            /**
+             * num1和num2尾部剩余数字长度之和，即最后计算出的值sum实际代表sum*(10^totalTailLength)
+             */
+            int totalTailLength = totalLength - 1 - i;
+            int x;
+            int y;
             int sum = 0;
-            for (int j = 0; j <= offset; j++) {
-                if (length1 - 1 - j >= 0 && length2 - 1 - (offset - j) >= 0) {
-                    firstNum = num1Array[length1 - 1 - j] - '0';
-                    secondNum = num2Array[length2 - 1 - (offset - j)] - '0';
-                    sum += firstNum * secondNum;
+            /**
+             * j为num1尾部剩余数字的个数，则totalTailLength-j为num2尾部剩余数字的个数
+             */
+            for (int j = 0; j <= totalTailLength; j++) {
+                if (length1 - 1 - j >= 0 && length2 - 1 - (totalTailLength - j) >= 0) {
+                    x = chars1[length1 - 1 - j] - '0';
+                    y = chars2[length2 - 1 - (totalTailLength - j)] - '0';
+                    sum += x * y;
                 } else {
                     sum += 0;
                 }
             }
             sum += carry;
-            array[i] = (char) (sum % 10 + '0');
+            result[i] = (char) (sum % 10 + '0');
             carry = sum / 10;
         }
-        return array[0] == '0' ? new String(array).substring(1) : new String(array);
+        /**
+         * 如果result最高位为0，则把该位去掉
+         */
+        return result[0] == '0' ? new String(result).substring(1) : new String(result);
     }
 }
