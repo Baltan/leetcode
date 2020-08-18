081712bdb471f2ec1317ab3f702b28a004f43b34
Baltan <617640006@qq.com>
2020-08-19 07:22:35 +0800

    647. Palindromic Substrings

--- src/leetcode/algorithms/CountSubstrings.java
+++ src/leetcode/algorithms/CountSubstrings.java
@@ -13,28 +13,36 @@ public class CountSubstrings {
     }
 
     public static int countSubstrings(String s) {
-        int palindromeNum = 0;
+        int result = 0;
+        char[] charArray = s.toCharArray();
+        int length = charArray.length;
+
         for (int i = 0; i < s.length(); i++) {
+            /**
+             * 向两侧延伸的长度
+             */
             int extendNum = 0;
-            while (i - extendNum >= 0 && i + extendNum < s.length()) {
-                if (s.substring(i - extendNum, i - extendNum + 1).equals(s.substring(i + extendNum, i + extendNum + 1))) {
-                    palindromeNum++;
-                    extendNum++;
-                } else {
-                    break;
-                }
+            /**
+             * 以字符charArray[i]作为中心向两侧延伸，查找回文子串
+             */
+            while (i - extendNum >= 0 && i + extendNum < length &&
+                    charArray[i - extendNum] == charArray[i + extendNum]) {
+                result++;
+                extendNum++;
             }
+
             extendNum = 0;
-            while (i - extendNum >= 0 &&
-                    i + 1 + extendNum < s.length() && s.substring(i, i + 1).equals(s.substring(i + 1, i + 2))) {
-                if (s.substring(i - extendNum, i - extendNum + 1).equals(s.substring(i + 1 + extendNum, i + 2 + extendNum))) {
-                    palindromeNum++;
+            /**
+             * 如果charArray[i]和charArray[i+1]相等，则以这两个字符作为中心想两侧延伸，查找回文子串
+             */
+            if (i + 1 < length && charArray[i] == charArray[i + 1]) {
+                while (i - extendNum >= 0 && i + 1 + extendNum < length &&
+                        charArray[i - extendNum] == charArray[i + 1 + extendNum]) {
+                    result++;
                     extendNum++;
-                } else {
-                    break;
                 }
             }
         }
-        return palindromeNum;
+        return result;
     }
 }
