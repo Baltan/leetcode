48471739d2abe271e086bff7cb8e74d931065e90
Baltan <617640006@qq.com>
2020-08-24 09:16:56 +0800

    459. Repeated Substring Pattern

--- src/leetcode/algorithms/RepeatedSubstringPattern.java
+++ src/leetcode/algorithms/RepeatedSubstringPattern.java
@@ -20,23 +20,35 @@ public class RepeatedSubstringPattern {
         if (s.length() < 2) {
             return false;
         }
+
         int length = s.length();
-        boolean flag = false;
-        for (int i = 1; i <= length / 2; i++) {
+        int halfLength = length / 2;
+
+        outer:
+        for (int i = 1; i <= halfLength; i++) {
+            /**
+             * 只有s的长度为子串的长度i的整数倍，才有可能满足要求
+             */
             if (length % i == 0) {
-                flag = true;
-                String standard = s.substring(0, i);
+                /**
+                 * 长度为i的子串
+                 */
+                String substring = s.substring(0, i);
+                /**
+                 * 逐一判断s截取下来的每一个长度为i的子串是否和第一个子串substring相同，如果不同则判断下一个可
+                 * 能的子串
+                 */
                 for (int j = i; j < length; j += i) {
-                    if (!s.substring(j, j + i).equals(standard)) {
-                        flag = false;
-                        break;
+                    if (!s.substring(j, j + i).equals(substring)) {
+                        continue outer;
                     }
                 }
-                if (flag == true) {
-                    break;
-                }
+                /**
+                 * 当前长度为i的子串满足要求，直接返回true
+                 */
+                return true;
             }
         }
-        return flag;
+        return false;
     }
 }
