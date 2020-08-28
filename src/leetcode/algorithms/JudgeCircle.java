ff9c21631a0a83868d01eadd802635f1b8318003
Baltan <617640006@qq.com>
2020-08-28 08:11:23 +0800

    657. Robot Return to Origin

--- src/leetcode/algorithms/JudgeCircle.java
+++ src/leetcode/algorithms/JudgeCircle.java
@@ -13,22 +13,26 @@ public class JudgeCircle {
     }
 
     public static boolean judgeCircle(String moves) {
+        /**
+         * 机器人所在坐标
+         */
         int[] position = {0, 0};
-        for (int i = 0; i < moves.length(); i++) {
-            String letter = moves.substring(i, i + 1);
-            if ("U".equalsIgnoreCase(letter)) {
+        char[] charArray = moves.toCharArray();
+
+        for (char c : charArray) {
+            if ('U' == c) {
                 position[0]++;
-            } else if ("D".equalsIgnoreCase(letter)) {
+            } else if ('D' == c) {
                 position[0]--;
-            } else if ("R".equalsIgnoreCase(letter)) {
+            } else if ('R' == c) {
                 position[1]++;
-            } else if ("L".equalsIgnoreCase(letter)) {
+            } else if ('L' == c) {
                 position[1]--;
             }
         }
-        if (position[0] == 0 && position[1] == 0) {
-            return true;
-        }
-        return false;
+        /**
+         * 判断最终位置的坐标是否是原点
+         */
+        return position[0] == 0 && position[1] == 0;
     }
 }
