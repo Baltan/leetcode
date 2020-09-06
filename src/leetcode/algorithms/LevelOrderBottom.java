fd886759d4149e8a8518da739d6be421b7007688
Baltan <617640006@qq.com>
2020-09-06 09:26:59 +0800

    107. Binary Tree Level Order Traversal II

--- src/leetcode/algorithms/LevelOrderBottom.java
+++ src/leetcode/algorithms/LevelOrderBottom.java
@@ -1,9 +1,11 @@
 package leetcode.algorithms;
 
 import leetcode.entity.TreeNode;
+import leetcode.util.BinaryTreeUtils;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Description: 107. Binary Tree Level Order Traversal II
@@ -13,89 +15,55 @@ import java.util.List;
  */
 public class LevelOrderBottom {
     public static void main(String[] args) {
-        TreeNode node11 = new TreeNode(3);
-        TreeNode node21 = new TreeNode(9);
-        TreeNode node22 = new TreeNode(20);
-        TreeNode node33 = new TreeNode(15);
-        TreeNode node34 = new TreeNode(7);
-        node11.left = node21;
-        node11.right = node22;
-        node22.left = node33;
-        node22.right = node34;
-        System.out.println(levelOrderBottom(node11));
+        TreeNode root1 = BinaryTreeUtils.arrayToBinaryTree(new Integer[]{3, 9, 20, null, null, 15, 7}, 0);
+        System.out.println(levelOrderBottom(root1));
 
-        TreeNode node41 = new TreeNode(1);
-        TreeNode node51 = new TreeNode(2);
-        TreeNode node52 = new TreeNode(3);
-        TreeNode node61 = new TreeNode(4);
-        TreeNode node62 = new TreeNode(5);
-        node41.left = node51;
-        node41.right = node52;
-        node51.left = node61;
-        node51.right = node62;
-        System.out.println(levelOrderBottom(node41));
+        TreeNode root2 = BinaryTreeUtils.arrayToBinaryTree(new Integer[]{1, 2, 3, 4, 5}, 0);
+        System.out.println(levelOrderBottom(root2));
 
-        TreeNode node71 = new TreeNode(1);
-        TreeNode node81 = new TreeNode(2);
-        TreeNode node82 = new TreeNode(2);
-        TreeNode node91 = new TreeNode(3);
-        TreeNode node92 = new TreeNode(4);
-        TreeNode node93 = new TreeNode(4);
-        TreeNode node94 = new TreeNode(3);
-        node71.left = node81;
-        node71.right = node82;
-        node81.left = node91;
-        node81.right = node92;
-        node82.left = node93;
-        node82.right = node94;
-        System.out.println(levelOrderBottom(node71));
+        TreeNode root3 = BinaryTreeUtils.arrayToBinaryTree(new Integer[]{1, 2, 2, 3, 4, 4, 3}, 0);
+        System.out.println(levelOrderBottom(root3));
     }
 
     public static List<List<Integer>> levelOrderBottom(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> result = new LinkedList<>();
+
         if (root == null) {
-            return res;
-        } else {
-            List<Integer> list = new ArrayList<>();
-            list.add(root.val);
-            res.add(list);
-            if (root.left == null && root.right == null) {
-                return res;
-            } else {
-                List<List<Integer>> list1 = new ArrayList<>();
-                List<List<Integer>> leftRes = levelOrderBottom(root.left);
-                List<List<Integer>> rightRes = levelOrderBottom(root.right);
-                int leftDepth = leftRes.size();
-                int rightDepth = rightRes.size();
-                if (leftDepth > rightDepth) {
-                    for (int i = 0; i < leftDepth - rightDepth; i++) {
-                        list1.add(leftRes.get(i));
-                    }
-                    for (int i = 0; i < rightDepth; i++) {
-                        leftRes.get(leftDepth - rightDepth + i).addAll(rightRes.get(i));
-                        list1.add(leftRes.get(leftDepth - rightDepth + i));
-                    }
-                    list1.add(list);
-                    return list1;
-                } else if (leftDepth < rightDepth) {
-                    for (int i = 0; i < rightDepth - leftDepth; i++) {
-                        list1.add(rightRes.get(i));
-                    }
-                    for (int i = 0; i < leftDepth; i++) {
-                        leftRes.get(i).addAll(rightRes.get(rightDepth - leftDepth + i));
-                        list1.add(leftRes.get(i));
-                    }
-                    list1.add(list);
-                    return list1;
-                } else {
-                    for (int i = 0; i < leftDepth; i++) {
-                        leftRes.get(i).addAll(rightRes.get(i));
-                        list1.add(leftRes.get(i));
-                    }
-                    list1.add(list);
-                    return list1;
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        /**
+         * 逐层遍历
+         */
+        while (!queue.isEmpty()) {
+            /**
+             * 该层节点的个数
+             */
+            int size = queue.size();
+            /**
+             * 保存该层所有节点的值
+             */
+            List<Integer> level = new LinkedList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
                 }
             }
+            /**
+             * 因为自底向上逐层遍历，所以每次将该层的列表插入到result头部的位置
+             */
+            result.add(0, level);
         }
+        return result;
     }
 }
