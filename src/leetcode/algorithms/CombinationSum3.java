2aa6261125f2d8b7ddef8510c2b26c004e2190a3
Baltan <617640006@qq.com>
2020-09-11 10:50:32 +0800

    216. Combination Sum III

--- src/leetcode/algorithms/CombinationSum3.java
+++ src/leetcode/algorithms/CombinationSum3.java
@@ -25,55 +25,53 @@ public class CombinationSum3 {
 
     public static List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> nums = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
+        List<Integer> candidates = new ArrayList<>();
 
         for (int i = 1; i <= 9; i++) {
-            nums.add(i);
+            candidates.add(i);
         }
 
-        help(result, list, nums, n, k, 1);
+        dfs(result, new ArrayList<>(), candidates, n, k, 0);
         return result;
     }
 
-    public static void help(List<List<Integer>> result, List<Integer> list, List<Integer> nums, int n,
-                            int k, int min) {
+    /**
+     * @param result
+     * @param temp
+     * @param candidates
+     * @param n          当前组合temp中的所有值的和
+     * @param k          当前组合中剩余需要添加数字的个数
+     * @param startIndex 从candidates的第startIndex索引开始尝试追加数字到temp中
+     */
+    public static void dfs(List<List<Integer>> result, List<Integer> temp, List<Integer> candidates, int n,
+                           int k, int startIndex) {
         /**
-         * 如果n和k都为0，此时组合list已满足要求，将组合加入result
+         * 如果n和k都为0，此时组合temp已满足要求，将组合加入result
          */
         if (k == 0 && n == 0) {
-            result.add(new ArrayList<>(list));
+            result.add(new ArrayList<>(temp));
             return;
         }
         /**
-         * 如果组合以包含k个数，但k个数的和不为n，组合list无法满足要求
+         * 如果组合已包含k个数，但k个数的和不为n，组合temp无法满足要求
          */
-        if (k <= 0 && n != 0) {
+        if (k == 0 && n != 0) {
             return;
         }
         /**
-         * 如果组合不足k个数，但组合中数字的和已达到n，组合list无法满足要求
+         * 如果组合不足k个数，但组合temp中数字的和已达到n，组合temp无法满足要求
          */
         if (n <= 0 && k != 0) {
             return;
         }
 
-        int length = nums.size();
+        int length = candidates.size();
 
-        for (int i = 0; i < length; i++) {
-            int num = nums.get(i);
-            /**
-             * 将不小于当前组合可以添加的最小值的数字加入组合中
-             */
-            if (num < min) {
-                continue;
-            }
-
-            list.add(num);
-            nums.remove(new Integer(num));
-            help(result, list, nums, n - num, k - 1, num + 1);
-            nums.add(i, num);
-            list.remove(list.size() - 1);
+        for (int i = startIndex; i < length; i++) {
+            int num = candidates.get(i);
+            temp.add(num);
+            dfs(result, temp, candidates, n - num, k - 1, i + 1);
+            temp.remove(temp.size() - 1);
         }
     }
 }
