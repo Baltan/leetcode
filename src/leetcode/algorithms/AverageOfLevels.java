25f2b9e4cd6b01c77a5fcfd4cc525f502e2c8839
Baltan <617640006@qq.com>
2020-09-12 09:54:09 +0800

    637. Average of Levels in Binary Tree

--- src/leetcode/algorithms/AverageOfLevels.java
+++ src/leetcode/algorithms/AverageOfLevels.java
@@ -1,11 +1,9 @@
 package leetcode.algorithms;
 
 import leetcode.entity.TreeNode;
+import leetcode.util.BinaryTreeUtils;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * Description: 637. Average of Levels in Binary Tree
@@ -15,49 +13,50 @@ import java.util.Queue;
  */
 public class AverageOfLevels {
     public static void main(String[] args) {
-        TreeNode root1 = new TreeNode(3);
-        TreeNode node1 = new TreeNode(9);
-        TreeNode node2 = new TreeNode(20);
-        TreeNode node3 = new TreeNode(15);
-        TreeNode node4 = new TreeNode(7);
-        root1.left = node1;
-        root1.right = node2;
-        node2.left = node3;
-        node2.right = node4;
+        TreeNode root1 = BinaryTreeUtils.arrayToBinaryTree(new Integer[]{3, 9, 20, null, null, 15, 7}, 0);
         System.out.println(averageOfLevels(root1));
 
-        TreeNode root2 = new TreeNode(5);
-        TreeNode node5 = new TreeNode(14);
-        TreeNode node6 = new TreeNode(1);
-        root2.left = node5;
-        node5.left = node6;
+        TreeNode root2 = BinaryTreeUtils.arrayToBinaryTree(new Integer[]{5, 14, 1}, 0);
         System.out.println(averageOfLevels(root2));
     }
 
     public static List<Double> averageOfLevels(TreeNode root) {
-        List<Double> list = new ArrayList<>();
+        List<Double> result = new ArrayList<>();
+
         if (root == null) {
-            return list;
+            return result;
         }
-        Queue<TreeNode> queue = new ArrayDeque<>();
+
+        Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
+
         while (!queue.isEmpty()) {
-            long levelSum = 0;
-            int levelNodeNum = queue.size();
-            for (int i = 0; i < levelNodeNum; i++) {
-                TreeNode current = queue.poll();
-                levelSum += current.val;
-                if (current.left != null) {
-                    queue.add(current.left);
+            /**
+             * 该层所有节点的和
+             */
+            long sum = 0;
+            /**
+             * 该层的节点数
+             */
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+
+                if (node.left != null) {
+                    queue.offer(node.left);
                 }
-                if (current.right != null) {
-                    queue.add(current.right);
+
+                if (node.right != null) {
+                    queue.offer(node.right);
                 }
             }
-            if (levelNodeNum != 0) {
-                list.add((double) levelSum / levelNodeNum);
+
+            if (size != 0) {
+                result.add((double) sum / size);
             }
         }
-        return list;
+        return result;
     }
 }
