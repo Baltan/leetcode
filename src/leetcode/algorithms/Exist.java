95284303c6706ef493112fbf5b3f038ad419316d
Baltan <617640006@qq.com>
2020-09-13 10:00:47 +0800

    79. Word Search

--- src/leetcode/algorithms/Exist.java
+++ src/leetcode/algorithms/Exist.java
@@ -89,24 +89,33 @@ public class Exist {
                 "baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
     }
 
-    private static boolean result = false;
+    private static boolean flag = false;
 
     public static boolean exist(char[][] board, String word) {
         /**
          * 因为把result定义成了static变量，为保证每个测试用例result的初始值都为false
          */
-        result = false;
-        int length = board.length;
-        int width = board[0].length;
+        flag = false;
+        int rows = board.length;
+        int cols = board[0].length;
+        char[] charArray = word.toCharArray();
 
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < width; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    boolean[][] book = new boolean[length][width];
-                    book[i][j] = true;
-                    dfs(board, book, word.substring(1), i, j);
-
-                    if (result) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] == charArray[0]) {
+                    /**
+                     * 标记board中的字符是否已被使用过
+                     */
+                    boolean[][] isVisited = new boolean[rows][cols];
+                    isVisited[i][j] = true;
+                    /**
+                     * 从charArray[1]开始继续向下查找
+                     */
+                    dfs(board, isVisited, charArray, 1, i, j);
+                    /**
+                     * 如果已经在board中找到了单词，直接返回true
+                     */
+                    if (flag) {
                         return true;
                     }
 
@@ -116,38 +125,52 @@ public class Exist {
         return false;
     }
 
-    public static void dfs(char[][] board, boolean[][] book, String word, int startX, int startY) {
-        if (word.length() == 0) {
-            result = true;
+    /**
+     * @param board
+     * @param isVisited 标记board[row][col]这个字符是否已被使用过
+     * @param charArray
+     * @param index     当前要匹配的字符为charArray[index]
+     * @param row
+     * @param col
+     */
+    public static void dfs(char[][] board, boolean[][] isVisited, char[] charArray, int index, int row,
+                           int col) {
+        /**
+         * 已在board中找到了charArray中的所有字符，直接返回true
+         */
+        if (index == charArray.length) {
+            flag = true;
             return;
         }
 
-        int length = board.length;
-        int width = board[0].length;
+        int rows = board.length;
+        int cols = board[0].length;
 
-        if (startX - 1 >= 0 && board[startX - 1][startY] == word.charAt(0) && !book[startX - 1][startY] &&
-                !result) {
-            book[startX - 1][startY] = true;
-            dfs(board, book, word.substring(1), startX - 1, startY);
-            book[startX - 1][startY] = false;
+        if (row - 1 >= 0 && board[row - 1][col] == charArray[index] && !isVisited[row - 1][col] && !flag) {
+            isVisited[row - 1][col] = true;
+            dfs(board, isVisited, charArray, index + 1, row - 1, col);
+            isVisited[row - 1][col] = false;
         }
-        if (startX + 1 < length && board[startX + 1][startY] == word.charAt(0) && !book[startX + 1][startY] &&
-                !result) {
-            book[startX + 1][startY] = true;
-            dfs(board, book, word.substring(1), startX + 1, startY);
-            book[startX + 1][startY] = false;
+
+        if (row + 1 < rows && board[row + 1][col] == charArray[index] && !isVisited[row + 1][col] &&
+                !flag) {
+            isVisited[row + 1][col] = true;
+            dfs(board, isVisited, charArray, index + 1, row + 1, col);
+            isVisited[row + 1][col] = false;
         }
-        if (startY - 1 >= 0 && board[startX][startY - 1] == word.charAt(0) && !book[startX][startY - 1] &&
-                !result) {
-            book[startX][startY - 1] = true;
-            dfs(board, book, word.substring(1), startX, startY - 1);
-            book[startX][startY - 1] = false;
+
+        if (col - 1 >= 0 && board[row][col - 1] == charArray[index] && !isVisited[row][col - 1] &&
+                !flag) {
+            isVisited[row][col - 1] = true;
+            dfs(board, isVisited, charArray, index + 1, row, col - 1);
+            isVisited[row][col - 1] = false;
         }
-        if (startY + 1 < width && board[startX][startY + 1] == word.charAt(0) && !book[startX][startY + 1] &&
-                !result) {
-            book[startX][startY + 1] = true;
-            dfs(board, book, word.substring(1), startX, startY + 1);
-            book[startX][startY + 1] = false;
+
+        if (col + 1 < cols && board[row][col + 1] == charArray[index] && !isVisited[row][col + 1] &&
+                !flag) {
+            isVisited[row][col + 1] = true;
+            dfs(board, isVisited, charArray, index + 1, row, col + 1);
+            isVisited[row][col + 1] = false;
         }
     }
 }
