27bdb0c5222be485cf0b3c9a23724c05520d0f78
Baltan <617640006@qq.com>
2020-10-08 21:47:12 +0800

    344. Reverse String

--- src/leetcode/algorithms/ReverseString.java
+++ src/leetcode/algorithms/ReverseString.java
@@ -1,5 +1,7 @@
 package leetcode.algorithms;
 
+import java.util.Arrays;
+
 /**
  * Description: 344. Reverse String
  *
@@ -8,15 +10,36 @@ package leetcode.algorithms;
  */
 public class ReverseString {
     public static void main(String[] args) {
-        String s1 = "hello";
-        System.out.println(reverseString(s1));
+        char[] s1 = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(s1);
+        System.out.println(Arrays.toString(s1));
+
+        char[] s2 = {'H', 'a', 'n', 'n', 'a', 'h'};
+        reverseString(s2);
+        System.out.println(Arrays.toString(s2));
+
+        char[] s3 = {};
+        reverseString(s3);
+        System.out.println(Arrays.toString(s3));
+
+        char[] s4 = {'a'};
+        reverseString(s4);
+        System.out.println(Arrays.toString(s4));
     }
 
-    public static String reverseString(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = s.length() - 1; i >= 0; i--) {
-            sb.append(s, i, i + 1);
+    public static void reverseString(char[] s) {
+        if (s.length == 0) {
+            return;
         }
-        return sb.toString();
+
+        int mid = (s.length - 1) / 2;
+        /**
+         * 首尾配对交换字符
+         */
+        for (int i = 0; i <= mid; i++) {
+            char temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
+        }
     }
 }
