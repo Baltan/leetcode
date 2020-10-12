8a5975af72f3f1569cf880d3412bfe397c7a0c2c
Baltan <617640006@qq.com>
2020-10-13 07:54:44 +0800

    24. Swap Nodes in Pairs

--- src/leetcode/algorithms/SwapPairs.java
+++ src/leetcode/algorithms/SwapPairs.java
@@ -1,6 +1,7 @@
 package leetcode.algorithms;
 
 import leetcode.entity.ListNode;
+import leetcode.util.ListNodeUtils;
 import leetcode.util.OutputUtils;
 
 /**
@@ -11,38 +12,30 @@ import leetcode.util.OutputUtils;
  */
 public class SwapPairs {
     public static void main(String[] args) {
-        ListNode head1 = new ListNode(1);
-        ListNode node1 = new ListNode(2);
-        ListNode node2 = new ListNode(3);
-        ListNode node3 = new ListNode(4);
-        head1.next = node1;
-        node1.next = node2;
-        node2.next = node3;
+        ListNode head1 = ListNodeUtils.arrayToListNode(new int[]{1, 2, 3, 4});
         OutputUtils.printListNode(swapPairs(head1));
 
-        ListNode head2 = new ListNode(1);
-        ListNode node4 = new ListNode(2);
-        ListNode node5 = new ListNode(3);
-        head2.next = node4;
-        node4.next = node5;
+        ListNode head2 = ListNodeUtils.arrayToListNode(new int[]{1, 2, 3});
         OutputUtils.printListNode(swapPairs(head2));
     }
 
     public static ListNode swapPairs(ListNode head) {
+        /**
+         * 如果链表中的节点数不超过1个，则直接返回自身
+         */
         if (head == null || head.next == null) {
             return head;
         }
-        if (head.next.next == null) {
-            ListNode temp = head;
-            head = head.next;
-            head.next = temp;
-            temp.next = null;
-            return head;
-        }
+        /**
+         * 如果链表中的节点数超过1个，则交换前面两个节点后，对后面的链表进行递归操作后再接在前面两个节点后面
+         */
         ListNode listNode = head.next.next;
         ListNode temp = head;
         head = head.next;
         head.next = temp;
+        /**
+         * 对后面的链表进行递归操作
+         */
         ListNode tail = swapPairs(listNode);
         temp.next = tail;
         return head;
