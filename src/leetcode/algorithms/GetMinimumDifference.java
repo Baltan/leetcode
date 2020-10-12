2e3f0001c73412f6ed0652a75c569650ff89e36c
Baltan <617640006@qq.com>
2020-10-12 09:00:01 +0800

    530. Minimum Absolute Difference in BST

--- src/leetcode/algorithms/GetMinimumDifference.java
+++ src/leetcode/algorithms/GetMinimumDifference.java
@@ -1,6 +1,7 @@
 package leetcode.algorithms;
 
 import leetcode.entity.TreeNode;
+import leetcode.util.BinaryTreeUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,38 +14,29 @@ import java.util.List;
  */
 public class GetMinimumDifference {
     public static void main(String[] args) {
-        TreeNode node1 = new TreeNode(1);
-        TreeNode node2 = new TreeNode(3);
-        TreeNode node3 = new TreeNode(2);
-
-        node1.right = node2;
-        node2.left = node3;
-
-        System.out.println(getMinimumDifference(node1));
+        TreeNode root1 = BinaryTreeUtils.arrayToBinaryTree(new Integer[]{1, null, 3, null, null, 2}, 0);
+        System.out.println(getMinimumDifference(root1));
     }
 
     public static int getMinimumDifference(TreeNode root) {
-        List<Integer> list = midOrder(root);
-        int difference = Integer.MAX_VALUE;
+        int result = Integer.MAX_VALUE;
+        List<Integer> list = inorder(root);
 
         for (int i = 0, size = list.size(); i < size - 1; i++) {
-            difference =
-                    list.get(i + 1) - list.get(i) < difference ? list.get(i + 1) - list.get(i) : difference;
+            result = Math.min(result, list.get(i + 1) - list.get(i));
         }
-        return difference;
+        return result;
     }
 
-    public static List<Integer> midOrder(TreeNode root) {
+    public static List<Integer> inorder(TreeNode root) {
         List<Integer> list = new ArrayList<>();
 
         if (root == null) {
             return list;
         }
-
-        list.addAll(midOrder(root.left));
+        list.addAll(inorder(root.left));
         list.add(root.val);
-        list.addAll(midOrder(root.right));
-
+        list.addAll(inorder(root.right));
         return list;
     }
 }
