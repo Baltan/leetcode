9fa43decb7fc707b48ac089444f355715310e576
Baltan <617640006@qq.com>
2020-10-21 23:02:45 +0800

    925. Long Pressed Name

--- src/leetcode/algorithms/IsLongPressedName.java
+++ src/leetcode/algorithms/IsLongPressedName.java
@@ -8,6 +8,10 @@ package leetcode.algorithms;
  */
 public class IsLongPressedName {
     public static void main(String[] args) {
+        System.out.println(isLongPressedName("zlexya", "aazlllllllllllllleexxxxxxxxxxxxxxxya"));
+        System.out.println(isLongPressedName("vtkgn", "vttkgnn"));
+        System.out.println(isLongPressedName("alex", "alexxr"));
+        System.out.println(isLongPressedName("alex", "aaleelx"));
         System.out.println(isLongPressedName("alex", "aaleex"));
         System.out.println(isLongPressedName("saeed", "ssaaedd"));
         System.out.println(isLongPressedName("leelee", "lleeelee"));
@@ -17,27 +21,44 @@ public class IsLongPressedName {
     public static boolean isLongPressedName(String name, String typed) {
         int nameLength = name.length();
         int typedLength = typed.length();
-        int index1 = 0;
-        int index2 = 0;
-        char previousChar = name.charAt(index1);
+        int nameIndex = 0;
+        int typedIndex = 0;
+        char[] nameChars = name.toCharArray();
+        char[] typedChars = typed.toCharArray();
 
-        while (index1 < nameLength) {
-            if (index2 >= typedLength) {
+        while (nameIndex < nameLength) {
+            /**
+             * 如果没有键入的字符了，则name无法被匹配完，返回false
+             */
+            if (typedIndex == typedLength) {
                 return false;
             }
-
-            char c1 = name.charAt(index1);
-            char c2 = typed.charAt(index2);
-
-            if (c2 != c1 && c2 != previousChar) {
-                return false;
-            } else if (c2 == c1) {
-                index2++;
-                index1++;
-                previousChar = c1;
-            } else if (c2 != c1 && c2 == previousChar) {
-                index2++;
+            /**
+             * 如果当前键入的字符和name指向的字符不匹配，考虑是否是重复键入字符了；否则继续判断下面的字符
+             */
+            if (typedChars[typedIndex] != nameChars[nameIndex]) {
+                /**
+                 * 如果键入的字符和前一个字符相同，则是重复键入字符了，继续判断下一个键入的字符；否则不是
+                 * 重复键入字符，即键入了错误的字符，返回false
+                 */
+                if (typedIndex > 0 && typedChars[typedIndex] == typedChars[typedIndex - 1]) {
+                    typedIndex++;
+                } else {
+                    return false;
+                }
+            } else {
+                typedIndex++;
+                nameIndex++;
             }
+        }
+        /**
+         * 判断剩余键入的字符是否是重复键入的
+         */
+        while (typedIndex < typedLength) {
+            if (typedChars[typedIndex] != typedChars[typedIndex - 1]) {
+                return false;
+            }
+            typedIndex++;
         }
         return true;
     }
