c1aa4d2c90839f1655fcb03e911f5e6ae85f8b1e
Baltan <617640006@qq.com>
2020-10-24 23:37:41 +0800

    1024. Video Stitching

--- src/leetcode/algorithms/VideoStitching.java
+++ src/leetcode/algorithms/VideoStitching.java
@@ -26,6 +26,9 @@ public class VideoStitching {
     }
 
     public static int videoStitching(int[][] clips, int T) {
+        /**
+         * 将所有视频片段先按照起始时间升序排列，起始时间相同的情况下再按照结束时间降序排列
+         */
         Arrays.sort(clips, (c1, c2) -> {
             if (c1[0] == c2[0]) {
                 return c2[1] - c1[1];
@@ -33,18 +36,29 @@ public class VideoStitching {
                 return c1[0] - c2[0];
             }
         });
-
+        /**
+         * 如果第一个片段的起始时间大于0，则我们无法得到从0时刻开始的完整视频，直接返回false
+         */
         if (clips[0][0] > 0) {
             return -1;
         }
 
         int result = 1;
+        /**
+         * 当前已拿到的视频片段的最后结束时刻
+         */
         int end = clips[0][1];
         int index = 0;
         int length = clips.length;
 
         while (end < T) {
+            /**
+             * 这轮筛选可以得到的最大视频结束时刻
+             */
             int endTime = -1;
+            /**
+             * 尽可能地选择能和已有的视频衔接并且结束时间尽可能大的视频片段
+             */
             for (int i = index + 1; i < length; i++) {
                 if (clips[i][0] <= end && clips[i][1] > endTime) {
                     endTime = clips[i][1];
@@ -53,9 +67,15 @@ public class VideoStitching {
                     break;
                 }
             }
+            /**
+             * 无法得到满足条件的视频片段，则视频的最后一部分无法拼接上，直接返回false
+             */
             if (endTime == -1) {
                 return -1;
             }
+            /**
+             * 更新当前已拿到的视频片段的最后结束时刻
+             */
             end = endTime;
             result++;
         }
