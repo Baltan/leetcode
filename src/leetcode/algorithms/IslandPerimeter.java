672757191832b3b36b9dc1546d4a22c9d484f2f3
Baltan <617640006@qq.com>
2020-10-30 23:56:20 +0800

    463. Island Perimeter

--- src/leetcode/algorithms/IslandPerimeter.java
+++ src/leetcode/algorithms/IslandPerimeter.java
@@ -8,33 +8,37 @@ package leetcode.algorithms;
  */
 public class IslandPerimeter {
     public static void main(String[] args) {
-        int[][] arr1 = {{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}};
-        System.out.println(islandPerimeter(arr1));
-        int[][] arr2 = {{}, {}};
-        System.out.println(islandPerimeter(arr2));
+        int[][] grid1 = {{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}};
+        System.out.println(islandPerimeter(grid1));
+
+        int[][] grid2 = {{}, {}};
+        System.out.println(islandPerimeter(grid2));
     }
 
     public static int islandPerimeter(int[][] grid) {
+        int result = 0;
         int rows = grid.length;
         int cols = grid[0].length;
-        int totalPerimeter = 0;
+
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == 1) {
-                    totalPerimeter += 4;
-                    if (i < rows - 1) {
-                        if (grid[i + 1][j] == 1) {
-                            totalPerimeter -= 2;
-                        }
+                    result += 4;
+                    /**
+                     * 如果当前格子为陆地且下方格子也为陆地，则它们的交接的边要被减掉
+                     */
+                    if (i < rows - 1 && grid[i + 1][j] == 1) {
+                        result -= 2;
                     }
-                    if (j < cols - 1) {
-                        if (grid[i][j + 1] == 1) {
-                            totalPerimeter -= 2;
-                        }
+                    /**
+                     * 如果当前格子为陆地且右方格子也为陆地，则它们的交接的边要被减掉
+                     */
+                    if (j < cols - 1 && grid[i][j + 1] == 1) {
+                        result -= 2;
                     }
                 }
             }
         }
-        return totalPerimeter;
+        return result;
     }
 }
