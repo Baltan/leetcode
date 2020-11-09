c1001ab58ca01ddcf5dcb6c815311b2acfa27a07
Baltan <617640006@qq.com>
2020-11-09 23:17:11 +0800

    973. K Closest Points to Origin

--- src/leetcode/algorithms/KClosest.java
+++ src/leetcode/algorithms/KClosest.java
@@ -15,6 +15,8 @@ public class KClosest {
         int[][] points1 = {{1, 3}, {-2, 2}};
         OutputUtils.print2DIntegerArray(kClosest(points1, 1));
 
+        System.out.println("------------------------------");
+
         int[][] points2 = {{3, 3}, {5, -1}, {-2, 4}};
         OutputUtils.print2DIntegerArray(kClosest(points2, 2));
 
@@ -25,24 +27,43 @@ public class KClosest {
     public static int[][] kClosest(int[][] points, int K) {
         int[][] result = new int[K][2];
         int length = points.length;
-        int[] distanceArray = new int[length];
+        /**
+         * 保存points中所有点到原点的距离
+         */
+        int[] sortedDistances = new int[length];
+        /**
+         * distances[i]保存points[i]到原点的距离
+         */
+        int[] distances = new int[length];
         int index = 0;
 
         for (int i = 0; i < length; i++) {
             int[] point = points[i];
-            distanceArray[i] = point[0] * point[0] + point[1] * point[1];
+            int distance = point[0] * point[0] + point[1] * point[1];
+            sortedDistances[i] = distance;
+            distances[i] = distance;
         }
-
-        Arrays.sort(distanceArray);
-
-        int KthDistance = distanceArray[K - 1];
+        /**
+         * 将points中所有点到原点的距离按照升序排列
+         */
+        Arrays.sort(sortedDistances);
+        /**
+         * points中距离原点第K远的点到原点的距离
+         */
+        int KthDistance = sortedDistances[K - 1];
 
         for (int i = 0; i < length; i++) {
             int[] point = points[i];
-            int distance = point[0] * point[0] + point[1] * point[1];
+            int distance = distances[i];
 
             if (distance <= KthDistance) {
                 result[index++] = point;
+                /**
+                 * 已经找到K个符合要求的点，退出循环
+                 */
+                if (index == K) {
+                    break;
+                }
             }
         }
         return result;
