3cefdaf77a32e0d6513e2e42fc5a1988bf5cf60d
Baltan <617640006@qq.com>
2020-11-10 21:12:20 +0800

    31. Next Permutation

--- src/leetcode/algorithms/NextPermutation.java
+++ src/leetcode/algorithms/NextPermutation.java
@@ -10,26 +10,56 @@ import leetcode.util.OutputUtils;
  */
 public class NextPermutation {
     public static void main(String[] args) {
-        nextPermutation(new int[]{1, 2, 3});
-        nextPermutation(new int[]{3, 2, 1});
-        nextPermutation(new int[]{2, 3, 1});
-        nextPermutation(new int[]{1, 1, 5});
-        nextPermutation(new int[]{1});
-        nextPermutation(new int[]{1, 2});
-        nextPermutation(new int[]{2, 1});
-        nextPermutation(new int[]{2, 4, 5, 3, 6, 3, 2, 1, 6});
+        int[] nums1 = {1, 2, 3};
+        nextPermutation(nums1);
+        OutputUtils.print1DIntegerArray(nums1);
+
+        int[] nums2 = {3, 2, 1};
+        nextPermutation(nums2);
+        OutputUtils.print1DIntegerArray(nums2);
+
+        int[] nums3 = {2, 3, 1};
+        nextPermutation(nums3);
+        OutputUtils.print1DIntegerArray(nums3);
+
+        int[] nums4 = {1, 1, 5};
+        nextPermutation(nums4);
+        OutputUtils.print1DIntegerArray(nums4);
+
+        int[] nums5 = {1};
+        nextPermutation(nums5);
+        OutputUtils.print1DIntegerArray(nums5);
+
+        int[] nums6 = {1, 2};
+        nextPermutation(nums6);
+        OutputUtils.print1DIntegerArray(nums6);
+
+        int[] nums7 = {2, 1};
+        nextPermutation(nums7);
+        OutputUtils.print1DIntegerArray(nums7);
+
+        int[] nums8 = {2, 4, 5, 3, 6, 3, 2, 1, 6};
+        nextPermutation(nums8);
+        OutputUtils.print1DIntegerArray(nums8);
     }
 
     public static void nextPermutation(int[] nums) {
         int index = -1;
+        /**
+         * 找到索引最大的相邻两数顺序排列的一对数字
+         */
         for (int i = nums.length - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
                 index = i;
                 break;
             }
         }
+
         if (index != -1) {
-            for (int i = nums.length - 2; i >= index + 1; i--) {
+            /**
+             * 冒泡排序将nums中索引index之后部分的数字按照升序排列
+             */
+            for (int i = nums.length - 2; i > index; i--) {
                 for (int j = index + 1; j <= i; j++) {
                     if (nums[j] > nums[j + 1]) {
                         int temp = nums[j];
@@ -38,6 +68,9 @@ public class NextPermutation {
                     }
                 }
             }
+            /**
+             * 找到nums中索引index之后部分的第一个大于nums[index]的数字nums[i]，交换两数
+             */
             for (int i = index + 1; i < nums.length; i++) {
                 if (nums[i] > nums[index]) {
                     int temp = nums[i];
@@ -47,6 +80,9 @@ public class NextPermutation {
                 }
             }
         } else {
+            /**
+             * 冒泡排序将nums中的所有数字按照升序排列
+             */
             for (int i = nums.length - 2; i >= 0; i--) {
                 for (int j = 0; j <= i; j++) {
                     if (nums[j] > nums[j + 1]) {
@@ -57,6 +93,5 @@ public class NextPermutation {
                 }
             }
         }
-        OutputUtils.print1DIntegerArray(nums);
     }
 }
