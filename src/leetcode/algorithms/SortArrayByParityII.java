5390f59e9c1f1198db1ef3118dfb706c9531392b
Baltan <617640006@qq.com>
2020-11-12 21:08:24 +0800

    922. Sort Array By Parity II

--- src/leetcode/algorithms/SortArrayByParityII.java
+++ src/leetcode/algorithms/SortArrayByParityII.java
@@ -22,19 +22,25 @@ public class SortArrayByParityII {
         int evenIndex = 0;
         int oddIndex = length - 1;
 
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < length; ) {
+            /**
+             * 如果i为偶数但是A[i]为奇数，就把A[i]和A[oddIndex]交换位置，从而使得奇数A[i]放到了奇数
+             * 索引位置oddIndex上；如果i为奇数但是A[i]为偶数，就把A[i]和A[evenIndex]交换位置，从而
+             * 使得偶数A[i]放到了偶数索引位置evenIndex上；如果i和A[i]奇偶性相同，则已经符合题意，继
+             * 续判断下一个数即可
+             */
             if ((i & 1) == 0 && (A[i] & 1) == 1) {
                 int temp = A[oddIndex];
                 A[oddIndex] = A[i];
                 A[i] = temp;
-                i--;
                 oddIndex -= 2;
             } else if ((i & 1) == 1 && (A[i] & 1) == 0) {
                 int temp = A[evenIndex];
                 A[evenIndex] = A[i];
                 A[i] = temp;
-                i--;
                 evenIndex += 2;
+            } else {
+                i++;
             }
         }
         return A;
