5624d4a858ff6b65c107807f0a17ca4571be02b4
Baltan <617640006@qq.com>
2021-12-30 14:10:27 +0800

    2121. Intervals Between Identical Elements

--- src/leetcode/algorithms/GetDistances.java
+++ src/leetcode/algorithms/GetDistances.java
@@ -17,22 +17,58 @@ public class GetDistances {
     public static long[] getDistances(int[] arr) {
         int length = arr.length;
         long[] result = new long[length];
-        Map<Integer, List<Integer>> indexMap = new HashMap<>();
+        /**
+         * 数字i -> 数字i在arr中从左向右出现的索引位置的前缀和列表
+         */
+        Map<Integer, List<Long>> prefixSumMap = new HashMap<>();
+        /**
+         * 数字i -> 已经遍历到的i的个数
+         */
+        Map<Integer, Integer> timeMap = new HashMap<>();
 
         for (int i = 0; i < length; i++) {
             int value = arr[i];
-            List<Integer> indexList = indexMap.computeIfAbsent(value, x -> new ArrayList<>());
-            indexList.add(i);
+            /**
+             * 数字i在arr中从左向右出现的索引位置的前缀和列表
+             */
+            List<Long> indexList = prefixSumMap.computeIfAbsent(value, x -> {
+                ArrayList<Long> prefixSumList = new ArrayList<>();
+                prefixSumList.add(0L);
+                return prefixSumList;
+            });
+            indexList.add(indexList.get(indexList.size() - 1) + i);
         }
 
         for (int i = 0; i < length; i++) {
             int value = arr[i];
-            long interval = 0L;
-
-            for (int index : indexMap.get(value)) {
-                interval += Math.abs(index - i);
-            }
-            result[i] = interval;
+            /**
+             * 遍历到arr[i]为止value是第time次出现
+             */
+            int time = timeMap.getOrDefault(value, 0) + 1;
+            timeMap.put(value, time);
+            /**
+             * 数字value在arr中从左向右出现的索引位置的前缀和列表
+             */
+            List<Long> prefixSumList = prefixSumMap.get(value);
+            /**
+             * arr[i]之后value在arr中出现的次数
+             */
+            int suffixLength = prefixSumList.size() - 1 - time;
+            /**
+             * arr[i]之前value在arr中出现的次数
+             */
+            int prefixLength = time - 1;
+            /**
+             * arr[i]之后value在arr中出现的索引位置和当前这个value的索引位置之差的和，乘法运算中必须乘以1L，否则会向下转型为Integer类型导致精度丢失
+             */
+            long suffixDiff =
+                    prefixSumList.get(prefixSumList.size() - 1) - prefixSumList.get(time) -
+                            1L * i * suffixLength;
+            /**
+             * arr[i]之前value在arr中出现的索引位置和当前这个value的索引位置之差的和，乘法运算中必须乘以1L，否则会向下转型为Integer类型导致精度丢失
+             */
+            long prefixDiff = 1L * i * prefixLength - prefixSumList.get(prefixLength);
+            result[i] = suffixDiff + prefixDiff;
         }
         return result;
     }
