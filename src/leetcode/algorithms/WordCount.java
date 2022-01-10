fc15642acb3d286053d8de27f21c7c0490aa8a9e
Baltan <617640006@qq.com>
2022-01-10 13:40:05 +0800

    2135. Count Words Obtained After Adding a Letter

--- src/leetcode/algorithms/WordCount.java
+++ src/leetcode/algorithms/WordCount.java
@@ -1,5 +1,9 @@
 package leetcode.algorithms;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Description: 2135. Count Words Obtained After Adding a Letter
  *
@@ -20,37 +24,27 @@ public class WordCount {
 
     public static int wordCount(String[] startWords, String[] targetWords) {
         int result = 0;
-        int[][] countArrays = new int[startWords.length][26];
-
-        for (int i = 0; i < startWords.length; i++) {
-            int[] countArray = new int[26];
-            countArrays[i] = countArray;
-
-            for (char c : startWords[i].toCharArray()) {
-                countArray[c - 'a']++;
-            }
+        Set<String> startWordSet = new HashSet<>();
+        /**
+         * 将startWords中的每一个单词按照自身的字母重新排序后保存
+         */
+        for (String startWord : startWords) {
+            char[] charArray = startWord.toCharArray();
+            Arrays.sort(charArray);
+            startWordSet.add(new String(charArray));
         }
 
         for (String targetWord : targetWords) {
-            int[] countArray = new int[26];
+            char[] charArray = targetWord.toCharArray();
+            int length = charArray.length;
+            Arrays.sort(charArray);
+            /**
+             * 将targetWord按照自身的字母重新排序，并去掉其中一个字母后，判断startWordSet中是否存在对应的单词
+             */
+            for (int i = 0; i < length; i++) {
+                String word = new String(charArray, 0, i) + new String(charArray, i + 1, length - i - 1);
 
-            for (char c : targetWord.toCharArray()) {
-                countArray[c - 'a']++;
-            }
-
-            mid:
-            for (int[] array : countArrays) {
-                int count = 0;
-
-                for (int i = 0; i < 26; i++) {
-                    if (countArray[i] < array[i] || countArray[i] - array[i] > 1) {
-                        continue mid;
-                    } else if (countArray[i] - array[i] == 1) {
-                        count++;
-                    }
-                }
-
-                if (count == 1) {
+                if (startWordSet.contains(word)) {
                     result++;
                     break;
                 }
