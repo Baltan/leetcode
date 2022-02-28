8533fb32cb05ff6d532ccca507ee027a03366148
Baltan <617640006@qq.com>
2022-02-28 09:10:14 +0800

    2186. Minimum Number of Steps to Make Two Strings Anagram II

---
+++ src/leetcode/algorithms/MinSteps3.java
@@ -0,0 +1,43 @@
+package leetcode.algorithms;
+
+/**
+ * Description: 2186. Minimum Number of Steps to Make Two Strings Anagram II
+ *
+ * @author Baltan
+ * @date 2022/2/27 22:34
+ */
+public class MinSteps3 {
+    public static void main(String[] args) {
+        System.out.println(minSteps("leetcode", "coats"));
+        System.out.println(minSteps("night", "thing"));
+    }
+
+    public static int minSteps(String s, String t) {
+        int result = 0;
+        int letterCount = 26;
+        /**
+         * 统计字符串s中每个字符出现的次数
+         */
+        int[] sCount = new int[letterCount];
+        /**
+         * 统计字符串t中每个字符出现的次数
+         */
+        int[] tCount = new int[letterCount];
+
+        for (char c : s.toCharArray()) {
+            sCount[c - 'a']++;
+        }
+
+        for (char c : t.toCharArray()) {
+            tCount[c - 'a']++;
+        }
+
+        for (int i = 0; i < letterCount; i++) {
+            /**
+             * 同一个字符在两个字符串中出现的次数之差是需要在较少的那个字符串后面追加这个字符的个数
+             */
+            result += Math.abs(sCount[i] - tCount[i]);
+        }
+        return result;
+    }
+}
