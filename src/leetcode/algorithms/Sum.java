b31b26590d1cca8fcb58ce029c9740ac297039d4
Baltan <617640006@qq.com>
2022-04-13 09:10:49 +0800

    2235. Add Two Integers

---
+++ src/leetcode/algorithms/Sum.java
@@ -0,0 +1,20 @@
+package leetcode.algorithms;
+
+/**
+ * Description: 2235. Add Two Integers
+ *
+ * @author Baltan
+ * @date 2022/4/12 22:44
+ * @see GetSum
+ * @see leetcode.interview.Add
+ */
+public class Sum {
+    public static void main(String[] args) {
+        System.out.println(sum(12, 5));
+        System.out.println(sum(-10, 4));
+    }
+
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
