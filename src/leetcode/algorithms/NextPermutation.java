37bcc33c27ac5c3638545e03e5e374dbf8eac120
Baltan <617640006@qq.com>
2022-04-17 19:16:14 +0800

    31. Next Permutation

--- src/leetcode/algorithms/NextPermutation.java
+++ src/leetcode/algorithms/NextPermutation.java
@@ -7,6 +7,7 @@ import leetcode.util.OutputUtils;
  *
  * @author Baltan
  * @date 2018/9/3 10:05
+ * @see MaxCompatibilitySum
  */
 public class NextPermutation {
     public static void main(String[] args) {
