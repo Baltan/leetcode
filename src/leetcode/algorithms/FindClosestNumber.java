a5944702fd4bf03794f0f1590bf0d7d0a52e77cc
Baltan <617640006@qq.com>
2022-04-20 22:42:20 +0800

    2239. Find Closest Number to Zero

--- src/leetcode/algorithms/FindClosestNumber.java
+++ src/leetcode/algorithms/FindClosestNumber.java
@@ -15,11 +15,18 @@ public class FindClosestNumber {
 
     public static int findClosestNumber(int[] nums) {
         int result = Integer.MIN_VALUE;
+        /**
+         * 已遍历的数字与0的最小差距
+         */
         int diff = Integer.MAX_VALUE;
 
         for (int num : nums) {
             if (num == 0) {
                 return 0;
+                /**
+                 * 因为有多个答案时，返回较大值，即存在绝对值相同的正负两个数时返回正数，所以如果num为正数且num与0的差距为
+                 * diff，即num本身等于diff时，也应该替换之前得到的最接近0的数字
+                 */
             } else if (num > 0 && num <= diff) {
                 result = num;
                 diff = num;
