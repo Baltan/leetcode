02c06c4870ad5c2bb021455221de7bf5c5232888
Baltan <617640006@qq.com>
2022-05-04 14:24:10 +0800

    1882. Process Tasks Using Servers —— Time Limit Exceeded

---
+++ src/leetcode/algorithms/AssignTasks.java
@@ -0,0 +1,80 @@
+package leetcode.algorithms;
+
+import leetcode.util.OutputUtils;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * Description: 1882. Process Tasks Using Servers
+ *
+ * @author Baltan
+ * @date 2022/5/4 12:57
+ */
+public class AssignTasks {
+    public static void main(String[] args) {
+        int[] servers1 = {3, 3, 2};
+        int[] tasks1 = {1, 2, 3, 2, 1, 2};
+        OutputUtils.print1DIntegerArray(assignTasks(servers1, tasks1));
+
+        int[] servers2 = {5, 1, 4, 3, 2};
+        int[] tasks2 = {2, 1, 2, 4, 5, 2, 1};
+        OutputUtils.print1DIntegerArray(assignTasks(servers2, tasks2));
+
+        int[] servers3 = {10, 63, 95, 16, 85, 57, 83, 95, 6, 29, 71};
+        int[] tasks3 = {70, 31, 83, 15, 32, 67, 98, 65, 56, 48, 38, 90, 5};
+        OutputUtils.print1DIntegerArray(assignTasks(servers3, tasks3));
+
+        int[] servers4 =
+                {338, 890, 301, 532, 284, 930, 426, 616, 919, 267, 571, 140, 716, 859, 980, 469, 628, 490,
+                        195, 664, 925, 652, 503, 301, 917, 563, 82, 947, 910, 451, 366, 190, 253, 516, 503,
+                        721, 889, 964, 506, 914, 986, 718, 520, 328, 341, 765, 922, 139, 911, 578, 86, 435,
+                        824, 321, 942, 215, 147, 985, 619, 865};
+        int[] tasks4 =
+                {773, 537, 46, 317, 233, 34, 712, 625, 336, 221, 145, 227, 194, 693, 981, 861, 317, 308, 400,
+                        2, 391, 12, 626, 265, 710, 792, 620, 416, 267, 611, 875, 361, 494, 128, 133, 157, 638,
+                        632, 2, 158, 428, 284, 847, 431, 94, 782, 888, 44, 117, 489, 222, 932, 494, 948, 405,
+                        44, 185, 587, 738, 164, 356, 783, 276, 547, 605, 609, 930, 847, 39, 579, 768, 59, 976,
+                        790, 612, 196, 865, 149, 975, 28, 653, 417, 539, 131, 220, 325, 252, 160, 761, 226,
+                        629, 317, 185, 42, 713, 142, 130, 695, 944, 40, 700, 122, 992, 33, 30, 136, 773, 124,
+                        203, 384, 910, 214, 536, 767, 859, 478, 96, 172, 398, 146, 713, 80, 235, 176, 876,
+                        983, 363, 646, 166, 928, 232, 699, 504, 612, 918, 406, 42, 931, 647, 795, 139, 933,
+                        746, 51, 63, 359, 303, 752, 799, 836, 50, 854, 161, 87, 346, 507, 468, 651, 32, 717,
+                        279, 139, 851, 178, 934, 233, 876, 797, 701, 505, 878, 731, 468, 884, 87, 921, 782,
+                        788, 803, 994, 67, 905, 309, 2, 85, 200, 368, 672, 995, 128, 734, 157, 157, 814, 327,
+                        31, 556, 394, 47, 53, 755, 721, 159, 843};
+        OutputUtils.print1DIntegerArray(assignTasks(servers4, tasks4));
+    }
+
+    public static int[] assignTasks(int[] servers, int[] tasks) {
+        int[] result = new int[tasks.length];
+        int currentTime = 0;
+        /**
+         * [服务器权重，索引，空闲时间]
+         */
+        Queue<int[]> freeServers =
+                new PriorityQueue<>(servers.length, (x, y) -> x[0] == y[0] ? x[1] - y[1] : x[0] - y[0]);
+        Queue<int[]> busyServers = new PriorityQueue<>(Comparator.comparingInt(x -> x[2]));
+
+        for (int i = 0; i < servers.length; i++) {
+            freeServers.offer(new int[]{servers[i], i, 0});
+        }
+
+        for (int i = 0; i < tasks.length; ) {
+            while (!busyServers.isEmpty() && busyServers.peek()[2] == currentTime) {
+                freeServers.offer(busyServers.poll());
+            }
+
+            while (currentTime >= i && i < tasks.length && !freeServers.isEmpty()) {
+                int[] server = freeServers.poll();
+                server[2] = currentTime + tasks[i];
+                busyServers.offer(server);
+                result[i] = server[1];
+                i++;
+            }
+            currentTime++;
+        }
+        return result;
+    }
+}
