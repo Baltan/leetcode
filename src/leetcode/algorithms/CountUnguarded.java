8414c918570493b31782875bf8781ade7a0f12a5
Baltan <617640006@qq.com>
2022-05-08 14:59:45 +0800

    2257. Count Unguarded Cells in the Grid

--- src/leetcode/algorithms/CountUnguarded.java
+++ src/leetcode/algorithms/CountUnguarded.java
@@ -24,51 +24,82 @@ public class CountUnguarded {
     public static int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
         int result = 0;
         /**
-         * 0-未被保卫，1-被保卫，2-警卫，3-墙
+         * book[0]表示网格未被保卫，book[1]表示网格被保卫了，book[2]表示网格里是警卫，book[3]表示网格里是墙
          */
         int[][] book = new int[m][n];
-
+        /**
+         * 标记所有墙所在的网格
+         */
         for (int[] wall : walls) {
             int x = wall[0];
             int y = wall[1];
             book[x][y] = 3;
         }
-
+        /**
+         * 标记所有警卫所在的网格
+         */
         for (int[] guard : guards) {
             int x = guard[0];
             int y = guard[1];
             book[x][y] = 2;
+        }
 
-            while (x > 0 && (book[x - 1][y] == 0 || book[x - 1][y] == 1)) {
+        for (int[] guard : guards) {
+            int x = guard[0];
+            int y = guard[1];
+            /**
+             * 从当前警卫所在的网格(x,y)开始向上查找所有可以被保卫的网格，直到遇到警卫或墙
+             */
+            while (x > 0) {
+                if (book[x - 1][y] == 2 || book[x - 1][y] == 3) {
+                    break;
+                }
                 book[x - 1][y] = 1;
                 x--;
             }
 
             x = guard[0];
             y = guard[1];
-
-            while (x < m - 1 && (book[x + 1][y] == 0 || book[x + 1][y] == 1)) {
+            /**
+             * 当前警卫所在的网格(x,y)开始向下查找所有可以被保卫的网格，直到遇到警卫或墙
+             */
+            while (x < m - 1) {
+                if (book[x + 1][y] == 2 || book[x + 1][y] == 3) {
+                    break;
+                }
                 book[x + 1][y] = 1;
                 x++;
             }
 
             x = guard[0];
             y = guard[1];
-
-            while (y > 0 && (book[x][y - 1] == 0 || book[x][y - 1] == 1)) {
+            /**
+             * 当前警卫所在的网格(x,y)开始向右查找所有可以被保卫的网格，直到遇到警卫或墙
+             */
+            while (y > 0) {
+                if (book[x][y - 1] == 2 || book[x][y - 1] == 3) {
+                    break;
+                }
                 book[x][y - 1] = 1;
                 y--;
             }
 
             x = guard[0];
             y = guard[1];
-
-            while (y < n - 1 && (book[x][y + 1] == 0 || book[x][y + 1] == 1)) {
+            /**
+             * 当前警卫所在的网格(x,y)开始向左查找所有可以被保卫的网格，直到遇到警卫或墙
+             */
+            while (y < n - 1) {
+                if (book[x][y + 1] == 2 || book[x][y + 1] == 3) {
+                    break;
+                }
                 book[x][y + 1] = 1;
                 y++;
             }
         }
-
+        /**
+         * 计数所有未被保卫的网格
+         */
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (book[i][j] == 0) {
