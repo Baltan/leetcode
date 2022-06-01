1ef804c55d29ef199117a056683729dd4c64e3ab
Baltan <617640006@qq.com>
2022-06-01 09:30:09 +0800

    1854. Maximum Population Year

--- src/leetcode/algorithms/MaximumPopulation1.java
+++ src/leetcode/algorithms/MaximumPopulation1.java
@@ -5,9 +5,9 @@ package leetcode.algorithms;
  *
  * @author Baltan
  * @date 2022/5/31 22:55
- * @see MaximumPopulation1
+ * @see MaximumPopulation
  */
-public class MaximumPopulation {
+public class MaximumPopulation1 {
     public static void main(String[] args) {
         int[][] logs1 = {{1993, 1999}, {2000, 2010}};
         System.out.println(maximumPopulation(logs1));
@@ -16,31 +16,42 @@ public class MaximumPopulation {
         System.out.println(maximumPopulation(logs2));
     }
 
+    /**
+     * 参考：
+     * <a href="https://leetcode.cn/problems/maximum-population-year/solution/ren-kou-zui-duo-de-nian-fen-by-leetcode-5m7r4/"></a>
+     *
+     * @param logs
+     * @return
+     */
     public static int maximumPopulation(int[][] logs) {
         int result = 0;
         /**
          * 根据题意，出生年份和死亡年份在[1950,2050]范围内，populations[0]-populations[100]依次表示1950年-2050年每年的
-         * 人口数
+         * 人口数变化数
          */
         int[] populations = new int[2050 - 1950 + 1];
         int max = Integer.MIN_VALUE;
         /**
-         * 统计每年的人口数
+         * 从起始开始到某一年的累计人口变化数
+         */
+        int current = 0;
+        /**
+         * 统计每年的人口数变化数
          */
         for (int[] log : logs) {
             int birth = log[0] - 1950;
             int death = log[1] - 1950;
-
-            for (int i = birth; i < death; i++) {
-                populations[i]++;
-            }
+            populations[birth]++;
+            populations[death]--;
         }
         /**
-         * 查找人口最多的最小年份
+         * 查找人口变化数最大的最小年份
          */
         for (int i = 0; i < populations.length; i++) {
-            if (populations[i] > max) {
-                max = populations[i];
+            current += populations[i];
+
+            if (current > max) {
+                max = current;
                 result = i + 1950;
             }
         }
