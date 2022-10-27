6b4e9ca11e0ac281ff57f8df27dc5c922a690702
Baltan <617640006@qq.com>
2022-10-27 16:41:30 +0800

    1559. Detect Cycles in 2D Grid
    Signed-off-by: Baltan <617640006@qq.com>

--- src/leetcode/algorithms/ContainsCycle.java
+++ src/leetcode/algorithms/ContainsCycle.java
@@ -1,8 +1,5 @@
 package leetcode.algorithms;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 /**
  * Description: 1559. Detect Cycles in 2D Grid
  *
@@ -26,65 +23,81 @@ public class ContainsCycle {
         System.out.println(containsCycle(grid4));
     }
 
+    /**
+     * 参考：
+     * <a href="https://leetcode.cn/problems/detect-cycles-in-2d-grid/solutions/1798758/-by-fei-xiao-r-m9j9/"></a>
+     *
+     * @param grid
+     * @return
+     */
     public static boolean containsCycle(char[][] grid) {
         int rows = grid.length;
         int cols = grid[0].length;
         /**
-         * 每一个格子可能有上下左右四个相邻的格子
+         * 标记所有已经访问过的格子的坐标
          */
-        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        boolean[][] isVisited = new boolean[rows][cols];
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 /**
-                 * 路径的起点格子的坐标为(startX,startY)
+                 * 假设初始时是从左边进入grid[i][j]这个格子的
                  */
-                int startX = i;
-                int startY = j;
-                char value = grid[startX][startY];
-                /**
-                 * 保存路径上所有格子的坐标
-                 */
-                Queue<int[]> queue = new LinkedList<>();
-                /**
-                 * 标记所有已经访问过的格子的坐标
-                 */
-                boolean[][] isVisited = new boolean[rows][cols];
-                queue.offer(new int[]{startX, startY});
-                isVisited[startX][startY] = true;
-
-                while (!queue.isEmpty()) {
-                    int size = queue.size();
-                    /**
-                     * 保存与起点格子的坐标(startX,startY)相同距离的所有格子的坐标
-                     */
-                    boolean[][] willVisit = new boolean[rows][cols];
-
-                    for (int k = 0; k < size; k++) {
-                        int[] coordinate = queue.poll();
-
-                        for (int[] direction : directions) {
-                            int nextX = coordinate[0] + direction[0];
-                            int nextY = coordinate[1] + direction[1];
-                            boolean inRange = nextX >= 0 && nextX < rows && nextY >= 0 && nextY < cols;
-                            /**
-                             * 如果从队列queue中的两个格子都能到达坐标为(nextX,nextY)的格子，说明已经找到了一个还，且环的
-                             * 长度一定是大于等于4的
-                             */
-                            if (inRange && willVisit[nextX][nextY]) {
-                                return true;
-                            }
-
-                            if (inRange && !isVisited[nextX][nextY] && grid[nextX][nextY] == value) {
-                                queue.offer(new int[]{nextX, nextY});
-                                isVisited[nextX][nextY] = true;
-                                willVisit[nextX][nextY] = true;
-                            }
-                        }
-                    }
+                if (!isVisited[i][j] && dfs(grid, isVisited, rows, cols, grid[i][j], i, j, 0)) {
+                    return true;
                 }
             }
         }
         return false;
+    }
+
+    /**
+     * @param grid
+     * @param isVisited
+     * @param rows
+     * @param cols
+     * @param value     当前查找的环上的字符
+     * @param x
+     * @param y
+     * @param from      进入格子grid[x][y]的方向：0-左，1-上，2-右，3-下
+     * @return
+     */
+    public static boolean dfs(char[][] grid, boolean[][] isVisited, int rows, int cols, char value, int x,
+                              int y, int from) {
+        /**
+         * 超出二维网格图的范围，不能构成环
+         */
+        if (x < 0 || y < 0 || x >= rows || y >= cols) {
+            return false;
+        }
+        /**
+         * grid[x][y]和之前路径上的字符不同，不能构成环
+         */
+        if (grid[x][y] != value) {
+            return false;
+        }
+        /**
+         * 和之前已经访问过的网格构成了环
+         */
+        if (isVisited[x][y]) {
+            return true;
+        }
+        isVisited[x][y] = true;
+        /**
+         * from：0-左，1-上，2-右，3-下
+         *
+         * 1、如果不是从右进入格子grid[x][y]的，下一步就可以向右走进入格子grid[x][y+1]，对于格子grid[x][y+1]来说即是从左边
+         * 进入格子
+         * 2、如果不是从下进入格子grid[x][y]的，下一步就可以向下走进入格子grid[x+1][y]，对于格子grid[x+1][y]来说即是从上边
+         * 进入格子
+         * 3、如果不是从左进入格子grid[x][y]的，下一步就可以向左走进入格子grid[x][y-1]，对于格子grid[x][y-1]来说即是从右边
+         * 进入格子
+         * 4、如果不是从上进入格子grid[x][y]的，下一步就可以向上走进入格子grid[x-1][y]，对于格子grid[x-1][y]来说即是从下边
+         * 进入格子
+         */
+        return (from != 2 && dfs(grid, isVisited, rows, cols, value, x, y + 1, 0)) ||
+                (from != 3 && dfs(grid, isVisited, rows, cols, value, x + 1, y, 1)) ||
+                (from != 0 && dfs(grid, isVisited, rows, cols, value, x, y - 1, 2)) ||
+                (from != 1 && dfs(grid, isVisited, rows, cols, value, x - 1, y, 3));
     }
 }
