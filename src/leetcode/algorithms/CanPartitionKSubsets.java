8557dda5e18f9df4fe686f337617d8c33a154e81
Baltan <617640006@qq.com>
2023-02-02 12:36:00 +0800

    698. Partition to K Equal Sum Subsets
    Signed-off-by: Baltan <617640006@qq.com>

--- src/leetcode/algorithms/CanPartitionKSubsets.java
+++ src/leetcode/algorithms/CanPartitionKSubsets.java
@@ -17,8 +17,13 @@ public class CanPartitionKSubsets {
         System.out.println(canPartitionKSubsets(new int[]{1, 2, 3, 4}, 3));
     }
 
-    private static boolean result;
-
+    /**
+     * 参考：<a href="https://leetcode.cn/problems/partition-to-k-equal-sum-subsets/solutions/1833777/hua-fen-wei-kge-xiang-deng-de-zi-ji-by-l-v66o/"></a>
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
     public static boolean canPartitionKSubsets(int[] nums, int k) {
         /**
          * 数组nums中所有数字的和
@@ -43,48 +48,57 @@ public class CanPartitionKSubsets {
         if (max > average) {
             return false;
         }
-        result = false;
         /**
-         * counts[i]表示数组nums中数字i的剩余个数
+         * status二进制值从低位到高位的第i位为0表示数字nums[i]可用，为1表示数字nums[i]已被使用，status的最大可能值为(1<<length)-1
          */
-        int[] counts = new int[max + 1];
-
-        for (int num : nums) {
-            counts[num]++;
-        }
-        dfs(nums, counts, average, 0, length - 1, length);
-        return result;
-    }
-
-    public static void dfs(int[] nums, int[] counts, int average, int sum, int currIndex, int leftCount) {
+        int status = (1 << length) - 1;
         /**
-         * 如果数组nums中所有数字都被用光了，说明已将数组nums分成了多个非空子集，每个非空子集的数字之和都为average
+         * dp[i]表示status值为i时，这种状态下是否可能按照题意划分数组nums，题目所求就是dp[status]
          */
-        if (result || leftCount == 0) {
-            result = true;
-            return;
-        }
+        boolean[] dp = new boolean[status + 1];
+        /**
+         * sums[i]表示status值为i时，当前正在查找的子集中数字的和
+         */
+        int[] sums = new int[status + 1];
+        dp[0] = true;
 
-        for (int i = currIndex; i >= 0; i--) {
-            int num = nums[i];
-
-            if (counts[num] == 0 || sum + num > average) {
+        for (int i = 0; i <= status; i++) {
+            /**
+             * status值为i时不能划分数组，加上其他数字也不可能可以划分数组
+             */
+            if (!dp[i]) {
                 continue;
             }
-            counts[num]--;
+            /**
+             * 在status值为i的基础上多增加一个数字nums[j]，判断是否可以划分数组
+             */
+            for (int j = 0; j < length; j++) {
+                /**
+                 * 说明在状态status值为i时，数字dp[j]已被使用，跳过
+                 */
+                if (((i >> j) & 1) == 1) {
+                    continue;
+                }
+                /**
+                 * 当前子集数字之和大于average，不能划分数组，后面的nums[j]值更大，不用继续计算
+                 */
+                if (sums[i] + nums[j] > average) {
+                    break;
+                }
+                /**
+                 * 标记数字dp[j]已被使用
+                 */
+                int next = i | (1 << j);
 
-            if (sum + num == average) {
-                /**
-                 * 当前非空子集的所有数字之和已经为average，继续查找下一个非空子集
-                 */
-                dfs(nums, counts, average, 0, nums.length - 1, leftCount - 1);
-            } else {
-                /**
-                 * 继续查找当前非空子集的剩余数字
-                 */
-                dfs(nums, counts, average, sum + num, i - 1, leftCount - 1);
+                if (!dp[next]) {
+                    /**
+                     * 当前子集中数字的和已经为average了，将当前子集中数字的和设为0，表示要查找一个新的子集
+                     */
+                    sums[next] = sums[i] + nums[j] == average ? 0 : sums[i] + nums[j];
+                    dp[next] = true;
+                }
             }
-            counts[num]++;
         }
+        return dp[status];
     }
 }
