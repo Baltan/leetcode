557efb6a0b79cf72170503b012fa3827a94000ab
Baltan <617640006@qq.com>
2023-04-16 01:09:47 +0800

    2630. Memoize II
    Signed-off-by: Baltan <617640006@qq.com>

--- src/leetcode/javascript/MemoizeII.js
+++ src/leetcode/javascript/MemoizeII.js
@@ -3,31 +3,32 @@
  * @param {Function} fn
  */
 function memoize(fn) {
-    var map = new Map();
+    var index = 0;
+    // 参数arg -> 参数arg对应的唯一索引
+    var argIndexMap = new Map();
+    var cache = new Map();
 
     return (...args) => {
-        var argCount = args.length;
-        var caches = map.get(argCount);
+        // 参数列表[...args]对应的索引数组，根据索引数组判断是否命中缓存
+        var argIndexes = [];
 
-        if (caches) {
-            outer:
-                for (var key of caches.keys()) {
-                    for (var i = 0; i < argCount; i++) {
-                        if ([...args][i] !== key[i]) {
-                            continue outer;
-                        }
-                    }
-                    return caches.get(key);
-                }
-            var value = fn(...args);
-            caches.set([...args], value);
-            count++;
-            return value;
+        for (var arg of args) {
+            if (!argIndexMap.has(arg)) {
+                var argIndex = index++;
+                argIndexMap.set(arg, argIndex);
+                argIndexes.push(argIndex);
+            } else {
+                argIndexes.push(argIndexMap.get(arg));
+            }
+        }
+        // 将索引数组转换成字符串
+        var key = argIndexes.join("-");
+
+        if (cache.has(key)) {
+            return cache.get(key);
         } else {
             var value = fn(...args);
-            caches = new Map();
-            caches.set([...args], value);
-            map.set(argCount, caches);
+            cache.set(key, value);
             count++;
             return value;
         }
@@ -60,8 +61,8 @@ console.log("-----------------------------");
 
 var count = 0;
 var fn = (a, b) => ({...a, ...b});
-var o = {};
 var memoize3 = memoize(fn);
+var o = {};
 console.log(memoize3(o, o));
 console.log(count);
 console.log(memoize3(o, o));
