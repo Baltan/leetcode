103c7159c6c2ef911e9db8ef5c60849ebf2a20d3
Baltan <617640006@qq.com>
2023-04-22 21:53:05 +0800

    2621. Sleep
    Signed-off-by: Baltan <617640006@qq.com>

---
+++ src/leetcode/javascript/Sleep.js
@@ -0,0 +1,17 @@
+// 2621. Sleep
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    // resolve就是then()调用的函数，reject就是catch()调用的函数
+    await new Promise((resolve) => setTimeout(() => {
+        resolve()
+    }, millis));
+}
+
+var t = Date.now();
+sleep(100).then(() => {
+    console.log(Date.now() - t);
+}).catch(() => {
+    console.log("error");
+});
