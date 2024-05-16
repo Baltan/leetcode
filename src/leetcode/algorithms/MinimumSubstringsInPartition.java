2ce82c738b1a876ad2903633a731625e3b81bd3a
baltan <617640006@qq.com>
2024-05-16 15:44:47 +0800

    3144. Minimum Substring Partition of Equal Character Frequency

--- src/leetcode/algorithms/MinimumSubstringsInPartition.java
+++ src/leetcode/algorithms/MinimumSubstringsInPartition.java
@@ -1,5 +1,7 @@
 package leetcode.algorithms;
 
+import java.util.Arrays;
+
 /**
  * Description: 3144. Minimum Substring Partition of Equal Character Frequency
  *
@@ -8,59 +10,115 @@ package leetcode.algorithms;
  */
 public class MinimumSubstringsInPartition {
     public static void main(String[] args) {
+        System.out.println(minimumSubstringsInPartition("abc"));
+        System.out.println(minimumSubstringsInPartition("c"));
         System.out.println(minimumSubstringsInPartition("hhqmqzavccbsjkfpijrpdjkmgcpyomborudbbfyeokkelzznstufyppndilajgxxhusslzvnwhfitqtsstjqclxqvkykovhzyyqfksmnhhtpdabfjrfflmqoakaackcchgzhrppmvpnunuzhoouftiqwdbxbemtzwkrqqkspwypxwzavvgelkxsgkipoottveturyicswfdemvpfegdclnjccqkgouzcdfvpstnjxzdukwctysagimjissinxkohfyapuuavvnssqzcynoixegzkuvphadbaedwkgpxaoohgmfoqmvfbbywinngwctrighefrnbllggbjfeslbfwzuktwvocbmokicwvpotpvbytlxajtidumtocerdqrrtvjawgikwkqnnfgbsczshswuvvnnmmjssrulcxattfgklzklfrahvbcdetxohipslfbifkmiljvbvpffeiymidgsaxkjvynjaaqkymyyejjyyxucuyehrnzzqwolgylytlviedicxxavsmytdyscdczhmpydzvwaeziczvolrzossqazqtccygbyunsxxwzxnsfboazgfcefmqggcgwhlaatfgatfkffgywldlmoubqlcstwwkepecxxrsgawwxpxmdvifiecopozvooomhbqmrsdraajwybdhnmrzfgnemzqnxvaedhvhexjlvkllzrtgcgnkkopnytjktjffndpqlpepqqxyjikcyfwaocbjcjlzybxemollniwuucqpshkkqwawyyuqligbzanssbsfwnckkkjfurvpxqcdauvrykrqlnpwacgxivlgppdqqkijjvyycwlxzxcgxudboopuehittsiknnlssspefgcvvctzhajjmkyyykkqlpynaconuuumzycvlrkgukuuwpleedpwtmsjfkbdmyubjuhoajcymrzeytmnkkoqgnsltpczzofvivpuogrunnnxkmyhbuuwvvvgnbvnvvlggnug"));
         System.out.println(minimumSubstringsInPartition("fabccddg"));
         System.out.println(minimumSubstringsInPartition("abababaccddb"));
     }
 
     public static int minimumSubstringsInPartition(String s) {
+        int result = Integer.MAX_VALUE;
         int length = s.length();
-        int[][] prefixCounts = new int[length + 1][26];
-        int[][] dp = new int[length + 1][length + 1];
+        /**
+         * isBalanced[i][j]表示子串s.substring(i,j)是否是平衡字符串
+         */
         boolean[][] isBalanced = new boolean[length + 1][length + 1];
+        /**
+         * prefixes[i]表示子串s.substring(0,i)最少可以分割成的平衡字符串的个数
+         */
+        int[] prefixes = new int[length + 1];
+        /**
+         * suffixes[i]表示子串s.substring(i,length)最少可以分割成的平衡字符串的个数
+         */
+        int[] suffixes = new int[length + 1];
+        Arrays.fill(prefixes, Integer.MAX_VALUE);
+        Arrays.fill(suffixes, Integer.MAX_VALUE);
+        prefixes[1] = 1;
+        suffixes[length - 1] = 1;
 
         for (int i = 0; i < length; i++) {
-            for (int j = 0; j < 26; j++) {
-                prefixCounts[i + 1][j] = prefixCounts[i][j];
-            }
-            prefixCounts[i + 1][s.charAt(i) - 'a']++;
-            dp[i][i + 1] = 1;
-        }
+            /**
+             * counts[0]-counts[25]依次表示子串s.substring(i,j)中字符a-z的个数
+             */
+            int[] counts = new int[26];
 
-        for (int i = 0; i < length; i++) {
             for (int j = i + 1; j <= length; j++) {
-                isBalanced[i][j] = isBalanced(prefixCounts, i, j);
+                counts[s.charAt(j - 1) - 'a']++;
+
+                if (isBalanced(counts)) {
+                    isBalanced[i][j] = true;
+                }
             }
         }
-
+        /**
+         *  字符串s本身就是平衡字符串
+         */
+        if (isBalanced[0][length]) {
+            return 1;
+        }
+        /**
+         * 对字符串s的所有前缀子串s.substring(0,i)进行分割计算
+         */
         for (int i = 2; i <= length; i++) {
-            for (int j = 0; j + i <= length; j++) {
-                if (isBalanced[j][j + i]) {
-                    dp[j][j + i] = 1;
-                    continue;
-                }
-                dp[j][j + i] = Integer.MAX_VALUE;
+            /**
+             * 前缀子串s.substring(0,i)本身就是平衡字符串
+             */
+            if (isBalanced[0][i]) {
+                prefixes[i] = 1;
+                continue;
+            }
 
-                for (int k = j + 1; k <= j + i; k++) {
-                    if (dp[j][k] != Integer.MAX_VALUE && dp[k][j + i] != Integer.MAX_VALUE) {
-                        dp[j][j + i] = Math.min(dp[j][j + i], dp[j][k] + dp[k][j + i]);
-                    }
+            for (int j = 1; j < i; j++) {
+                if (isBalanced[j][i]) {
+                    prefixes[i] = Math.min(prefixes[i], prefixes[j] + 1);
                 }
             }
         }
-        return dp[0][length];
+        /**
+         * 对字符串s的所有后缀子串s.substring(i,length)进行分割计算
+         */
+        for (int i = length - 2; i >= 0; i--) {
+            /**
+             * 后缀子串s.substring(i,length)本身就是平衡字符串
+             */
+            if (isBalanced[i][length]) {
+                suffixes[i] = 1;
+                continue;
+            }
+
+            for (int j = i + 1; j < length; j++) {
+                if (isBalanced[i][j]) {
+                    suffixes[i] = Math.min(suffixes[i], suffixes[j] + 1);
+                }
+            }
+        }
+        /**
+         * 将字符串s分成首尾两个子串，计算最少可以分割成的平衡字符串的个数
+         */
+        for (int i = 1; i < length; i++) {
+            result = Integer.min(result, prefixes[i] + suffixes[i]);
+        }
+        return result;
     }
 
-    public static boolean isBalanced(int[][] prefixCounts, int start, int end) {
+    /**
+     * 判断一个字符串是否是平衡字符串
+     *
+     * @param counts counts[0]-counts[25]依次表示字符串中字符a-z的个数
+     * @return
+     */
+    public static boolean isBalanced(int[] counts) {
+        /**
+         * 遍历过程中第一个出现次数不为0的字符的个数，作为后面其他字符比较的基准
+         */
         int count = -1;
 
         for (int i = 0; i < 26; i++) {
-            if (prefixCounts[end][i] - prefixCounts[start][i] != 0) {
-                if (count == -1) {
-                    count = prefixCounts[end][i] - prefixCounts[start][i];
-                } else if (prefixCounts[end][i] - prefixCounts[start][i] != count) {
-                    return false;
-                }
+            if (counts[i] > 0 && count == -1) {
+                count = counts[i];
+            } else if (counts[i] > 0 && counts[i] != count) {
+                return false;
             }
         }
         return true;
