902700fd6d9c4c417c0a4071330deb050533d55f
baltan <617640006@qq.com>
2024-06-22 19:44:01 +0800

    3181. Maximum Total Reward Using Operations II

--- src/leetcode/algorithms/MaxTotalReward1.java
+++ src/leetcode/algorithms/MaxTotalReward1.java
@@ -1,6 +1,6 @@
 package leetcode.algorithms;
 
-import java.util.BitSet;
+import java.math.BigInteger;
 
 /**
  * Description: 3181. Maximum Total Reward Using Operations II
@@ -16,6 +16,12 @@ public class MaxTotalReward1 {
         System.out.println(maxTotalReward(new int[]{1, 6, 4, 3, 2}));
     }
 
+    /**
+     * 参考：<a href="https://leetcode.cn/problems/maximum-total-reward-using-operations-ii/solutions/2805413/bitset-you-hua-0-1-bei-bao-by-endlessche-m1xn/"></a>
+     *
+     * @param rewardValues
+     * @return
+     */
     public static int maxTotalReward(int[] rewardValues) {
         /**
          * isVisited[i]表示数组rewardValues中是否存在数字i
@@ -25,12 +31,27 @@ public class MaxTotalReward1 {
          * 数组rewardValues中不同数字的个数
          */
         int count = 0;
+        /**
+         * 数组rewardValues中的最大值
+         */
+        int max = Integer.MIN_VALUE;
 
         for (int rewardValue : rewardValues) {
             if (!isVisited[rewardValue]) {
                 count++;
             }
             isVisited[rewardValue] = true;
+            max = Math.max(max, rewardValue);
+        }
+        /**
+         * 最大总奖励总是包含数组rewardValues中的最大值max，否则一定可以用max替换已选数字中的最大值得到更大的总奖励值。而选择max之前，已选
+         * 数字之和至多为max-1，所以理论上的最大总奖励不可能大于max-1+max。如果数组rewardValues中存在数字max-1，或者存在两个数字之和为
+         * max-1，则直接选择它们和max即可
+         */
+        for (int i = 0; i < max / 2; i++) {
+            if (isVisited[max - 1] || (isVisited[i] && isVisited[max - 1 - i])) {
+                return max * 2 - 1;
+            }
         }
         /**
          * 升序保存数组rewardValues中的不同数字，因为每次选择一个数字必须保证它大于已选数字之和，所以不可能选择到同样的数字
@@ -45,25 +66,14 @@ public class MaxTotalReward1 {
             }
         }
         /**
-         * 用一个二进制数表示数组values可能获得的各种总奖励值，如果能获得总奖励i，则bitSet[i]为true
+         * conditions的二进制值从低到高第i位为1表示可以从数组values中获得总奖励i。初始状态下没有选择任何数字，总奖励值为0，将conditions
+         * 二进制值的第0位置为1，即初始化conditions为1
          */
-        BitSet bitSet = new BitSet(values[count - 1] * 2);
-        /**
-         * 初始状态下没有选择任何数字，总奖励值为0
-         */
-        bitSet.set(0);
+        BigInteger conditions = BigInteger.ONE;
 
         for (int value : values) {
-            BitSet nextBitSet = new BitSet();
-            nextBitSet.or(bitSet);
-            int bitIndex;
-
-            while ((bitIndex = bitSet.nextSetBit(0)) != -1 && bitIndex < value) {
-                nextBitSet.set(bitIndex + value);
-                bitSet.clear(bitIndex);
-            }
-            bitSet = nextBitSet;
+            conditions = conditions.or(conditions.subtract(conditions.shiftRight(value).shiftLeft(value)).shiftLeft(value));
         }
-        return bitSet.length() - 1;
+        return conditions.bitLength() - 1;
     }
 }
