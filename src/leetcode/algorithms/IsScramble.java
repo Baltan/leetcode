3c95a42d1cbae0b6f2c66bf31cb8998bedec1159
Baltan <617640006@qq.com>
2024-06-23 16:03:25 +0800

    87. Scramble String
    Signed-off-by: Baltan <617640006@qq.com>

--- src/leetcode/algorithms/IsScramble.java
+++ src/leetcode/algorithms/IsScramble.java
@@ -19,52 +19,70 @@ public class IsScramble {
         System.out.println(isScramble("ab", "ba"));
     }
 
+    /**
+     * 参考：<a href="https://leetcode.cn/problems/scramble-string/solutions/51990/miao-dong-de-qu-jian-xing-dpsi-lu-by-sha-yu-la-jia/"></a>
+     *
+     * @param s1
+     * @param s2
+     * @return
+     */
     public static boolean isScramble(String s1, String s2) {
         if (Objects.equals(s1, s2)) {
             return true;
         }
-
+        /**
+         * 如果字符串s1和s2长度不等，则不可能互为扰乱字符串
+         */
         if (s1.length() != s2.length()) {
             return false;
         }
         int length = s1.length();
-
-        for (int i = 1; i < length; i++) {
-            String prefix1 = s1.substring(0, i);
-            String suffix1 = s1.substring(i);
-            String prefix2 = s2.substring(0, i);
-            String suffix2 = s2.substring(i);
-
-            if (isAnagram(prefix1, prefix2) && isAnagram(suffix1, suffix2) && isScramble(prefix1, prefix2) && isScramble(suffix1, suffix2)) {
-                return true;
-            }
-            prefix2 = s2.substring(0, length - i);
-            suffix2 = s2.substring(length - i);
-
-            if (isAnagram(prefix1, suffix2) && isAnagram(suffix1, prefix2) && isScramble(prefix1, suffix2) && isScramble(suffix1, prefix2)) {
-                return true;
+        /**
+         * dp[i][j][l]表示子串s1.substring(i,i+l)和s2.substring(j,j+l)是否互为扰乱字符串，题目所求即为dp[0][0][length]
+         */
+        boolean[][][] dp = new boolean[length][length][length + 1];
+        /**
+         * 初始化子串长度为1的情况
+         */
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                dp[i][j][1] = s1.charAt(i) == s2.charAt(j);
             }
         }
-        return false;
-    }
-
-    public static boolean isAnagram(String s1, String s2) {
-        int[] counts = new int[26];
-
-        if (s1.length() != s2.length()) {
-            return false;
-        }
-
-        for (int i = 0; i < s1.length(); i++) {
-            counts[s1.charAt(i) - 'a']++;
-            counts[s2.charAt(i) - 'a']--;
-        }
-
-        for (int count : counts) {
-            if (count != 0) {
-                return false;
+        /**
+         * l表示两个子串的长度
+         */
+        for (int l = 2; l <= length; l++) {
+            for (int i = 0; i + l <= length; i++) {
+                for (int j = 0; j + l <= length; j++) {
+                    /**
+                     * 判断子串s1.substring(i,i+l)和s2.substring(j,j+l)是否互为扰乱字符串
+                     */
+                    for (int k = 1; k <= l - 1; k++) {
+                        /**
+                         * 假设子串s1.substring(i,i+l)被进一步分割为s1.substring(i,i+k)和s1.substring(i+k,i+l)两部分，子串
+                         * s2.substring(j,j+l)被进一步分割为s2.substring(j,j+k)和s2.substring(j+k,j+l)两部分。如果子串
+                         * s1.substring(i,i+k)和s2.substring(j,j+k)互为扰乱字符串，并且子串s1.substring(i+k,i+l)和
+                         * s2.substring(j+k,j+l)互为扰乱字符串，则子串s1.substring(i,i+l)和s2.substring(j,j+l)互为扰乱字符串
+                         */
+                        if (dp[i][j][k] && dp[i + k][j + k][l - k]) {
+                            dp[i][j][l] = true;
+                            break;
+                        }
+                        /**
+                         * 假设子串s1.substring(i,i+l)被进一步分割为s1.substring(i,i+k)和s1.substring(i+k,i+l)两部分，子串
+                         * s2.substring(j,j+l)被进一步分割为s2.substring(j,j+l-k)和s2.substring(j+l-k,j+l)两部分。如果子串
+                         * s1.substring(i,i+k)和s2.substring(j+l-k,j+l)互为扰乱字符串，并且子串s1.substring(i+k,i+l)和
+                         * s2.substring(j,j+l-k)互为扰乱字符串，则子串s1.substring(i,i+l)和s2.substring(j,j+l)互为扰乱字符串
+                         */
+                        if (dp[i][j + l - k][k] && dp[i + k][j][l - k]) {
+                            dp[i][j][l] = true;
+                            break;
+                        }
+                    }
+                }
             }
         }
-        return true;
+        return dp[0][0][length];
     }
 }
