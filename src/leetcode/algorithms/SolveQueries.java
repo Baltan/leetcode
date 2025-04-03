2e10292b0d0745b2a99c2a721e79f61252e3b3d1
Baltan <617640006@qq.com>
2025-04-04 00:09:57 +0800

    3488. Closest Equal Element Queries

--- src/leetcode/algorithms/SolveQueries.java
+++ src/leetcode/algorithms/SolveQueries.java
@@ -20,6 +20,9 @@ public class SolveQueries {
     public static List<Integer> solveQueries(int[] nums, int[] queries) {
         List<Integer> result = new ArrayList<>(queries.length);
         int max = Arrays.stream(nums).max().getAsInt();
+        /**
+         * indexesArray[i]升序保存数组nums中所有元素i的索引值，如果数组nums中不存在元素i，则indexesArray[i]为null
+         */
         List<Integer>[] indexesArray = new List[max + 1];
 
         for (int i = 0; i < nums.length; i++) {
@@ -31,22 +34,26 @@ public class SolveQueries {
 
         for (int index : queries) {
             int num = nums[index];
-
-            if (indexesArray[num] == null || indexesArray[num].size() == 1) {
+            /**
+             * 数组nums中只存在唯一的元素num，即元素nums[index]
+             */
+            if (indexesArray[num].size() == 1) {
                 result.add(-1);
                 continue;
             }
             List<Integer> indexes = indexesArray[num];
             int lo = 0;
             int hi = indexes.size() - 1;
-
+            /**
+             * 二分查找当前nums[index]在数组nums中的索引值在索引数组indexes中的索引位置
+             */
             while (lo <= hi) {
                 int mid = (lo + hi) / 2;
 
                 if (indexes.get(mid) < index) {
-                    lo++;
+                    lo = mid + 1;
                 } else if (indexes.get(mid) > index) {
-                    hi--;
+                    hi = mid - 1;
                 } else {
                     lo = mid;
                     break;
@@ -54,10 +61,19 @@ public class SolveQueries {
             }
 
             if (lo == 0) {
+                /**
+                 * 元素nums[index]和数组nums中最后一个元素num的距离，元素nums[index]和数组nums中下一个元素num的距离，两者取较小值
+                 */
                 result.add(Math.min(index + nums.length - indexes.getLast(), indexes.get(lo + 1) - index));
             } else if (lo == indexes.size() - 1) {
+                /**
+                 * 元素nums[index]和数组nums中第一个元素num的距离，元素nums[index]和数组nums中上一个元素num的距离，两者取较小值
+                 */
                 result.add(Math.min(index - indexes.get(lo - 1), indexes.getFirst() + nums.length - index));
             } else {
+                /**
+                 * 元素nums[index]和数组nums中上一个元素num的距离，元素nums[index]和数组nums中下一个元素num的距离，两者取较小值
+                 */
                 result.add(Math.min(index - indexes.get(lo - 1), indexes.get(lo + 1) - index));
             }
         }
