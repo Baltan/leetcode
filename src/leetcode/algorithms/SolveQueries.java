8ff096a7c27915d2e036bdf7646263360fd33364
Baltan <617640006@qq.com>
2025-04-04 00:01:57 +0800

    3488. Closest Equal Element Queries —— Time Limit Exceeded

---
+++ src/leetcode/algorithms/SolveQueries.java
@@ -0,0 +1,66 @@
+package leetcode.algorithms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Description: 3488. Closest Equal Element Queries
+ *
+ * @author Baltan
+ * @date 2025/4/2 23:30
+ */
+public class SolveQueries {
+    public static void main(String[] args) {
+        System.out.println(solveQueries(new int[]{15, 1, 10, 1, 20, 4, 6, 14, 4, 9, 4, 18}, new int[]{0, 2, 10, 6, 11, 8}));
+        System.out.println(solveQueries(new int[]{1, 3, 1, 4, 1, 3, 2}, new int[]{0, 3, 5}));
+        System.out.println(solveQueries(new int[]{1, 2, 3, 4}, new int[]{0, 1, 2, 3}));
+    }
+
+    public static List<Integer> solveQueries(int[] nums, int[] queries) {
+        List<Integer> result = new ArrayList<>(queries.length);
+        int max = Arrays.stream(nums).max().getAsInt();
+        List<Integer>[] indexesArray = new List[max + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (indexesArray[nums[i]] == null) {
+                indexesArray[nums[i]] = new ArrayList<>();
+            }
+            indexesArray[nums[i]].add(i);
+        }
+
+        for (int index : queries) {
+            int num = nums[index];
+
+            if (indexesArray[num] == null || indexesArray[num].size() == 1) {
+                result.add(-1);
+                continue;
+            }
+            List<Integer> indexes = indexesArray[num];
+            int lo = 0;
+            int hi = indexes.size() - 1;
+
+            while (lo <= hi) {
+                int mid = (lo + hi) / 2;
+
+                if (indexes.get(mid) < index) {
+                    lo++;
+                } else if (indexes.get(mid) > index) {
+                    hi--;
+                } else {
+                    lo = mid;
+                    break;
+                }
+            }
+
+            if (lo == 0) {
+                result.add(Math.min(index + nums.length - indexes.getLast(), indexes.get(lo + 1) - index));
+            } else if (lo == indexes.size() - 1) {
+                result.add(Math.min(index - indexes.get(lo - 1), indexes.getFirst() + nums.length - index));
+            } else {
+                result.add(Math.min(index - indexes.get(lo - 1), indexes.get(lo + 1) - index));
+            }
+        }
+        return result;
+    }
+}
