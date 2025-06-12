9902ebf9f7b88ddeb3c4404cbf384ce88fb9a839
baltan <617640006@qq.com>
2025-06-12 17:05:36 +0800

    3558. Number of Ways to Assign Edge Weights I

--- src/leetcode/algorithms/AssignEdgeWeights.java
+++ src/leetcode/algorithms/AssignEdgeWeights.java
@@ -18,22 +18,38 @@ public class AssignEdgeWeights {
     }
 
     public static int assignEdgeWeights(int[][] edges) {
-        long result = 0L;
+        long result = 1L;
         int mod = 1000000007;
+        /**
+         * 节点个数
+         */
         int n = edges.length + 1;
+        /**
+         * 节点1到深度最大节点的路径上的边数
+         */
         int depth = -1;
         Queue<Integer> queue = new LinkedList<>();
+        /**
+         * isVisited[i]表示节点i是否访问过
+         */
         boolean[] isVisited = new boolean[n + 1];
+        /**
+         * graph[i]表示与节点i相连的所有其他的节点列表
+         */
         List<Integer>[] graph = new List[n + 1];
         Arrays.setAll(graph, i -> new ArrayList<>());
         queue.offer(1);
         isVisited[1] = true;
-
+        /**
+         * 构建图
+         */
         for (int[] edge : edges) {
             graph[edge[0]].add(edge[1]);
             graph[edge[1]].add(edge[0]);
         }
-
+        /**
+         * 广度优先搜索计算节点1到深度最大节点的路径上的边数
+         */
         while (!queue.isEmpty()) {
             depth++;
             int size = queue.size();
@@ -47,19 +63,12 @@ public class AssignEdgeWeights {
                 }
             }
         }
-        long[][] combinations = new long[depth + 1][depth + 1];
-
-        for (int i = 0; i < combinations.length; i++) {
-            combinations[i][0] = 1;
-
-            for (int j = 1; j <= i; j++) {
-                combinations[i][j] = (combinations[i - 1][j - 1] + combinations[i - 1][j]) % mod;
-            }
+        /**
+         * 从depth条边中任意选择奇数条边的情况数为C(depth,1)+C(depth,3)+C(depth,5)+……C(depth,2k+1)=2^(depth-1)，其中2k+1<=depth
+         */
+        for (int i = 1; i < depth; i++) {
+            result = result * 2 % mod;
         }
-
-        for (int i = 1; i <= depth; i += 2) {
-            result = (result + combinations[depth][i]) % mod;
-        }
-        return (int) result % mod;
+        return (int) result;
     }
 }
