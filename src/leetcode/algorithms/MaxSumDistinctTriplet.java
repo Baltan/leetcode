3614ac9d1d4f9bed8594bca639032b117fce7923
baltan <617640006@qq.com>
2025-06-18 11:38:43 +0800

    3572. Maximize Y‑Sum by Picking a Triplet of Distinct X‑Values —— Time Limit Exceeded

---
+++ src/leetcode/algorithms/MaxSumDistinctTriplet.java
@@ -0,0 +1,26 @@
+package leetcode.algorithms;
+
+import java.util.Arrays;
+
+/**
+ * Description: 3572. Maximize Y‑Sum by Picking a Triplet of Distinct X‑Values
+ *
+ * @author baltan
+ * @date 2025/6/18 08:59
+ */
+public class MaxSumDistinctTriplet {
+    public static void main(String[] args) {
+        System.out.println(maxSumDistinctTriplet(new int[]{1, 2, 1, 3, 2}, new int[]{5, 3, 4, 6, 2}));
+        System.out.println(maxSumDistinctTriplet(new int[]{1, 2, 1, 2}, new int[]{4, 5, 6, 7}));
+    }
+
+    public static int maxSumDistinctTriplet(int[] x, int[] y) {
+        int[] max = new int[1000001];
+
+        for (int i = 0; i < x.length; i++) {
+            max[x[i]] = Math.max(max[x[i]], y[i]);
+        }
+        Arrays.sort(max);
+        return max[max.length - 3] == 0 || max[max.length - 2] == 0 || max[max.length - 1] == 0 ? -1 : max[max.length - 3] + max[max.length - 2] + max[max.length - 1];
+    }
+}
