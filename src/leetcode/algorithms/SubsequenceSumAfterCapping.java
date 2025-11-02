026537064b5a471622c50be02b2df7ebca96fa1d
Baltan <617640006@qq.com>
2025-11-02 23:55:24 +0800

    3685. Subsequence Sum After Capping Elements

--- src/leetcode/algorithms/SubsequenceSumAfterCapping.java
+++ src/leetcode/algorithms/SubsequenceSumAfterCapping.java
@@ -25,48 +25,68 @@ public class SubsequenceSumAfterCapping {
         Arrays.sort(nums);
         int length = nums.length;
         boolean[] result = new boolean[length];
-        int leftCount = length;
+        /**
+         * 当前要加入背包数字为nums[j]
+         */
+        int j = 0;
+        /**
+         * 数组nums中的最大数字
+         */
         int max = nums[length - 1];
-        int[] counts = new int[max + 1];
-        int value = 0;
-        int maxSum = max * length;
-        int currentMaxSum = 0;
-        boolean[] dp = new boolean[maxSum + 1];
+        /**
+         * dp[i]表示能否从背包中选择若干个已有数字，使得和为i，元素之和最大只需要计算到k即可
+         */
+        boolean[] dp = new boolean[k + 1];
+        /**
+         * 当背包为空时，可以使得若干个已有数字之和为0
+         */
         dp[0] = true;
-
-        for (int num : nums) {
-            counts[num]++;
-        }
-
+        /**
+         * 当前限制值为i
+         */
         for (int i = 1; i <= length; i++) {
+            /**
+             * 当限制值大于max时，背包中的数字始终和初始时一样，不需要重复计算
+             */
             if (i > max) {
                 result[i - 1] = result[i - 2];
                 continue;
             }
-
-            while (value <= i) {
-                leftCount -= counts[value];
-
-                for (int sum = currentMaxSum; sum >= 0; sum--) {
+            /**
+             * 如果元素nums[j]不大于i，就将其加入背包中
+             */
+            for (; j < length && nums[j] <= i; j++) {
+                /**
+                 * 遍历加入元素nums[j]之前背包中选择若干个已有数字之和的情况。注意必须降序遍历，否则可能出现nums[j]被重复累加的情况
+                 */
+                for (int sum = k; sum >= 0; sum--) {
                     if (!dp[sum]) {
                         continue;
                     }
+                    /**
+                     * 背包中选择若干个已有数字，可以使得和为newSum
+                     */
+                    int newSum = sum + nums[j];
 
-                    if ((k - sum) % i == 0 && (k - sum) / i <= leftCount && (k - sum) / i >= 0) {
-                        result[i - 1] = true;
-                    }
-
-                    for (int count = counts[value]; count >= 1; count--) {
-                        int newSum = sum + value * count;
+                    if (newSum <= k) {
                         dp[newSum] = true;
-
-                        if ((k - newSum) % i == 0 && (k - newSum) / i <= leftCount && (k - newSum) / i >= 0) {
-                            result[i - 1] = true;
-                        }
                     }
                 }
-                currentMaxSum += value * counts[value];
-                value++;
+            }
+            /**
+             * 出去背包中的已有元素，剩下的length-j个元素都为被限制值更新为i
+             */
+            for (int l = 0; l <= length - j; l++) {
+                /**
+                 * 如果选择l个被限制值更新后的元素i，则还需要从背包中选择若干个元素，使得他们的的和为k-i*l，此时包含所有这些被选元素的子序
+                 * 列之和才能为k
+                 */
+                int otherSum = k - l * i;
+
+                if (otherSum >= 0 && dp[otherSum]) {
+                    result[i - 1] = true;
+                    break;
+                }
             }
         }
         return result;
