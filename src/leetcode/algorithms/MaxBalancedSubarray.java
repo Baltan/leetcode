fac3912120217357a6370aff43df93b8421ef1ae
baltan <617640006@qq.com>
2026-01-27 15:11:55 +0800

    3755. Find Maximum Balanced XOR Subarray Length

--- src/leetcode/algorithms/MaxBalancedSubarray.java
+++ src/leetcode/algorithms/MaxBalancedSubarray.java
@@ -1,8 +1,6 @@
 package leetcode.algorithms;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -18,54 +16,46 @@ public class MaxBalancedSubarray {
         System.out.println(maxBalancedSubarray(new int[]{0}));
     }
 
+    /**
+     * 参考：<a href="https://leetcode.cn/problems/find-maximum-balanced-xor-subarray-length/solutions/3839714/qian-zhui-he-ha-xi-biao-by-tsreaper-wefq/"></a>
+     *
+     * @param nums
+     * @return
+     */
     public static int maxBalancedSubarray(int[] nums) {
         int result = 0;
-        int length = nums.length;
         /**
-         * evenCounts[i+1]表示数组nums的前i个元素中偶数的个数
-         */
-        int[] evenCounts = new int[length + 1];
-        /**
-         * oddCounts[i+1]表示数组nums的前i个元素中奇数的个数
-         */
-        int[] oddCounts = new int[length + 1];
-        /**
-         * xor -> 数组nums中所有元素按位异或值为xor的前缀子数组中最后一个元素的索引值的集合
-         */
-        Map<Integer, List<Integer>> xorMap = new HashMap<>();
-        /**
-         * 数组nums的前缀子数组中所有元素按位异或值
+         * 数组nums的前缀数组中所有元素按位异或的值
          */
         int prefixXor = 0;
         /**
-         * 哨兵，数组nums的前缀子数组为空数组时的情况
+         * 数组nums的前缀数组中奇数元素和偶数元素个数之差
          */
-        xorMap.computeIfAbsent(0, x -> new ArrayList<>()).add(-1);
-
+        int prefixCountDiff = 0;
+        /**
+         * 数组nums的前缀数组中所有元素按位异或的值 ->
+         *      { 数组nums的前缀数组中奇数元素和偶数元素个数之差 -> 满足条件的最短前缀数组的最后一个元素的索引值 }
+         */
+        Map<Integer, Map<Integer, Integer>> xorMap = new HashMap<>();
+        /**
+         * 哨兵，前缀数组为空数组的情况
+         */
+        xorMap.computeIfAbsent(0, x -> new HashMap<>()).put(0, -1);
+        /**
+         * 如果子数组nums[x……y]是平衡异或子数组，即nums[x]^nums[x+1]^……^nums[y-1]^nums[y]=0，且nums[x……y]中奇数元素和偶数元素个数
+         * 之差为0，则nums[0]^nums[1]^……^nums[x-1]^nums[x]=nums[0]^nums[1]^……^nums[y-1]^nums[y]，且nums[0……x]中奇数元素和偶数
+         * 元素个数之差与nums[0……y]中奇数元素和偶数元素个数之差相等
+         */
         for (int i = 0; i < nums.length; i++) {
             prefixXor ^= nums[i];
-            List<Integer> indexes = xorMap.computeIfAbsent(prefixXor, x -> new ArrayList<>());
-            /**
-             * 计算数组nums的前缀子数组nums[0……i]中奇数元素和偶数元素的个数
-             */
-            if (nums[i] % 2 == 0) {
-                evenCounts[i + 1] = evenCounts[i] + 1;
-                oddCounts[i + 1] = oddCounts[i];
+            prefixCountDiff += nums[i] % 2 == 0 ? -1 : 1;
+            Map<Integer, Integer> prefixCountDiffMap = xorMap.computeIfAbsent(prefixXor, x -> new HashMap<>());
+
+            if (prefixCountDiffMap.containsKey(prefixCountDiff)) {
+                result = Math.max(result, i - prefixCountDiffMap.get(prefixCountDiff));
             } else {
-                oddCounts[i + 1] = oddCounts[i] + 1;
-                evenCounts[i + 1] = evenCounts[i];
+                prefixCountDiffMap.put(prefixCountDiff, i);
             }
-            /**
-             * 前缀子数组nums[0……i]和nums[0……index]中所有元素按位异或值相等，则子数组nums[i+1……index]中所有元素按位异或值为0，如果子
-             * 数组中奇数元素和偶数元素的个数相等，则说明nums[i+1……index]是一个平衡异或子数组
-             */
-            for (int index : indexes) {
-                if (evenCounts[i + 1] - evenCounts[index + 1] == oddCounts[i + 1] - oddCounts[index + 1]) {
-                    result = Math.max(result, i - index);
-                    break;
-                }
-            }
-            indexes.add(i);
         }
         return result;
     }
