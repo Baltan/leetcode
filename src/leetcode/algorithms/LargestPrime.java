01a029844e2f0e062d00bbb74a8ffec420496c7c
baltan <617640006@qq.com>
2026-02-09 10:37:02 +0800

    3770. Largest Prime from Consecutive Prime Sum

--- src/leetcode/algorithms/LargestPrime.java
+++ src/leetcode/algorithms/LargestPrime.java
@@ -1,5 +1,8 @@
 package leetcode.algorithms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Description: 3770. Largest Prime from Consecutive Prime Sum
  *
@@ -15,6 +18,35 @@ public class LargestPrime {
         System.out.println(largestPrime(2));
     }
 
+    /**
+     * 根据题意，nums[i]∈[1,500000]
+     */
+    private static final int MAX = 500000;
+    /**
+     * 升序保存[1,500000]中的所有质数
+     */
+    private static final List<Integer> PRIME_NUMS = new ArrayList<>();
+    /**
+     * IS_PRIME[i]表示数字i是否是质数
+     */
+    private static final boolean[] IS_PRIME = new boolean[MAX + 1];
+
+    /**
+     * 初始化计算[1,500000]中的各个元素是否是质数
+     */
+    static {
+        outer:
+        for (int i = 2; i <= MAX; i++) {
+            for (int j = 2; j * j <= i; j++) {
+                if (IS_PRIME[j] && i % j == 0) {
+                    continue outer;
+                }
+            }
+            IS_PRIME[i] = true;
+            PRIME_NUMS.add(i);
+        }
+    }
+
     public static int largestPrime(int n) {
         if (n < 2) {
             return 0;
@@ -24,43 +56,18 @@ public class LargestPrime {
          * 从2开始的所有质数之和
          */
         int sum = 0;
-        /**
-         * isPrime[i]表示i是否是质数
-         */
-        boolean[] isPrime = new boolean[n + 1];
 
-        for (int i = 0; i <= n; i++) {
-            isPrime[i] = isPrime(i);
-        }
+        for (int primeNum : PRIME_NUMS) {
+            sum += primeNum;
 
-        for (int i = 2; sum < n; i++) {
-            if (isPrime[i]) {
-                sum += i;
+            if (sum > n) {
+                break;
             }
 
-            if (sum <= n && isPrime[sum]) {
+            if (IS_PRIME[sum]) {
                 result = sum;
             }
         }
         return result;
-    }
-
-    /**
-     * 判断n是不是质数
-     *
-     * @param n
-     * @return
-     */
-    public static boolean isPrime(int n) {
-        if (n == 0 || n == 1) {
-            return false;
-        }
-
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
     }
 }
